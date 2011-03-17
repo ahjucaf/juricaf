86e42c01bf6905d5022de1465242dfe0c21e9f9b
Tangui Morlier <tangui@tangui.eu.org>
2011-03-17 15:05:18 +0000

    Import des juricafs existants dans couchdb
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@4 59802511-991d-49c3-8a85-851a450bd420

---
+++ couchdb2solr/juricaf2couchdb/import.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cpt=0; 
+rm test.json ; 
+for i in ../../data/XML/* ; do 
+    for y in $i/*; do 
+	cat "$y" | iconv -f ISO88591 -t UTF8 | sed 's/\r/\n/g' | sed 's/<BR *\/*>/\n/gi' >  data.xml ; 
+	php juricaf2json.php >> test.json ; 
+	echo -n ',' >> test.json ; cpt=$(expr $cpt + 1) ; 
+	if test $cpt -eq 100 ; then 
+	    sed 's/^/{"docs":[/' test.json | sed 's/,$/]}/' > test.json.tmp; 
+	    mv test.json.tmp test.json ; 
+	    curl -d @test.json  -X POST "http://127.0.0.1:5984/ahjucaf/_bulk_docs"; 
+	    cpt=0; 
+	    rm test.json ; 
+	fi  ; 
+    done ; 
+done
+
---
+++ couchdb2solr/juricaf2couchdb/juricaf2json.php
@@ -0,0 +1,51 @@
+<?php
+
+function parse($obj, $n = 0) 
+{
+  $res = array();
+  $cpt = 0;
+  if (is_a($obj, 'SimpleXMLElement'))
+    $obj = get_object_vars($obj);
+  if (is_array($obj))
+  foreach($obj as $k => $v) {
+      if (!is_a($v, 'SimpleXMLElement')) {
+	if (is_array($v)) {
+	  $i = 0;
+	  foreach ($v as $a) {
+	    $r = parse($a, $n + 1);
+	    if ($r)
+	      $res[$i++] = $r;
+	  }
+	  if ($i)
+	    $cpt++;
+	} else {
+	  $cpt++;
+	  $res[$k] = utf8_decode("$v");
+	}
+      } else {
+	$r = parse($obj[$k], $n + 1);
+	if ($r) 
+	  $res[$k] = $r;
+      }
+    }
+  if (!$cpt)
+    return ;
+  return $res;
+}
+
+
+function ids($str) {
+  $str = strtr($str,
+	       array('è'=>'e','é'=>'e','ê'=>'e','ë'=>'e','à'=>'a','á'=>'a','â'=>'a','ã'=>'a','ä'=>'a','ç'=>'c','ì'=>'i','í'=>'i','î'=>'i','ï'=>'i','ñ'=>'n','ò'=>'o','ó'=>'o','ô'=>'o','õ'=>'o','ö'=>'o','ù'=>'u','ú'=>'u','û'=>'u','ü'=>'u','ý'=>'y','ÿ'=>'y','À'=>'A','Á'=>'A','Â'=>'A','Ã'=>'A','Ä'=>'A','Ç'=>'C','È'=>'E','É'=>'E','Ê'=>'E','Ë'=>'E','Ì'=>'I','Í'=>'I','Î'=>'I','Ï'=>'I','Ñ'=>'N','Ò'=>'O','Ó'=>'O','Ô'=>'O','Õ'=>'O','Ö'=>'O','Ù'=>'U','Ú'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y'));
+  $str = preg_replace('/[^a-z0-9\-]/i', '', $str);
+  return strtoupper($str);
+}
+
+$obj = simplexml_load_file("data.xml");
+$res = parse($obj);
+if (!$res['TITRE']) 
+{
+  $res['TITRE'] = $res['PAYS'].' : Décision n°'.$res['NUM_ARRET'].' du '.$res['DATE_ARRET'].' ('.$res['FORMATION'].')';
+}
+$res['_id'] = ids($res['PAYS'].'-'.$res['FORMATION'].'-'.$res['NUM_ARRET']);
+print json_encode($res);
