90fc5b1fd4d4a01123cbd1be8c8ce46e1f132c19
Tangui Morlier <tangui@tangui.eu.org>
2011-03-21 11:44:29 +0000

    Début de synchronisateur couchdb -> solr
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@5 59802511-991d-49c3-8a85-851a450bd420

---
+++ couchdb2solr/couchdb2solr/couchdb2solr.php
@@ -0,0 +1,121 @@
+<?php
+
+include("conf/config.php");
+include("utils.php");
+
+
+global $lock, $cpt;
+
+$lock = fopen($lock_seq_file, 'a+');
+if (!$lock) die('error with lock file');
+fseek($lock, 0);
+$last_seq = rtrim(fgets($lock));
+
+echo "last_seq : $last_seq\n";
+
+
+$cpt = 0;
+
+
+function storeSeq($seq) {
+  global $lock, $cpt, $lock_seq_file;
+  fclose($lock);
+  $lock = fopen($lock_seq_file, 'w+');
+  fwrite($lock, $seq."\n");
+  $cpt = 0;
+  commitIndexer();
+}
+
+
+function updateIndexer($id) {
+  global $couchdb_url_db, $solr_url_db, $last_seq;
+  $couchdata = json_decode(file_get_contents($couchdb_url_db.'/'.$id));
+  if (!$couchdata)
+    continue;
+  unset($couchdata->_rev);
+  $solrdata = '<add><doc>';
+  foreach($couchdata as $k => $v) {
+    $k = strtolower($k);
+    if ($k == 'id')
+      continue;
+    if ($k == '_id') {
+      $k = 'id';
+      $id = $v;
+      echo "$last_seq : $id\n";
+    }
+    if (preg_match('/^_/', $k))
+      continue;
+    if (preg_match('/^_/', $id)) {
+      $solrdata = '';
+	break ;
+    }
+    $solrdata .= '<field name="'.$k.'">';
+      $v = preg_replace('/&/', ' ', $v.'');
+      if (preg_match('/date/', $k) && preg_match('/(\d{2})\/(\d{2})\/(\d{4})/', $v, $match))
+	$v = $match[3].'-'.$match[2].'-'.$match[1].'T12:00:00.000Z';
+      $solrdata .= preg_replace('/\n/', ' ', $v);
+      $solrdata .= '</field>';
+  }
+  if ($solrdata) {
+    $solrdata .= '</doc></add>';
+    //    echo "$solrdata\n";
+    do_post_request($solr_url_db.'/update', $solrdata, "content-type: text/xml");
+  }
+}
+
+function deleteIndexer($id) {
+  global $solr_url_db, $last_seq;
+  $solrdata = "<delete><id>$id</id></delete>";
+  echo "$last_seq : $id (DELETED)\n";
+  do_post_request($solr_url_db.'/update', $solrdata, "content-type: text/xml");
+}
+
+function commitIndexer() {
+  global $solr_url_db, $last_seq;
+  $solrdata = "<commit/>";
+  do_post_request($solr_url_db.'/update', $solrdata, "content-type: text/xml");
+}
+
+
+
+while(1) {
+
+  $url = $couchdb_url_db.'/_changes?feed=continuous';
+  if ($last_seq)
+    $url .= '&since='.$last_seq;
+  echo "$url\n";
+  $changes = fopen($url, 'r');
+  
+  while($l = fgets($changes)) {
+    $cpt++;
+    $change = json_decode($l);
+    if (!$change) {
+      echo "pb json : $l\n";
+      continue;
+    }
+    
+    if ($change->last_seq) {
+	echo "last_seq\n";
+      storeSeq($change->last_seq);
+      break;
+    }
+
+    $last_seq = $change->seq;
+    if ($cpt > 100) {
+      storeSeq($last_seq);
+    }
+
+
+
+    if ($change->deleted) {
+      deleteIndexer($change->id);
+      continue;
+    }
+
+    updateIndexer($change->id);
+    
+  }
+
+  fclose($changes);
+ }
+fclose($lock);
