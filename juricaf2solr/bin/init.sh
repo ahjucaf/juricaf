05cd2966178d3a93e554daf4091363036546e76a
Tangui Morlier <tangui@tangui.eu.org>
2011-03-22 13:42:50 +0000

    Ajout des reduces
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@28 59802511-991d-49c3-8a85-851a450bd420

--- juricaf2solr/bin/init.sh
+++ juricaf2solr/bin/init.sh
@@ -43,10 +43,12 @@ cat <<EOF > $TMPFILE
   "views":
   {
     "pas_de_texte_arret": {
-      "map": "function(doc) { if (doc.type == 'arret' && (!doc.texte_arret || doc.texte_arret.length < 1))  emit(doc._id, 1); }"
+      "map": "function(doc) { if (doc.type == 'arret' && (!doc.texte_arret || doc.texte_arret.length < 1))  emit(doc._id, 1); }",
+      "reduce": "function(keys, values) { return sum(values) }"
     },
     "num_arret_trop_gros": {
-      "map": "function(doc) { if (doc.type == 'arret' && doc.num_arret.length > 20)  emit(doc._id, 1); }"
+      "map": "function(doc) { if (doc.type == 'arret' && doc.num_arret.length > 20)  emit(doc._id, 1); }",
+      "reduce": "function(keys, values) { return sum(values) }"
     }
   }
 }
