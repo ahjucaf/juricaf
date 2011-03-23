54efca2142a129290dc7a0f67ffba4f015f9ad8a
Tangui Morlier <tangui@tangui.eu.org>
2011-03-23 13:57:56 +0000

    Ajout des exceptions d'accès au _dev
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@34 59802511-991d-49c3-8a85-851a450bd420

--- project/web/frontend_dev.php
+++ project/web/frontend_dev.php
@@ -2,8 +2,9 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '82.225.240.116')))
 {
+  php_info();
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
