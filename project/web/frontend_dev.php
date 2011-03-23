30da89ae39ee2850fa4ced3fef35787c6bb7d311
Tangui Morlier <tangui@tangui.eu.org>
2011-03-23 14:22:01 +0000

    Oups n'aurait pas du être commité
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@37 59802511-991d-49c3-8a85-851a450bd420

--- project/web/frontend_dev.php
+++ project/web/frontend_dev.php
@@ -4,7 +4,6 @@
 // feel free to remove this, extend it or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '82.225.240.116')))
 {
-  php_info();
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
