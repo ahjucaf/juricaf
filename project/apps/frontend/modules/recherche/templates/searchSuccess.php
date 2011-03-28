4092abd3091019887c53d824d4e7944b4d0d9624
Tangui Morlier <tangui@tangui.eu.org>
2011-03-28 21:58:36 +0000

    Ajout du nombre de résultats
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@54 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/searchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/searchSuccess.php
@@ -3,15 +3,8 @@
 <a href="<?php echo url_for('@recherche'); ?>" style="border: none;"><img src="/images/juricaf.png" alt="Juricaf" /></a>
 </div>
 <div>
-<h2>Résultats pour "<?php echo $query; ?>"</h2>
+<h2><?php echo $resultats->numFound; ?> résultats pour "<?php echo $query; ?>"</h2>
 <?php
-/*
-echo "<pre>";
-print_r($resultats->docs);
-echo "</pre>";
-
-'.url_for('@arret?id='.$resultat->_fields->id).'*/
-
 foreach ($resultats->docs as $resultat) {
   echo '<h3><a href="'.url_for('@arret?id='.$resultat->id).'">'.$resultat->titre.'</a></h3>';
   echo '<p>'.truncate_text($resultat->texte_arret, 500, "...").'</p>';
