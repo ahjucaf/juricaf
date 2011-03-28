bb54a1f508e0e5395c4b3ee79e5f6664e77a0db8
Brice Person <brice@ideeslibres.org>
2011-03-28 21:11:25 +0000

    Corrections diverses et logo cliquable
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@49 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/searchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/searchSuccess.php
@@ -1,20 +1,20 @@
 <?php use_helper('Text'); ?>
 <div>
-<img src="/images/juricaf.png" alt="Juricaf" />
+<a href="<?php echo url_for('@recherche'); ?>" style="border: none;"><img src="/images/juricaf.png" alt="Juricaf" /></a>
 </div>
 <div>
 <h2>Résultats pour "<?php echo $query; ?>"</h2>
 <?php
+/*
 echo "<pre>";
 print_r($resultats->docs);
-echo "</pre>";/*
+echo "</pre>";
 
-'.url_for('@arret?id='.$resultat->_fields->id).'
+'.url_for('@arret?id='.$resultat->_fields->id).'*/
+
 foreach ($resultats->docs as $resultat) {
-  echo '<h3><a href="">'.$resultat->_fields->titre.'</a></h3>';
-  echo '<p>'.truncate_text($resultat->_fields->texte_arret, 200, "...").'</p>';
+  echo '<h3><a href="'.url_for('@arret?id='.$resultat->id).'">'.$resultat->titre.'</a></h3>';
+  echo '<p>'.truncate_text($resultat->texte_arret, 500, "...").'</p>';
 }
-*/
-
 ?>
 </div>
