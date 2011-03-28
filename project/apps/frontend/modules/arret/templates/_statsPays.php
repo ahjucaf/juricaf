2275637173412a3fdca2ebf0d0e76bef1198e31e
Tangui Morlier <tangui@tangui.eu.org>
2011-03-28 23:43:12 +0000

    Prise en compte des pays avec des espaces
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@64 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -2,5 +2,8 @@
 <?php
 foreach ($pays as $p) 
 {
-  echo '<li>'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$p['key'][0]).'</li>';
+  $sep = '';
+  if (preg_match('/ /', $p['key'][0]))
+    $sep = '"';
+  echo '<li>'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$sep.$p['key'][0].$sep).'</li>';
 }?></ul>
