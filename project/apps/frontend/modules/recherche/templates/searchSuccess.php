6d3deb177e5ae6ef51759d7a15fc598fee843900
Brice Person <brice@ideeslibres.org>
2011-03-28 22:44:27 +0000

    Champ de recherche aussi dans les resultats
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@63 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/searchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/searchSuccess.php
@@ -1,6 +1,10 @@
 <?php use_helper('Text'); ?>
 <div>
 <a href="<?php echo url_for('@recherche'); ?>"><img src="/images/juricaf.png" alt="Juricaf" /></a>
+<form method="get" action="<?php echo url_for('recherche_resultats'); ?>">
+    <input type="text" name="q" value="<?php echo $query; ?>" tabindex="10" style="width: 300px;" />
+    <input type="submit" value="Rechercher" tabindex="20" />
+  </form>
 </div>
 <div>
 <h2><?php echo $resultats->numFound; ?> résultats pour "<?php echo $query; ?>"</h2>
