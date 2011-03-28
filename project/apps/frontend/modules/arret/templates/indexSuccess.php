1dbbd1e3074c96999b946729750bd1fdbdd36122
Brice Person <brice@ideeslibres.org>
2011-03-28 21:11:25 +0000

    Corrections diverses et logo cliquable
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@49 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/indexSuccess.php
+++ project/apps/frontend/modules/arret/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <?php $sf_response->setTitle('Bienvenue sur Juricaf.org'); ?>
 <div>
-<img src="/images/juricaf.png" alt="Juricaf" />
+<a href="<?php echo url_for('@recherche'); ?>" style="border: none;"><img src="/images/juricaf.png" alt="Juricaf" /></a>
 </div>
 <div>
 <h2><?php echo $document->titre; ?></h2>
--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,9 +1,8 @@
 <?php $sf_response->setTitle('Bienvenue sur Juricaf.org'); ?>
 <div class="form_recherche">
-  <img src="/images/juricaf.png" alt="Juricaf" />
+<a href="<?php echo url_for('@recherche'); ?>" style="border: none;"><img src="/images/juricaf.png" alt="Juricaf" /></a>
   <form method="get" action="<?php echo url_for('recherche_resultats'); ?>">
     <input type="text" name="q" tabindex="10" style="width: 300px;" /><br />
     <input type="submit" value="Rechercher" tabindex="20" />
-    <input type="button" value="Recherche avancée" tabindex="30" />
   </form>
 </div>
