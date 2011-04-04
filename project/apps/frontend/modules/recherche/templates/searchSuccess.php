bce7ac0ef3eaf74e8f9869cc8f0513c600a6bb5b
Tangui Morlier <tangui@tangui.eu.org>
2011-04-04 14:08:02 +0000

    Ajout des mises en valeur des mots clés + analyses
    (demande #16)
    
    
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@67 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/searchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/searchSuccess.php
@@ -10,9 +10,20 @@
 <div>
 <h2><?php echo $resultats->numFound; ?> résultats pour «&nbsp;<?php echo $query; ?>&nbsp;»</h2>
 <?php
-foreach ($resultats->docs as $resultat) {
-  echo '<h3><a href="'.url_for('@arret?id='.$resultat->id).'">'.$resultat->titre.'</a></h3>';
-  echo '<p>'.preg_replace ('/[^a-z0-9]*\.\.\.$/i', '...', truncate_text($resultat->texte_arret, 500, "...", true)).'</p>';
+foreach ($resultats->response->docs as $resultat) {
+  echo '<div class="resultat"><h3><a href="'.url_for('@arret?id='.$resultat->id).'">'.$resultat->titre.'</a></h3>';
+  echo '<p>';
+  $exerpt = '';
+  if (isset($resultats->highlighting) && $resultats->highlighting->{$resultat->id}) {
+    foreach ($resultats->highlighting->{$resultat->id}->content as $h)
+      $exerpt .= '...'.html_entity_decode($h);
+    $exerpt .= '...' ;
+  }
+  if ($resultat->analyses) 
+    $exerpt .= $resultat->analyses.'...';
+  echo preg_replace ('/[^a-z0-9]*\.\.\.$/i', '...', truncate_text($exerpt.$resultat->texte_arret, 500, "...", true));
+  echo '</p>';
+  echo '<div class="extra"><span class="pays '.preg_replace('/ /', '_', $resultat->pays).'">'.$resultat->pays.'</span> - <span class="date">'.date('d/m/Y', strtotime($resultat->date_arret)).'</span> - <span class="juridiction">'.$resultat->juridiction.', '.$resultat->formation.'</span> - <span class="num">'.$resultat->num_arret.'</span></div></div>';
 }
 ?>
 </div>
