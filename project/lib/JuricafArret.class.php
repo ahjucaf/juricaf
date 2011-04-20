f683b78f8d9705ba967acd437df757ac0a0e3c28
Tangui Morlier <tangui@tangui.eu.org>
2011-04-20 07:47:36 +0000

    Correction du plugin sfCouchDocument
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@95 59802511-991d-49c3-8a85-851a450bd420

--- project/lib/JuricafArret.class.php
+++ project/lib/JuricafArret.class.php
@@ -4,6 +4,11 @@ class JuricafArret extends sfCouchDocument
 {
   private static $fields = array('_id', 'analyses', 'date_arret', 'formation', 'juricaf_id', 'juridiction', 'num_arret', 'pays', 'section', 'texte_arret', 'titre', 'type'); 
   public function getFields() {
-    return self::$fields;
+    $fields = array();
+    foreach (self::$fields as $f) {
+      if ($this->__isset($f))
+	$fields[] = $f;
+    }
+    return $fields;
   }
 }
