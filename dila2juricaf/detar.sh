a17feed6ee017adbf632565c82143de27b02cb43
AHJUCAF <contact@ajucaf.org>
2011-05-03 09:34:39 +0000

    (Brice) Automatisation de la conversion Dila -> Juricaf v2
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@109 59802511-991d-49c3-8a85-851a450bd420

--- dila2juricaf/detar.sh
+++ dila2juricaf/detar.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+DILA2JURICAF=/home/ahjucaf/project/dila2juricaf
 DILA=/home/ahjucaf/project/data/dila
-cd /home/ahjucaf/ftp/dila
-find -name '*.tar.gz' > to_detar.txt
-for fichier in $(cat to_detar.txt); do tar -zxvf "$fichier" -C $DILA ; done
+FTP=/home/ahjucaf/ftp/dila
+cd $FTP
+find -name '*.tar.gz' > $DILA/to_detar.txt
+for fichier in $(cat $DILA/to_detar.txt); 
+  do
+cd $FTP 
+echo "Décompression de $fichier" ; tar -zxvf "$fichier" -C "$DILA" ; 
+cd $DILA2JURICAF
+echo "Conversion des fichiers";
+./extract.sh ; 
+cd $DILA
+rm -r * ;
+done
 #rm to_detar.txt
