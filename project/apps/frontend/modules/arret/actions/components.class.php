2123e68de56e00fec3f701d7a7f47b9c978d92fd
Tangui Morlier <tangui@tangui.eu.org>
2011-05-05 21:42:33 +0000

    Essaye de survivre aux longs calculs
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@119 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/actions/components.class.php
+++ project/apps/frontend/modules/arret/actions/components.class.php
@@ -4,6 +4,8 @@ class arretComponents extends sfComponents
 {
   public function executeStatsPays() {
     $db = sfCouchConnection::getInstance();
+    try{
     $this->pays = $db->get('_design/stats/_view/pays_juridiction?group_level=1')->rows;
+    }catch(Exception $e) {$this->pays = null;}
   }
 }
