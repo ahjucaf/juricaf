fa52156fa2e57dc04e1048183d41e20ff6cbc60d
Tangui Morlier <tangui@tangui.eu.org>
2011-05-09 09:30:32 +0000

    Changement de non du component
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@124 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/actions/components.class.php
+++ project/apps/frontend/modules/arret/actions/components.class.php
@@ -5,7 +5,7 @@ class arretComponents extends sfComponents
   public function executeStatsPays() {
     $db = sfCouchConnection::getInstance();
     try{
-    $this->pays = $db->get('_design/stats/_view/pays_juridiction?group_level=1')->rows;
+    $this->pays = $db->get('_design/stats/_view/pays_juridiction_date?group_level=1')->rows;
     }catch(Exception $e) {$this->pays = null;}
   }
 }
