5ba5b31087b9f2aa4ddad2c2d62d54aafa8fb1a6
Tangui Morlier <tangui@tangui.eu.org>
2011-05-09 16:39:58 +0000

    Template oublié
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@138 59802511-991d-49c3-8a85-851a450bd420

---
+++ project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
@@ -0,0 +1,10 @@
+<ul class="juridcols">
+<?php
+foreach ($pays as $p => $juridictions) 
+{
+  echo '<li>'.link_to($p,'recherche/search?query=pays:'.$p).'<ul class="subul">';
+  foreach ($juridictions as $j => $v) {
+    echo '<li>'.link_to($j.' ('.$v['value'].' arrêts de '.$v['deb'].' à '.$v['fin'].')', 'recherche/search?query=pays:'.$p.'+juridiction:'.$j).'</li>';
+  }
+  echo "</ul></li>";
+}?></ul>
