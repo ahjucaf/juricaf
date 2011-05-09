c7b390f7f4ed18ea98ed754edf20a4180d0c8779
Tangui Morlier <tangui@tangui.eu.org>
2011-05-09 16:36:54 +0000

    Des stats avec les années et les juridictions (pas de présentation pour le moment)
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@137 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/actions/components.class.php
+++ project/apps/frontend/modules/arret/actions/components.class.php
@@ -8,4 +8,23 @@ class arretComponents extends sfComponents
     $this->pays = $db->get('_design/stats/_view/pays_juridiction_date?group_level=1')->rows;
     }catch(Exception $e) {$this->pays = null;}
   }
+  public function executeStatsPaysJuridiction() {
+    $db = sfCouchConnection::getInstance();
+    try{
+    $pays = $db->get('_design/stats/_view/pays_juridiction_date?group_level=3')->rows;
+    $this->pays = array();
+    foreach ($pays as $p) {
+      if (!isset($this->pays[$p['key'][0]]))
+	$this->pays[$p['key'][0]] = array();
+      if (!isset($this->pays[$p['key'][0]][$p['key'][1]])) {
+	$this->pays[$p['key'][0]][$p['key'][1]] = array('value'=>0, 'deb' => '9999', 'fin' => 0);
+      }
+      $this->pays[$p['key'][0]][$p['key'][1]]['value'] += $p['value'];
+      if ($this->pays[$p['key'][0]][$p['key'][1]]['deb'] > $p['key'][2])
+	$this->pays[$p['key'][0]][$p['key'][1]]['deb'] = $p['key'][2];
+      if ($this->pays[$p['key'][0]][$p['key'][1]]['fin'] < $p['key'][2])
+	$this->pays[$p['key'][0]][$p['key'][1]]['fin'] = $p['key'][2];
+    }
+    }catch(Exception $e) {$this->pays = null;}
+  }
 }
