2e723c66c0dbe90a4f1926f8c50a5807ef64e830
Brice Person <brice@ideeslibres.org>
2011-05-12 23:44:59 +0000

    Synchronisation automatique FTP Dila
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@146 59802511-991d-49c3-8a85-851a450bd420

---
+++ dila2juricaf/compare.php
@@ -0,0 +1,46 @@
+<?php
+$old = file('log/old.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$new = file('log/new.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$to_update = "";
+$file_log = "log/to_detar_update.txt";
+$prev_file_log = "log/prev_to_detar_update.txt";
+
+foreach ($new as $line) {
+  if(!in_array($line, $old)) {
+    $line = explode('#', $line);
+    $to_process[$line[0]] = $line[1];
+  }
+}
+
+if (is_array($to_process)) {
+  asort($to_process);
+  foreach ($to_process as $value) {
+    $to_update .= $value."\n";
+  }
+  $handler = fopen($file_log,"w");
+  try {
+    fputs($handler,$to_update);
+  }
+  catch (Exception $e) {
+    echo "Erreur d'enregistrement de ".$file_log." contenant la liste des nouveaux fichiers à décompresser\n";
+    echo $e->getMessage()."\n";
+    exit;
+  }
+}
+else {
+  if ($stream = fopen($file_log, 'r')) {
+    $prev_update = stream_get_contents($stream);
+    $handler = fopen($prev_file_log,"w");
+    try {
+      fputs($handler,$prev_update);
+    }
+    catch (Exception $e) {
+      echo "Erreur d'enregistrement de ".$prev_file_log." contenant la liste des fichiers décompressés précédement\n";
+      echo $e->getMessage()."\n";
+      exit;
+    }
+    fclose($stream);
+    unlink ($file_log);
+  }
+}
+?>
---
+++ dila2juricaf/sort.php
@@ -0,0 +1,26 @@
+<?php
+$log = 'log/to_detar_all.txt';
+$to_detar_all = file($log, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+foreach ($to_detar_all as $line) {
+  $line = explode('#', $line);
+  $to_process[$line[0]] = $line[1];
+}
+
+if (is_array($to_process)) {
+  asort($to_process);
+  foreach ($to_process as $value) {
+    $to_update .= $value."\n";
+  }
+  $handler = fopen($log,"w");
+  try {
+    fputs($handler,$to_update);
+  }
+  catch (Exception $e) {
+    echo "Erreur d'enregistrement de ".$log." contenant la liste des fichiers à décompresser\n";
+    echo $e->getMessage()."\n";
+    exit;
+  }
+}
+
+?>
