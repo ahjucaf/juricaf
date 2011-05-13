f010f57a34d0a009bd97e6740e7a70a9baea94ad
Brice Person <brice@ideeslibres.org>
2011-05-13 09:59:22 +0000

    Faire de même avec les liens de premier niveau
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@153 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
+++ project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
@@ -6,7 +6,7 @@ function replaceBlank($str) {
 
 foreach ($pays as $p => $juridictions)
 {
-  echo '<li>'.link_to($p,'recherche/search?query=pays:'.$p).'<ul class="subul">';
+  echo '<li>'.link_to($p,'recherche/search?query=pays:'.replaceBlank($p)).'<ul class="subul">';
   foreach ($juridictions as $j => $v) {
     echo '<li>'.link_to($j.' ('.$v['value'].' décisions de '.$v['deb'].' à '.$v['fin'].')', 'recherche/search?query=pays:'.replaceBlank($p).'+juridiction:'.replaceBlank($j)).'</li>';
   }
