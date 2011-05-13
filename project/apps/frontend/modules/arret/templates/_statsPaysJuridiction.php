0928bb5e45aedc7b2b5ffca7526d6a83b54bcb06
Brice Person <brice@ideeslibres.org>
2011-05-13 09:54:27 +0000

    Underscores à la place des espaces pour les liens
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@152 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
+++ project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
@@ -1,10 +1,14 @@
 <ul class="juridcols">
 <?php
-foreach ($pays as $p => $juridictions) 
+function replaceBlank($str) {
+  return str_replace (' ', '_', $str);
+}
+
+foreach ($pays as $p => $juridictions)
 {
   echo '<li>'.link_to($p,'recherche/search?query=pays:'.$p).'<ul class="subul">';
   foreach ($juridictions as $j => $v) {
-    echo '<li>'.link_to($j.' ('.$v['value'].' décisions de '.$v['deb'].' à '.$v['fin'].')', 'recherche/search?query=pays:'.$p.'+juridiction:'.$j).'</li>';
+    echo '<li>'.link_to($j.' ('.$v['value'].' décisions de '.$v['deb'].' à '.$v['fin'].')', 'recherche/search?query=pays:'.replaceBlank($p).'+juridiction:'.replaceBlank($j)).'</li>';
   }
   echo "</ul></li>";
 }?></ul>
