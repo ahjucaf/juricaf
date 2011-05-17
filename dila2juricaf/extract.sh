3926d3e207e2d478394fbcb748045ecb3b9bbb0e
Tangui Morlier <tangui@tangui.eu.org>
2011-05-17 11:05:54 +0000

    Pour éviter la limite du nombre de caractères dans un shell imbriqué
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@158 59802511-991d-49c3-8a85-851a450bd420

--- dila2juricaf/extract.sh
+++ dila2juricaf/extract.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-find ../data/dila/ -name '*.xml' > to_process.txt
-for fichier in $(cat to_process.txt); do php dila2juricaf.php "$fichier"; done
-rm to_process.txt
+find ../data/dila/ -name '*.xml' | while read fichier ;
+do 
+	php dila2juricaf.php "$fichier"; 
+done
