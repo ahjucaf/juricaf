73cdbd49f9ad1388fe18808badbb9930d0d5e55f
Tangui Morlier <tangui@tangui.eu.org>
2011-06-13 23:05:47 +0000

    Debut des stats pour la home et sur page dédiée
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@208 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -1,7 +1,13 @@
+<div class="stats_pays_juridiction">
 <ul class="payscols">
 <?php
-foreach ($pays as $p) 
+function replaceBlank($str) {
+  return str_replace (' ', '_', $str);
+}
+
+foreach ($pays as $p)
 {
   $pays = preg_replace('/ /', '_', $p['key'][0]);
-  echo '<li>'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$pays).'</li>';
+  echo '<li style="list-style-image: url(/images/drapeaux/'.ucfirst($pays).'.png)">'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$pays).'</li>';
 }?></ul>
+</div>
