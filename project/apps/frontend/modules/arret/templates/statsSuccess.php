152e3353ae8e6b1381e48629093cec06655c59c1
Brice Person <brice@ideeslibres.org>
2011-06-14 15:17:46 +0000

    Validité w3c et mise en page
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@225 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/statsSuccess.php
+++ project/apps/frontend/modules/arret/templates/statsSuccess.php
@@ -1,4 +1,4 @@
-<h1>Les statistiques de la base Juricaf</h1>
+<h1 style="text-align: center;">Les statistiques de la base Juricaf</h1>
 <div class="stats">
     <?php include_component('arret', 'statsPaysJuridiction'); ?>
 </div>
