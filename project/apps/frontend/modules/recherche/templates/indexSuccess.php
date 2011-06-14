7607f811e6efaecbd5ded86dbc3f2f99c1959994
Tangui Morlier <tangui@tangui.eu.org>
2011-06-14 09:29:29 +0000

    Présentation des stats en Home
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@212 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,7 +1,4 @@
 <?php $sf_response->setTitle('Bienvenue sur Juricaf.org'); ?>
 <div class="stats">
     <?php include_component('arret', 'statsPays'); ?>
-<div class="plus">
-    <?php echo link_to('Plus de statistiques', '@stats'); ?>
-</div>
 </div>
