7999f2f717c02ba5d624b77c56fa2d1fd6899785
Brice Person <brice@ideeslibres.org>
2011-06-14 23:27:04 +0000

    Meilleure présentation des facets pour les résultats de recherche
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@239 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/_facets.php
+++ project/apps/frontend/modules/recherche/templates/_facets.php
@@ -3,12 +3,11 @@
 <ul><?php
 if (count($facets) > 1) {
   foreach($facets as $k => $v) {
-    echo '<li';
-    echo '>'.link_to($k."&nbsp;(".$v['count'].")", '@recherche_resultats?query='.$query.'&facets='.$v['fid'].':'.preg_replace('/ /', '_', $v['fname']).$facetslink);
+    echo '<li style="margin-top: 5px;"><img style="height: 10px;" src="/images/drapeaux/'.urlencode(replaceBlank($v['fname'])).'.png" alt="" /> '.link_to($k."&nbsp;(".$v['count'].")", '@recherche_resultats?query='.$query.'&facets='.$v['fid'].':'.replaceBlank($v['fname']).$facetslink);
     if (count($v['sub'])) {
       echo '<ul>';
       foreach ($v['sub'] as $f => $o) {
-	echo "<li class='sub'>".link_to($f."&nbsp;(".$o['count'].")", '@recherche_resultats?query='.$query.'&facets='.$o['fid'].':'.preg_replace('/ /', '_', $o['fname']).$facetslink).'</li>';
+  echo "<li class='sub'>".link_to($f."&nbsp;(".$o['count'].")", '@recherche_resultats?query='.$query.'&facets='.$o['fid'].':'.replaceBlank($o['fname']).$facetslink).'</li>';
       }
       echo '</ul>';
     }
