95e664a78866a0c0165492d775423475709b81ac
Tangui Morlier <tangui@tangui.eu.org>
2011-06-14 09:29:29 +0000

    Présentation des stats en Home
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@212 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -1,13 +1,15 @@
-<div class="stats_pays_juridiction">
-<ul class="payscols">
+<div class="payscols">
+<table><tr>
 <?php
 function replaceBlank($str) {
   return str_replace (' ', '_', $str);
 }
-
+$cpt = 0;
 foreach ($pays as $p)
 {
   $pays = preg_replace('/ /', '_', $p['key'][0]);
-  echo '<li style="list-style-image: url(/images/drapeaux/'.ucfirst($pays).'.png)">'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$pays).'</li>';
-}?></ul>
+  if (++$cpt % 2)
+    echo '</tr><tr>';
+  echo '<td><img src="/images/drapeaux/'.ucfirst($pays).'.png"/>&nbsp;'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=pays:'.$pays).'</li></td>';
+}?></tr><tr><td colspan=2 class="plus"><?php echo link_to('Plus de statistiques', '@stats'); ?></td></table>
 </div>
