f1041b0344c520e495590c39299777c958ad5853
Brice Person <brice@ideeslibres.org>
2011-06-14 17:00:58 +0000

    Mise en place de google analytics
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@231 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/templates/layout.php
+++ project/apps/frontend/templates/layout.php
@@ -57,5 +57,18 @@
         <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
       </div>
     </div>
+    <script type="text/javascript">
+    <!--
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-8802834-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    // -->
+    </script>
   </body>
 </html>
