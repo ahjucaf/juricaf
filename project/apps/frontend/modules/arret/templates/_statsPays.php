bb7e31db50cb7dcab74aac051f9c40a450f45f36
Tangui Morlier <tangui@tangui.eu.org>
2011-06-15 13:31:29 +0000

    Ajout de stats globales
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@244 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -2,16 +2,22 @@
 function replaceBlank($str) {
   return str_replace (' ', '_', $str);
 }
+
+function num($num) {
+  return preg_replace('/(\d)(\d{3})$/', '\1&nbsp;\2', $num);
+}
+
 $cpt = 0;
 ?>
 <div class="payscols">
+  <p style='text-align: center;'>Rechercher parmis <?php echo num($nb); ?> arrets provenant de <?php echo count($pays); ?> pays :</p>
 <table><tr>
 <?php
 foreach ($pays as $p)
 {
   $pays = preg_replace('/ /', '_', $p['key'][0]);
   if (++$cpt%2 && $cpt !== 1) { echo '</tr><tr>'; }
-  echo '<td><img src="/images/drapeaux/'.urlencode(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.link_to($p['key'][0].' ('.$p['value'].')','recherche/search?query=+&facets=pays:'.$pays).'</td>';
+  echo '<td><img src="/images/drapeaux/'.urlencode(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.link_to($p['key'][0].' ('.num($p['value']).')','recherche/search?query=+&facets=pays:'.$pays).'</td>';
 }
 ?>
 </tr><tr><td colspan="2" class="plus"><?php echo link_to('Plus de statistiques', '@stats'); ?></td></tr></table>
