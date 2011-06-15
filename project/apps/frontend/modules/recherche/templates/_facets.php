d766f47192b9bd6a2363761081b2a554aa54317f
Tangui Morlier <tangui@tangui.eu.org>
2011-06-15 11:14:21 +0000

    Gestion plus générique des facettes
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@242 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/_facets.php
+++ project/apps/frontend/modules/recherche/templates/_facets.php
@@ -1,22 +1,30 @@
 <?php if (count($facets) > 0) : ?>
 <p><strong><?php echo $label; ?></strong></p>
 <ul><?php
-if (count($facets) > 1) {
-  foreach($facets as $k => $v) {
-    echo '<li style="margin-top: 5px;"><img style="height: 10px;" src="/images/drapeaux/'.urlencode(replaceBlank($v['fname'])).'.png" alt="" /> '.link_to($k."&nbsp;(".$v['count'].")", '@recherche_resultats?query='.$query.'&facets='.$v['fid'].':'.replaceBlank($v['fname']).$facetslink);
+foreach($facets as $k => $v) {
+    echo '<li style="margin-top: 5px;">';
+    if($mainid === 'pays')
+	echo '<img style="height: 10px;" src="/images/drapeaux/'.urlencode(replaceBlank($v['fname'])).'.png" alt="" />&nbsp;';
+    $facet = $v['fid'].':'.preg_replace('/ /', '_', $v['fname']);
+    if (preg_match('/'.$facet.'/', $facetslink))
+      echo $k."&nbsp;(".$v['count'].")";
+    else
+      echo link_to($k."&nbsp;(".$v['count'].")", '@recherche_resultats?query='.$query.'&facets='.$facet.$facetslink);
     if (count($v['sub'])) {
       echo '<ul>';
       foreach ($v['sub'] as $f => $o) {
-  echo "<li class='sub'>".link_to($f."&nbsp;(".$o['count'].")", '@recherche_resultats?query='.$query.'&facets='.$o['fid'].':'.replaceBlank($o['fname']).$facetslink).'</li>';
+	echo "<li class='sub'>";
+	$facet = $o['fid'].':'.preg_replace('/ /', '_', $o['fname']);
+	if (preg_match('/'.$facet.'/', $facetslink) || $o['count'] == $v['count'])
+	  echo $f."&nbsp;(".$o['count'].")";
+	else
+	  echo link_to($f."&nbsp;(".$o['count'].")", '@recherche_resultats?query='.$query.'&facets='.$facet.$facetslink);
+	echo '</li>';
       }
       echo '</ul>';
     }
     echo "</li>";
   }
-} else {
-     $k = array_keys($facets);
-     echo "<li>".$k[0]."&nbsp;(".$facets[$k[0]]['count'].")"."</li>";
-   }
 ?>
 </ul>
 <?php endif;
