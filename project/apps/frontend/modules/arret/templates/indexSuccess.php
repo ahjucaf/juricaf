7019d226493d172395dac08d2f331e9afb2c9a29
Brice Person <brice@ideeslibres.org>
2011-07-15 09:34:16 +0000

    oops oublié
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@270 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/indexSuccess.php
+++ project/apps/frontend/modules/arret/templates/indexSuccess.php
@@ -576,7 +576,7 @@ if (array_key_exists($document->pays, $pays_iso3166)) {
 
   $num = str_replace($urnlex_reserved, "", $document->num_arret);
 
-  $urnlex = strtolower('urn:lex;'.$code_pays_euro[$document->pays].';'.$juridiction.';'.$type.';'.$document->date_arret.';'.$num);
+  $urnlex = strtolower('urn:lex;'.$pays_iso3166[$document->pays].';'.$juridiction.';'.$type.';'.$document->date_arret.';'.$num);
 }
 ?>
   <div class="arret">
