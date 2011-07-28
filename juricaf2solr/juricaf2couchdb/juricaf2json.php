9b62865b6196e1c85c2cde9e3fd082d9aee6a0d8
Brice Person <brice@ideeslibres.org>
2011-07-28 23:07:52 +0000

    Convertisseur champs Juricaf V1 -> V2, champs obligatoires + tests,  mise à la date du jour si erreur, Tribunal fédéral suisse au lieu de Tribunal fédéral, noms des villes françaises en maj et améliorations diverses
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@277 59802511-991d-49c3-8a85-851a450bd420

--- juricaf2solr/juricaf2couchdb/juricaf2json.php
+++ juricaf2solr/juricaf2couchdb/juricaf2json.php
@@ -19,7 +19,7 @@ $mois = array(
         '12'=>'décembre',
         );
 
-// Date pour _id couchdb (Ex : CETATEXT000007604769 et CETATEXT000007602727 : continuité d'une affaire : même numéro, même année, deux décisions)
+// Date pour _id couchdb
 function date_id($d) {
   $d = explode('-', $d);
   $date = $d[0].$d[1].$d[2];
@@ -46,35 +46,56 @@ function cleanArray($array) {
       if(is_object($value)) { $value = (array)$value; } ;
       $array[$key] = cleanArray($value);
     }
-    else { $array[$key] = str_replace(array('<<','>>','<','>'), array('«','»','',''), $value); }
+    else { $array[$key] = trim(str_replace(array('<<','>>','<','>'), array('«','»','',''), $value)); }
   }
   $array = array_filter($array);
   return $array;
 }
 
+// pour convertisseur V1 -> V2
+function extractFormationDate($str, $type = "DECISION") {
+  $str = ucfirst(trim($str));
+  $value = explode(',', $str);
+  if(count($value) == 2) { // si formation, date
+    if(preg_match('/(\d{2})\/(\d{2})\/(\d{4})/', trim($value[1]), $date_dec)) {
+      $date = $date_dec[3]."-".$date_dec[2]."-".$date_dec[1];
+      $extracted = array('type' => $type, 'formation' => trim($value[0]), 'date' => $date);
+    }
+    else {
+      $extracted = array('type' => $type, 'formation' => trim($str, ","));
+    }
+  }
+  else {
+    $extracted = array('type' => $type, 'formation' => $str);
+  }
+  return $extracted;
+}
+
 function addError($str) {
   global $errors, $res;
-  if(!empty($errors)) { $sep = ", "; } else { $sep = ''; }
-  $errors .= $sep.$str;
+  $errors[count($errors)] = $str;
+  sort($errors);
   $res['type'] = 'error_arret';
-  $res['on_error'] = $errors;
+  $res['on_error'] = implode(', ', $errors);
 }
+
 // Chargement
 $obj = simplexml_load_file("data.xml");
 $res = (array)$obj;
 $res = cleanArray($res);
-$res['type'] = 'arret';
 
 if (!count($res)) {
   fprintf(STDERR, 'id":"UNKNOWN","error":"XML parsing","reason":"Cannot parse '.$argv[1]."\"\n");
   exit(33);
 }
 
-if (!isset($res['pays']) && isset($argv[2]))
-  $res['pays'] = $argv[2];
+$res['type'] = 'arret';
 
-if (!isset($res['juridiction']) && isset($argv[3]))
-  $res['juridiction'] = $argv[3];
+if(!isset($res['pays']) && isset($argv[2])) { $res['pays'] = $argv[2]; }
+if(!isset($res['pays'])) { addError("pays manquant"); $res['pays'] = 'inconnu'; }
+
+if(!isset($res['juridiction']) && isset($argv[3])) { $res['juridiction'] = $argv[3]; }
+if(!isset($res['juridiction'])) { addError("juridiction manquante"); $res['juridiction'] = 'inconnue'; }
 
 // Gestion des numéros d'arrets
 if (empty($res['num_arret']))
@@ -106,48 +127,21 @@ if (empty($res['num_arret']))
     $res['num_arret'] = $res['id'];
   }
 }
-//clean them
-unset($res['cat_pub']);
-$res['pays'] = ucfirst(strtolower($res['pays']));
-$res['juridiction'] = ucfirst(strtolower($res['juridiction']));
-$res['formation'] = ucfirst(strtolower($res['formation']));
-if ($res['juridiction'] == $res['formation'] || $res['formation'] == '-' ||
-    strtolower($res['juridiction'].' '.$res['pays'])  == strtolower($res['formation']))
-  unset($res['formation']);
-if ($res['juridiction'] == 'Conseil d-etat')
-  $res['juridiction'] = 'Conseil d\'état';
-if ($res['juridiction'] == 'Cour d-arbitrage')
-  $res['juridiction'] = 'Cour d\'arbitrage';
-if (!isset($res['section']) || $res['section'] == '-')
-  unset($res['section']);
 
-//create extra fields
-if (preg_match('/([0-9][0-9])[\/.]([0-9][0-9])[\/.]([0-9][0-9][0-9][0-9])/', $res['date_arret'], $match))
-{
-  $res['date_arret'] = $match[3].'-'.$match[2].'-'.$match[1];
-  if ($match[3] > date('Y') || $match[3] < 1000) {
-    addError("invalid date");
+if(isset($res['date_arret'])) {
+  if (preg_match('/([0-9][0-9])[\/.]([0-9][0-9])[\/.]([0-9][0-9][0-9][0-9])/', $res['date_arret'], $match))
+  {
+    $res['date_arret'] = $match[3].'-'.$match[2].'-'.$match[1];
+    if ($match[3] > date('Y') || $match[3] < 1000) {
+      addError("date invalide");
+    }
   }
 }
-if (!isset($res['titre']))
-{
-  $formation = '';
-  if (isset($res['formation']))
-    $formation = ', '.$res['formation'];
-  $date = new DateTime($res['date_arret']);
-  $res['titre'] = $res['pays'].', '.$res['juridiction'].$formation.', '.
-             $date->format('d').' '.$mois[$date->format('m')].' '.$date->format('Y').
-             ', décision n°'.$res['num_arret'];
+else {
+  $res['date_arret'] = date('Y-m-d');
+  addError("date manquante");
 }
-$date = date_id($res['date_arret']);
-$num_arret_id = preg_replace('/[^a-z0-9]/i', '', $res['num_arret']);
-$num_arret_id = str_replace(';', '-', $res['num_arret']);
-$res['_id'] = ids($res['pays'].'-'.$res['juridiction'].'-'.$date.'-'.$num_arret_id);
-if (isset($res['id']))
-    $res['juricaf_id'] = $res['id'];
-unset($res['id']);
 
-//Handle errors
 if (strlen($res['num_arret']) > 30)
 {
   addError("num_arret trop gros");
@@ -165,11 +159,158 @@ if (isset($res['texte_arret']) && $res['texte_arret'] && !is_array($res['texte_a
  } else {
   addError("texte de l'arret manquant");
  }
+unset($res['no_error']);
+
+//clean them
+unset($res['cat_pub']);
+$res['pays'] = ucfirst(strtolower($res['pays']));
+$res['juridiction'] = ucfirst(strtolower($res['juridiction']));
+
+// Cas particuliers et noms des villes en majuscules
+if($res['pays'] == 'Suisse' && $res['juridiction'] == 'Tribunal fédéral') { $res['juridiction'] = 'Tribunal fédéral suisse'; }
+
+if($res['pays'] == 'France') {
+  $villes_fr = array(
+      "amiens" => "Amiens",
+      "angers" => "Angers",
+      "auch" => "Auch",
+      "basse-terre" => "Basse-Terre",
+      "bastia" => "Bastia",
+      "besançon" => "Besançon",
+      "bordeaux" => "Bordeaux",
+      "caen" => "Caen",
+      "cayenne" => "Cayenne",
+      "châlons-en-champagne" => "Châlons-en-Champagne",
+      "châlons-sur-marne" => "Châlons-sur-Marne",// nommée ainsi jusqu'en 1995 (et une courte période en 1997) puis est devenue Châlons-en-champagne
+      "clermont-ferrand" => "Clermont-Ferrand",
+      "condom" => "Condom",
+      "dijon" => "Dijon",
+      "douai" => "Douai",
+      "fort-de-france" => "Fort-de-France",
+      "grenoble" => "Grenoble",
+      "illkirch-graffenstaden" => "Illkirch-Graffenstaden",
+      "lille" => "Lille",
+      "limoges" => "Limoges",
+      "lyon" => "Lyon",
+      "marseille" => "Marseille",
+      "montpellier" => "Montpellier",
+      "nancy" => "Nancy",
+      "nantes" => "Nantes",
+      "nice" => "Nice",
+      "nouméa" => "Nouméa",
+      "orléans" => "Orléans",
+      "papeete" => "Papeete",
+      "paris" => "Paris",
+      "pau" => "Pau",
+      "poitiers" => "Poitiers",
+      "rennes" => "Rennes",
+      "rouen" => "Rouen",
+      "saint-denis de la réunion" => "Saint-Denis de la Réunion",
+      "strasbourg" => "Strasbourg",
+      "toulouse" => "Toulouse",
+      "versailles" => "Versailles"
+      );
+  $res['juridiction'] = strtr($res['juridiction'], $villes_fr);
+}
+
+$res['formation'] = ucfirst(strtolower($res['formation']));
+if ($res['juridiction'] == $res['formation'] || $res['formation'] == '-' ||
+    strtolower($res['juridiction'].' '.$res['pays'])  == strtolower($res['formation']))
+  unset($res['formation']);
+if ($res['juridiction'] == 'Conseil d-etat')
+  $res['juridiction'] = 'Conseil d\'État';
+if ($res['juridiction'] == 'Cour d-arbitrage')
+  $res['juridiction'] = 'Cour d\'arbitrage';
+if (!isset($res['section']) || $res['section'] == '-')
+  unset($res['section']);
+
+//create extra fields
+if (!isset($res['titre']))
+{
+  $formation = '';
+  if (isset($res['formation'])) { $formation = ', '.$res['formation']; }
+  $date = new DateTime($res['date_arret']);
+  $res['titre'] = $res['pays'].', '.$res['juridiction'].$formation.', '.$date->format('d').' '.$mois[$date->format('m')].' '.$date->format('Y').', décision n°'.$res['num_arret'];
+}
+$date = date_id($res['date_arret']);
+$num_arret_id = preg_replace('/[^a-z0-9]/i', '', $res['num_arret']);
+$num_arret_id = str_replace(';', '-', $res['num_arret']);
+$res['_id'] = ids($res['pays'].'-'.$res['juridiction'].'-'.$date.'-'.$num_arret_id);
+if (isset($res['id']))
+    $res['juricaf_id'] = $res['id'];
+unset($res['id']);
+
 if (preg_match('/\-\-/', $res['_id'])) {
   fprintf(STDERR, 'id":"UNKNOWN","error":"wrong_id","reason":"Empty id is invalid '.preg_replace('/\n/', '', print_r($res, true))."\"\n");
   exit(32);
 }
-unset($res['no_error']);
+
+/////////// CONVERTISSEUR JURICAF V1 -> V2 ///////////
+
+// REFERENCES
+if(isset($res['references'])) {
+  if(!is_array($res['references'])) {
+    if(strtolower($res['references']) !== 'non') {
+      $references = $res['references'];
+      unset($res['references']);
+      if(!preg_match('/\n/', $references)) {
+        $res['references']['reference'][0] = array('type' => 'ARRET', 'titre' => $references);
+      }
+      else {
+        $res['references']['reference'][0] = array('type' => 'ARRET', 'contenu' => $references);
+      }
+    }
+    else {
+      unset($res['references']);
+    }
+  }
+}
+
+// DECISIONS ATTAQUEES
+//si decision attaquée contient une valeur de type string ou un tableau numérique qui contient des valeurs de type string, c'est un V1
+if(isset($res['decisions_attaquees'])) {
+  if(!is_array($res['decisions_attaquees']['decision_attaquee'])) {  // V1 Unique
+    $v1tov2 = extractFormationDate($res['decisions_attaquees']['decision_attaquee']);
+  }
+  else {
+    if(isset($res['decisions_attaquees']['decision_attaquee'][0]) || isset($res['decisions_attaquees']['decision_attaquee'][1])) {
+      $i = 0;
+      foreach ($res['decisions_attaquees']['decision_attaquee'] as $value)
+      {
+        if(!is_array($value)) { // V1 Multiple
+          $v1tov2[$i] = extractFormationDate($value);
+          $i++;
+        }
+      }
+    }
+  }
+  if(isset($v1tov2)) {
+    $res['decisions_attaquees']['decision_attaquee'] = $v1tov2;
+  }
+}
+
+// PUBLICATIONS
+if(isset($res['publication'])) {
+  if(strtolower($res['publication']) !== 'non') {
+    if(isset($res['references'])) {
+      $i = count($res['references']['reference']);
+      $res['references']['reference'][$i] = array('type' => 'PUBLICATION', 'titre' => $res['publication']);
+    }
+    else {
+      $res['references']['reference'][0] = array('type' => 'PUBLICATION', 'titre' => $res['publication']);
+    }
+    unset($res['publication']);
+  }
+  else {
+    unset($res['publication']);
+  }
+}
+
+ksort($res);
+//debug :
+//echo '<pre>';
+//var_dump($res);
 print json_encode($res);
+//echo '</pre>';
 
 
