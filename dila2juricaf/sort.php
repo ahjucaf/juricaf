3e1b44a01afc340fb9249cc259d22a4232a83933
Brice Person <brice@ideeslibres.org>
2011-08-21 14:34:51 +0000

    Ajout de "de" et "d'" pour harmoniser les juridictions Améliorations diverses : log horodaté, prévision de l'id, gestion des erreurs, section devenu inutile, etc... error_arret pas systématique Utilisation d'un dossier temporaire pour l'extraction des tar.gz de la dila avec archivage Résolution du bug de la boucle du dernier import
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@281 59802511-991d-49c3-8a85-851a450bd420

--- dila2juricaf/sort.php
+++ dila2juricaf/sort.php
@@ -9,6 +9,7 @@ foreach ($to_detar_all as $line) {
 
 if (is_array($to_process)) {
   asort($to_process);
+  $to_update = '';
   foreach ($to_process as $value) {
     $to_update .= $value."\n";
   }
