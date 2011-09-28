dc86c7f16ff14f8d57f0339b9b5e1365e82eed47
Tangui Morlier <tangui@tangui.eu.org>
2011-09-28 18:28:46 +0000

    Le parser des décisions belges (manque la gestion des analyses)
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@344 59802511-991d-49c3-8a85-851a450bd420

---
+++ parsers/ccBelgique/ccBelgique_parser.php
@@ -0,0 +1,76 @@
+<?php
+
+$content = file_get_contents('php://stdin');
+$mois = array('JANVIER' => '01', 'FEVRIER' => '02', 'MARS' => '03', 'AVRIL'=>'04', 'MAI'=>'05', 'JUIN'=>'06', 'JUILLET'=>'07', 'AOUT'=>'08', 'SEPTEMBRE'=>'09', 'OCTOBRE'=>'10', 'NOVEMBRE'=>'11', 'DECEMBRE'=>'12');
+
+if (!preg_match('/arret/', $content)) {
+  error_log("ERROR: Arret non Francais");
+  exit(1) ;
+ }
+
+if (!preg_match('/cour de cassation/i', $content)) {
+  error_log("ERROR: Arret non Francais");
+  exit(1) ;
+ }
+
+$num = '';
+if (preg_match('/NDEG\.?\s*([A-Z][^A-Z][^-\n]*)/', $content, $match)) {
+  $num = preg_replace('/ /', '', $match[1]);
+ }
+
+if (!$num && preg_match('/([A-Z]\.[\d\.]+\.[A-Z])\/\d+\s+$/', $content, $match)) {
+  $num = preg_replace('/ /', '', $match[1]);
+ }
+
+$num = preg_replace('/[^A-Z\d]/', '', $num);
+
+if ($num && preg_match('/([A-Z])(\d{2})(\d+)([A-Z])/', $num, $match)) {
+  $num = $match[1].'.'.sprintf('%02d', $match[2]).'.'.sprintf('%04d', $match[3]).'.'.$match[4];
+ }else{
+  error_log("ERROR: Numéro non trouvé $num");
+  exit(1) ;
+ }
+
+$date = '';
+if (preg_match('/(\d+)[eE]?[rR]?[\s\.]+(\w+)[\s\.]+(\d+)\s+(('.$num.'|[A-Z][\d\.]+\.[A-Z][\/\-])|\s*$)/', $content, $match)) {
+  $date = $match[3].'-'.$mois[strtoupper($match[2])].'-'.sprintf('%02d', $match[1]);
+ }
+
+if (!$date || !preg_match('/\d{4}\-\d{2}\-\d{2}/', $date)) {
+  error_log("ERROR: Date non trouvée ou mal formée $date");
+  exit(1);
+ }
+
+$analyses = array();
+if (preg_match_all('/(\d+|Cour de cassation|Arret)/', $content, $match)) {
+  foreach ($match[0] as $m) {
+    if (!preg_match('/\d/', $m))
+      break;
+    $analyses[] = $m;
+  }
+ }
+
+$text = "\n"; $header = 1;
+foreach (split("\n", $content) as $ligne) {
+  if ($header && !preg_match('/Cour de cassation/', $ligne))
+    continue;
+  $header = 0;
+  $text .= preg_replace(array('/\`a/', '/<</', '/>>/'), array('a', '«', '»'), $ligne)."\n";
+}
+
+echo "<?xml version=\"1.0\"?>
+<DOCUMENT>
+  <PAYS>Belgique</PAYS>
+  <JURIDICTION>Cour de cassation</JURIDICTION>
+  <NUM_ARRET>$num</NUM_ARRET>
+  <DATE_ARRET>$date</DATE_ARRET>
+  <ANALYSES>\n";
+foreach ($analyses as $a) {
+  echo "    <ANALYSE>
+      <SOMMAIRE>$a</SOMMAIRE>
+    </ANALYSE>\n";
+}
+  echo "  </ANALYSES>
+  <TEXTE_ARRET>$text</TEXTE_ARRET>
+</DOCUMENT>
+";
