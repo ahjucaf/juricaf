ef1b6177ea846377f567bfe949892e4d4422ef9f
Tangui Morlier <tangui@tangui.eu.org>
2011-10-11 22:23:33 +0000

    Lien facet avaient changé lors de la réindexation
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@372 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
+++ project/apps/frontend/modules/arret/templates/_statsPaysJuridiction.php
@@ -13,14 +13,14 @@ function pathToFlag($str) {
 foreach ($pays as $p => $juridictions)
 {
   $no_blank_p = replaceBlank($p);
-  echo '<li style="list-style-image: url(/images/drapeaux/'.pathToFlag(ucfirst($no_blank_p)).'.png)"><strong>'.link_to($p,'recherche/search?query=+&facets=pays:'.$no_blank_p).'</strong><ul class="subul">';
+  echo '<li style="list-style-image: url(/images/drapeaux/'.pathToFlag(ucfirst($no_blank_p)).'.png)"><strong>'.link_to($p,'recherche/search?query=+&facet_facets=pays:'.$no_blank_p).'</strong><ul class="subul">';
   foreach ($juridictions as $j => $v) {
     $pluriel = '';
     if ($v['value'] > 1)
       $pluriel = 's';
     if($v['deb'] == $v['fin']) { $periode = "décision$pluriel de ".$v['deb']; }
     else { $periode = "décisions de ".$v['deb'].' à '.$v['fin']; }
-    echo '<li>'.link_to($j.' ('.number_format($v['value'], 0, ',', ' ').' '.$periode.')', 'recherche/search?query=+&facets=pays:'.$no_blank_p.',juridiction:'.replaceBlank($j)).'</li>';
+    echo '<li>'.link_to($j.' ('.number_format($v['value'], 0, ',', ' ').' '.$periode.')', 'recherche/search?query=+&facets=facet_pays:'.$no_blank_p.',facet_juridiction:'.replaceBlank($j)).'</li>';
   }
   echo "</ul></li>";
 }?></ul>
