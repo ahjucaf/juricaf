d0481fb443362179f0b516984d665132c0e5b470
Tangui Morlier <tangui@tangui.eu.org>
2011-10-14 23:25:53 +0000

    Cas des arrets avec des &
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@383 59802511-991d-49c3-8a85-851a450bd420

--- parsers/ccBelgique/ccBelgique_parser.php
+++ parsers/ccBelgique/ccBelgique_parser.php
@@ -3,6 +3,9 @@
 include('config/analyses.php');
 
 $content = file_get_contents('php://stdin');
+
+$content = preg_replace('/\&/', '&amp;', $content);
+
 $mois = array('JANVIER' => '01', 'FEVRIER' => '02', 'MARS' => '03', 'AVRIL'=>'04', 'MAI'=>'05', 'JUIN'=>'06', 'JUILLET'=>'07', 'AOUT'=>'08', 'SEPTEMBRE'=>'09', 'OCTOBRE'=>'10', 'NOVEMBRE'=>'11', 'DECEMBRE'=>'12');
 
 if (!preg_match('/arret/', $content)) {
