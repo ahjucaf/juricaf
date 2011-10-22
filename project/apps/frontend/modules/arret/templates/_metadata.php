1c1ba40a7e0ac533e529664a89f046a2891e8624
Brice Person <brice@ideeslibres.org>
2011-10-22 13:50:53 +0000

    End tag
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@400 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_metadata.php
+++ project/apps/frontend/modules/arret/templates/_metadata.php
@@ -7,8 +7,8 @@
 <meta name="DC.identifier" scheme="DCTERMS.URI" content="<?php echo $dc_identifier_uri; ?>" />
 <?php } ?>
     <?php if(!empty($pays)) { ?>
-<link rel="alternate" type="application/rss+xml" href="/recherche/+/facet_pays%3A<?php echo replaceBlank($pays); ?>?format=rss" title="Collection <?php echo $pays; ?>">
+<link rel="alternate" type="application/rss+xml" href="/recherche/+/facet_pays%3A<?php echo replaceBlank($pays); ?>?format=rss" title="Collection <?php echo $pays; ?>" />
 <?php } ?>
     <?php if(!empty($pays) && !empty($juridiction)) { ?>
-<link rel="alternate" type="application/rss+xml" href="/recherche/+/facet_pays_juridiction%3A<?php echo replaceBlank($pays); ?>_%7C_<?php echo replaceBlank($juridiction); ?>%2Cfacet_pays%3A<?php echo replaceBlank($pays); ?>?format=rss" title="Collection <?php echo $pays.' | '.$juridiction; ?>">
+<link rel="alternate" type="application/rss+xml" href="/recherche/+/facet_pays_juridiction%3A<?php echo replaceBlank($pays); ?>_%7C_<?php echo replaceBlank($juridiction); ?>%2Cfacet_pays%3A<?php echo replaceBlank($pays); ?>?format=rss" title="Collection <?php echo $pays.' | '.$juridiction; ?>" />
 <?php } ?>
