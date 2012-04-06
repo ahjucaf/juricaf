294fd957d7d3955cbcee20a2a460b5ad409d949c
Brice Person <brice@ideeslibres.org>
2012-04-06 13:03:00 +0000

    Parseur complet pour la Cour suprême du Canada Téléchargeur, convertisseur et mises à jour automatiques
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@439 59802511-991d-49c3-8a85-851a450bd420

---
+++ parsers/Canada/CSCLexum/extractFileList.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf8 -*-
+import sys, re
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open(sys.argv[1], 'r'))
+
+for link in soup.find_all(href=re.compile("/fr/[0-9]{4}/")):
+  line = 'http://csc.lexum.org'+link.get('href')
+  print(line.encode('utf8'))
