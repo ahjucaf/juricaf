4cf7bd12c81b7bb8121735203776f33b14b1dcc7
Guillaume Adreani <guillaume.adreani@gmail.com>
2012-10-05 13:37:53 +0000

    Mise à jour étudiants EPITA
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@500 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -13,16 +13,28 @@ function pathToFlag($str) {
 
 $cpt = 0;
 ?>
-<div class="payscols">
-  <p style='text-align: center;'>Rechercher parmi <?php echo num($nb); ?> décisions provenant de <?php echo count($pays); ?> pays francophones :</p>
-<table><tr>
-<?php
-foreach ($pays as $p)
-{
-  $pays = preg_replace('/ /', '_', $p['key'][0]);
-  if (++$cpt%2 && $cpt !== 1) { echo '</tr><tr>'; }
-  echo '<td><img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.link_to($p['key'][0].' ('.num($p['value']).')','recherche/search?query=+&facets=facet_pays:'.$pays).'</td>';
-}
-?>
-</tr><tr><td colspan="2" class="plus"><a href="/documentation/stats/statuts.php">Plus de statistiques</a></td></tr></table>
+<div class="pays">
+	<h3>Rechercher parmi <?php echo num($nb); ?> décisions provenant de <?php echo count($pays); ?> pays francophones</h3>
+	<div class="payscols">
+	  <table><tr>
+		<?php
+		foreach ($pays as $p)
+		{
+		  $pays = preg_replace('/ /', '_', $p['key'][0]);
+		  if ($cpt % 4 == 0) { echo '</tr><tr>'; }
+		  $cpt++;
+		  
+		  // Traitement du nom du pays : si trop long, le nom est coupé
+		  $pays_nom = $p['key'][0].' ('.num($p['value']).')';
+		  $link = link_to($pays_nom,'recherche/search?query=+&facets=facet_pays:'.$pays);
+		  if (strlen($pays_nom) > 18) {
+			  $pays_nom_min = substr($pays_nom, 0, 16) . '...';
+			  $link = link_to($pays_nom_min,'recherche/search?query=+&facets=facet_pays:'.$pays);
+			  $link = str_replace("<a ", "<a title=\"$pays_nom\" ", $link ); // Ajout du titre pour que popup du nom entier apparaisse
+		  }
+		  echo '<td><img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.$link .'</td>';
+		}
+		?>
+		</tr><tr><td colspan="4" class="plus"><img src="images/+.png" alt="+" /> <a href="/documentation/stats/statuts.php">Plus de statistiques</a></td></tr></table>
+	</div>
 </div>
