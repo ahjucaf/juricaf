db5cd8f1ebceef7087a9aef1a4b87282f7249949
Guillaume Adreani <guillaume.adreani@gmail.com>
2012-11-14 15:11:26 +0000

    Livraison séries modifications mineures
    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@518 59802511-991d-49c3-8a85-851a450bd420

--- project/web/documentation/stats/statuts.php
+++ project/web/documentation/stats/statuts.php
@@ -45,58 +45,56 @@
     </style>
   </head>
   <body>
-    <div class="site">
+
 
       <div class="head">
-        <div class="reseaux_sociaux">
-          <a href="https://www.facebook.com/Juricaf"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook" /></a>
-          <a href="https://twitter.com/#!/juricaf"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter" /></a>
-          <a href="http://www.juricaf.org/documentation/spip.php?page=distrib"><img src="/images/rss.png" alt="RSS" title="Flux RSS" /></a>
-          <a href="javascript:juricafSearch();"><img src="/images/juricaf_search.png" alt="J" title="Ajouter Juricaf aux moteurs de recherches de votre navigateur" /></a>
-        </div>
-        <div class="menu">
-
-           <ul>
-            <li><a href="/documentation/presentation-generale-38/article/a-propos">A propos</a></li>
-            <li><a href="/documentation/stats/statuts.php">Étendue des collections</a></li>
-            <li><a href="/documentation/presentation-generale-38/article/partenaires">Partenaires</a></li>
-            <li><a href="/documentation/mentions-legales/">Mentions légales</a></li>
-			<li><a href="/documentation/fonds-documentaire/">Fonds documentaire</a></li>
-			<li><a href="/documentation/aide/">Aide</a></li>
+                <div class="menu">
+         		  <ul>
+			<li><a href="http://www.juricaf.org/documentation/presentation-generale-38/article/a-propos">A propos</a></li>
+			<li><a href="http://www.juricaf.org/documentation/stats/statuts.php">Étendue des collections</a></li>
+			<li><a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires">Partenaires</a></li>
+			<li><a href="http://www.juricaf.org//documentation/mentions-legales/">Mentions légales</a></li>
+			<li><a href="http://www.juricaf.org//documentation/fonds-documentaire/">Fonds documentaire</a></li>
 			<li><a href="/documentation/auteur/secretariat-general-de-l-ahjucaf">Contact</a></li>
-          </ul>
-        </div>
+			</ul>
+		    </div>
+			    <div class="site">
+			  <div class="reseaux_sociaux">
+          <a href="https://www.facebook.com/Juricaf" target="_blank"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook"/></a>
+          <a href="http://twitter.com/juricaf" target="_blank"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter"/></a>
+          <a href="http://www.juricaf.org/documentation/spip.php?page=backend"><img src="/images/rss.png" alt="RSS" title="Flux RSS" /></a>
+          <a href="javascript:juricafSearch();"><img src="/images/juricaf_search.png" alt="J" title="Ajouter Juricaf aux moteurs de recherches de votre navigateur" /></a>
       </div>
+   
       <div class="main">
         <div class="content">
-          <div class="form_recherche">
+         <div class="form_recherche">
             <form method="get" action="/recherche">
             <table summary="Rechercher">
               <tr>
-                <td>
+                <td align="center" width="1024px" height="250px">
                   <a href="/recherche"><img id="logo" style="width: 100px; height: 100px;" src="/images/juricaf.png" alt="Juricaf" /></a><br />
-                  <img src="/images/slogan.png" style="width: 276px; height: 12px; margin-top: 4px;" alt="La jurisprudence francophone des cours suprêmes" /><br />
-                  <input type="text" style="width: 300px; margin-top: 2px;" name="q" value="" tabindex="10" /><br />
+                  <img src="/images/slogan.png" style="width: 276px; height: 12px; margin-top: 4px;" alt="La jurisprudence francophone des cours suprêmes" itemprop="image" /><br />
+                  <input type="text" name="q" value=" " tabindex="10" /><br />
                   <input type="submit" value="Rechercher" tabindex="20" /> <a href="/recherche_avancee">recherche avancée</a>
                 </td>
               </tr>
             </table>
             </form>
           </div>
-          <hr />
           <h1>Statuts et licences des collections</h1>
           <?php include('base.html'); ?>
           <div style="text-align: right;"><a id="top" href="#">Haut de page</a></div>
         </div>
       </div>
-      <a href="documentation/presentation-generale-38/article/partenaires"><img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a>
-	  <a href="documentation/presentation-generale-38/article/partenaires"><img src="../local/cache-vignettes/L202xH60/lnlilogopetit-22b81.jpg" alt="LNLI" /></a>
-	  <a href="documentation/presentation-generale-38/article/partenaires"><img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a>
+      <a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires"><img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a>
+	  <a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires"><img src="http://www.juricaf.org/documentation/local/cache-vignettes/L202xH60/lnlilogopetit-22b81.jpg" alt="LNLI" /></a>
+	  <a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires"><img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a>
       <div class="bottom">
-        <p>Juricaf est un projet de l'<a href="documentation/presentation-generale-38/article/partenaires">AHJUCAF</a>, l'association des cours judiciaires suprêmes francophones,<br />
-        réalisé en partenariat avec <a href="documentation/presentation-generale-38/article/partenaires">le Laboratoire Normologie Linguistique et Informatique du droit (Université&nbsp;Paris&nbsp;I).</a><br />
-        Il est soutenu par l'<a href="documentation/presentation-generale-38/article/partenaires">Organisation internationale de la Francophonie</a>,
-        le <a href="documentation/presentation-generale-38/article/partenaires">Fonds francophone des inforoutes</a>.</p>
+        <p>Juricaf est un projet de l'<a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires">AHJUCAF</a>, l'association des cours judiciaires suprêmes francophones,<br />
+        réalisé en partenariat avec <a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires">le Laboratoire Normologie Linguistique et Informatique du droit (Université&nbsp;Paris&nbsp;I).</a><br />
+        Il est soutenu par l'<a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires">Organisation internationale de la Francophonie</a>,
+        le <a href="http://www.juricaf.org/documentation/presentation-generale-38/article/partenaires">Fonds francophone des inforoutes</a>.</p>
       </div>
     </div>
     <script type="text/javascript">
@@ -123,6 +121,6 @@
     })();
     // -->
     </script>
-
+   </div>
   </body>
 </html>
