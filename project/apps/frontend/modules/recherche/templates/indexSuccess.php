4ad8dbece5eccb71775858e2164ad1b93f27cacb
Guillaume Adreani <guillaume.adreani@gmail.com>
2012-11-14 14:25:57 +0000

    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@514 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,12 +1,12 @@
 ﻿<div class="stats">
 <?php include_component('arret', 'statsPays'); ?>
-	<div class="prix">
+	<div class="pays"><h3>Récompenses</h3>
 	<center><h4><a href="http://www.i-expo.net/fr/prix" target="_blank"><img src="images/iexpo.jpg" />
 	<p>Lauréat du prix I-Expo 2012 <br>catégorie "Open Data </a>
 	<p><a href="http://iall.org/webAward.html" target="_blank"><img src="images/iall.jpg" /></a></h4></center>
 	</div>
-	<div class="rss">
-		<h3><a href="/documentation/spip.php?page=distrib">Flux RSS </a><img src="images/reseaux_sociaux/rss_ico.png" /></h3>
+	<div class="pays">
+		<h3>Actualités<a href="/documentation/spip.php?page=distrib"><img src="images/reseaux_sociaux/rss_ico.png" vertical-align="middle"/></a></h3>
 		<div class="rsscols">
 <script type="text/javascript" 	src="http://dev.juricaf.org/documentation/spip.php?page=distrib"></script>
 		</div>
