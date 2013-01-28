c8fdb92e454ccee0014f85fcff2ab295698b6a6a
Guillaume Adreani <guillaume.adreani@gmail.com>
2013-01-28 11:08:40 +0000

    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@536 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,14 +1,14 @@
-﻿<div class="stats">
+<div class="stats">
 <?php include_component('arret', 'statsPays'); ?>
 	<div class="pays"><h3>Récompenses</h3>
-	<center><h4><a href="http://www.i-expo.net/fr/prix" target="_blank"><img src="images/iexpo.jpg" />
-	<p>Lauréat du prix I-Expo 2012 <br>catégorie "Open Data </a>
+	<center><h4><img src="images/iexpo.jpg" />
+	<p>Lauréat du prix I-Expo 2012 <br>catégorie "Open Data"
 	<p><a href="http://iall.org/webAward.html" target="_blank"><img src="images/iall.jpg" /></a></h4></center>
 	</div>
 	<div class="pays">
-		<h3>Actualités<a href="http://www.juricaf.org/documentation/spip.php?page=backend"><img src="images/reseaux_sociaux/rss_ico.png" vertical-align="middle"/></a></h3>
+		<h3>Actualités<a href="http://www.juricaf.org/documentation/spip.php?page=backend"><img src="http://www.juricaf.org/images/reseaux_sociaux/rss_ico.png" vertical-align="middle"/></a></h3>
 		<div class="rsscols">
-<script type="text/javascript" 	src="http://dev.juricaf.org/documentation/spip.php?page=distrib"></script>
+<script type="text/javascript" 	src="http://www.juricaf.org/documentation/spip.php?page=distrib"></script>
 		</div>
 	</div>
 	<div style="clear:both;">&nbsp;</div>
