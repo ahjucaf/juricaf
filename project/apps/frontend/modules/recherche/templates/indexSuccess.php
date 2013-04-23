62dca713dddd760e10db724806dbbc410fabb839
Guillaume Adreani <guillaume.adreani@gmail.com>
2013-04-23 13:19:18 +0000

    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@544 59802511-991d-49c3-8a85-851a450bd420

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,15 +1,9 @@
 <div class="stats">
 <?php include_component('arret', 'statsPays'); ?>
-	<div class="pays"><h3>Récompenses</h3>
+	<div class="pays">
 	<center><h4><img src="images/iexpo.jpg" />
 	<p>Lauréat du prix I-Expo 2012 <br>catégorie "Open Data"
 	<p><a href="http://iall.org/webAward.html" target="_blank"><img src="images/iall.jpg" /></a></h4></center>
-	</div>
-	<div class="pays">
-		<h3>Actualités<a href="http://www.juricaf.org/documentation/spip.php?page=backend"><img src="http://www.juricaf.org/images/reseaux_sociaux/rss_ico.png" vertical-align="middle"/></a></h3>
-		<div class="rsscols">
-<script type="text/javascript" 	src="http://www.juricaf.org/documentation/spip.php?page=distrib"></script>
-		</div>
 	</div>
 	<div style="clear:both;">&nbsp;</div>
 </div>
