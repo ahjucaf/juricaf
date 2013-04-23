25d5815af4c986419d001f456c162e7b80d8ae1d
Guillaume Adreani <guillaume.adreani@gmail.com>
2013-04-23 13:21:38 +0000

    git-svn-id: file:///home/ahjucaf/svn/ahjucaf/trunk@547 59802511-991d-49c3-8a85-851a450bd420

---
+++ project/web/documentation/stats/statuts.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="title" content="Juricaf" />
+<meta name="description" content="La jurisprudence francophone des cours suprêmes" />
+<meta name="keywords" content="jurisprudence, cassation, cour, suprême, francophone, francophonie, ahjucaf, arrêt, décision" />
+<meta name="language" content="fr" />
+    <title>Statuts et licences des collections - Juricaf</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+
+<link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/jquery-ui-1.8.16.custom.css" />
+    <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="/js/jquery.scrollTo-min.js"></script>
+<script type="text/javascript" src="/js/jquery-ui-1.8.16.custom.min.js"></script>
+    <link rel="search" href="/juricaf.xml" title="Rechercher sur Juricaf" type="application/opensearchdescription+xml" />
+    <style type="text/css">
+    .statsbase {
+      border-collapse: collapse;
+      border: 1px solid #EDEDED;
+    }
+    .statsbase td, .statsbase th {
+      border-collapse: collapse;
+      padding-left: 10px;
+      padding-right: 10px;
+      border : 1px solid #EDEDED;
+    }
+    .statsbase tr:hover {
+      background-color: #F2F2F2;
+    }
+    .num {
+      text-align: right;
+    }
+    .color1 {
+      background-color: #F9F9F9;
+    }
+    .color2 {
+      background-color: #fff;
+    }
+    .stat_legend {
+      cursor: help;
+    }
+    </style>
+  </head>
+  <body>
+    <div class="site">
+
+      <div class="head">
+       <div class="menu">
+          <ul>
+            <li><a href="http://www.juricaf.org/documentation/a_propos.php">A propos</a></li>
+            <li><a href="http://www.juricaf.org/documentation/stats/statuts.php">Etendue des collections</a></li>
+            <li><a href="http://www.juricaf.org/documentation/partenaires.php">Partenaires</a></li>
+            <li><a href="http://www.juricaf.org/documentation/mentions_legales.php">Mentions légales</a></li>
+	    <li><a href="http://www.juricaf.org/documentation/contact.php">Contact</a></li>
+			
+            </ul>
+        </div>
+      </div>
+	  <div class="reseaux_sociaux">
+
+	   <a href="https://www.facebook.com/Juricaf" target="_blank"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook"/></a>
+          <a href="http://twitter.com/juricaf" target="_blank"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter"/></a>
+	<a href="https://itunes.apple.com/fr/app/id587420315?mt=8&affId=1578782" target="_blank"><img src="/images/appstore.png" alt="Appstore" title="Accs  la version IPhone"/></a>
+      </div>
+      <div class="main">
+        <div class="content">
+          <div class="form_recherche">
+            <form method="get" action="/recherche">
+            <table summary="Rechercher">
+              <tr>
+                <td>
+                  <a href="/recherche"><img id="logo" style="width: 100px; height: 100px;" src="/images/juricaf.png" alt="Juricaf" /></a><br />
+                  <img src="/images/slogan.png" style="width: 276px; height: 12px; margin-top: 4px;" alt="La jurisprudence francophone des cours suprêmes" /><br />
+                  <input type="text" style="width: 300px; margin-top: 2px;" name="q" value="" tabindex="10" /><br />
+                  <input type="submit" value="Rechercher" tabindex="20" /> <a href="/recherche_avancee">recherche avancée</a>
+                </td>
+              </tr>
+            </table>
+            </form>
+          </div>
+          <hr />
+          <h1>Statuts et licences des collections</h1>
+          <?php include('base.html'); ?>
+          <div style="text-align: right;"><a id="top" href="#">Haut de page</a></div>
+        </div>
+      </div>
+ <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" />
+	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+      <div class="bottom">
+        <p>Juricaf est un projet de l'AHJUCAF, l'association des cours judiciaires suprêmes francophones,<br />
+        réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I).<br />
+        Il est soutenu par l'Organisation internationale de la Francophonie et le Fonds francophone des inforoutes.</p>
+      </div>
+
+    </div>
+    <script type="text/javascript">
+    <!--
+    function juricafSearch() {
+      if (window.external && ("AddSearchProvider" in window.external)) {
+        window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
+      }
+      else {
+         alert("Votre navigateur ne supporte pas cette fonctionnalité");
+      }
+    }
+
+    $('#top').attr('href', 'javascript:$.scrollTo("body", 500);');
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-8802834-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    // -->
+    </script>
+
+  </body>
+</html>
