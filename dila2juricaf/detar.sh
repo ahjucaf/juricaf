cd193b06b10b8810ecd3287b69418b7502d45195
Tangui Morlier <tangui@tangui.eu.org>
2017-12-12 21:11:37 +0100

    Plus de simplification & messages d'erreur

--- dila2juricaf/detar.sh
+++ dila2juricaf/detar.sh
@@ -71,27 +71,26 @@ for fichier in $(cat $TOPROCESS);
 
     DELETEDJSONDIR=$DELETED"/"$(date +%Y-%m-%d);
     mkdir -p $DELETEDJSONDIR
-    find $DATA/ -name "*.dat" -exec cat '{}' ';' | sort -u | while read linetobedeleted ; do 
-        ID_DILA=$(echo "$linetobedeleted" | sed 's:.*/::')
+    find ../data/ -name "*.dat" -exec cat '{}' ';' | sort -u | while read deleteorder ; do
+        ID_DILA=$(echo "$deleteorder" | sed 's:.*/::')
         ID_JURICAF=$(php getIdFromSolr.php $ID_DILA)
         # Si le résultat de l'interrogation solr contient un espace c'est une erreur donc on l'affiche
         if echo $ID_JURICAF | grep " "  > /dev/null; then
-          echo "Erreur : ID CouchDB non conforme : $ID_JURICAF ("$ID_DILA" <= "$linetobedeleted")" 
+          echo "Erreur : ID CouchDB non conforme : $ID_JURICAF ("$ID_DILA" <= "$deleteorder")"
         else
           # Le document existe dans solr, on interroge couchdb
-          DOC_COUCHDB=$(curl -s GET $COUCHDBURL/$ID_JURICAF) ;
           # Si le document n'existe pas dans couchdb
-          if echo $DOC_COUCHDB | grep "not_found" > /dev/null; then
+          if curl -s $COUCHDBURL/$ID_JURICAF | grep "not_found" > /dev/null; then
             echo "Erreur : $ID_JURICAF ($ID_DILA) existe dans solr mais pas dans couchdb"
           # S'il existe
           else
           # Suppression
-            curl --stderr /dev/null $COUCHDBURL/$ID_JURICAF > $DELETEDJSONDIR"/"$ID_JURICAF
-            REV=$(cat $DELETEDJSONDIR"/"$ID_JURICAF | sed 's/.*_rev":"//' | sed 's/",".*//' 2> /dev/null)
-            if curl -X DELETE $COUCHDBURL/$ID_JURICAF?rev=$REV | grep -v $ID_JURICAF > /dev/null; then
-              echo "Erreur : $ID_JURICAF ($ID_DILA) existe dans solr et couchdb mais n'a pas pu en être supprimé" 
+            curl -s $COUCHDBURL/$ID_JURICAF > $DELETEDJSONDIR"/"$ID_JURICAF
+            REV=$(cat $DELETEDJSONDIR"/"$ID_JURICAF | sed 's/.*_rev":"//' | sed 's/",".*//')
+            if curl -s -X DELETE $COUCHDBURL/$ID_JURICAF?rev=$REV | grep -v $ID_JURICAF > /dev/null; then
+              echo "Erreur : $ID_JURICAF ($ID_DILA) existe dans solr et couchdb mais n'a pas pu en être supprimé"
             else
-              echo "Info : $ID_JURICAF ($ID_DILA) a été supprimé de couchdb avec succès" 
+              echo "Info : $ID_JURICAF ($ID_DILA) a été supprimé de couchdb avec succès"
             fi
           fi
         fi
