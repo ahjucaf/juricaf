4ff63eba0ac3a42d666eb09e8fedf6e7c266fb19
Tangui Morlier <tangui@tangui.eu.org>
2018-01-15 17:48:29 +0100

    Script de replication

---
+++ juricaf2solr/bin/replicate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. $(dirname $0)"/../conf/juricaf.conf"
+
+if ! test  "$COUCHDISTANT" ; then
+	echo "ERROR: COUCHDISTANT non configué dans juricaf.conf" >&2
+	exit 2;
+fi
+
+REV=$(curl -s "http://10.80.75.2:5984/_replicator/REPLICATION_PROD" | sed 's/.*_rev":"//' | sed 's/{*".*//')
+if test "$REV" ; then
+	curl -s -X DELETE  "http://127.0.0.1:5984/_replicator/REPLICATION_PROD?rev="$REV
+fi
+
+curl -s -X PUT -d '{"_id": "REPLICATION_PROD", "source": "'$COUCHDISTANT'", "target": "'$COUCHDBURL'", "continuous": true}' http://127.0.0.1:5984/_replicator/REPLICATION_PROD
