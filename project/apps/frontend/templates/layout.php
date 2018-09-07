07ca34c65ae1c012dc06628f49892c8104550091
bidarddelanoe <40470907+bidarddelanoe@users.noreply.github.com>
2018-09-07 14:04:00 +0200

    Modification de la page d'accueil

--- project/apps/frontend/templates/layout.php
+++ project/apps/frontend/templates/layout.php
@@ -61,7 +61,8 @@
 		  <div class="top_page"><a id="top" href="#">Haut de page<img src="/images/fleche_haut.png" alt="Haut de page"/></a></div>
         </div>
       </div>
-	 <a href="ahjucaf.org"><img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a>
+	 <a href="http://www.ahjucaf.org/"><img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a>
+	    <a href="http://www.ahjucaf.org/"><img style="margin-left: 10px;" src="/images/ahjucafSite.jpg" alt="Association des cours judiciaires suprêmes francophones" />Visietez le nouveau site de l'AHJUCAF</a>
 	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
       <div class="bottom">
         <p>Juricaf est un projet de l'AHJUCAF, l'association des cours judiciaires suprêmes francophones,<br />
