ef5e447afe5d27f58ab6efe2e3b46a85af5a5bec
Tangui Morlier <tangui@tangui.eu.org>
2019-04-15 18:25:22 +0200

    Usage de la commande qui permet le parsing d'un pdf

--- parsers/luxembourg/parse_all.sh
+++ parsers/luxembourg/parse_all.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-mkdir -p tmp
 find docs/justice.public.lu -name '*pdf' | while read pdf ; do
-    pdftohtml $pdf tmp/arret > /dev/null 2>&1
-    xmlfile=$(echo $pdf | sed 's/\.pdf/.xml/')
     echo -n $pdf";"
-    cat tmp/arrets.html | php parser_pdftohtml.php $xmlfile
+    bash parse_pdf.sh $pdf
     echo
 done | grep pdf
-rm -rf tmp
+rm -rf .tmp
