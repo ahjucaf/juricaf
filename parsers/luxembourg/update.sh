778c7bdee39996cebb77655ff738cb47253c638c
Tangui Morlier <tangui@tangui.eu.org>
2019-04-17 11:57:07 +0200

    Plus d'info sur les fichiers (comme la source)

--- parsers/luxembourg/update.sh
+++ parsers/luxembourg/update.sh
@@ -5,8 +5,10 @@ cd $(dirname $0)
 mkdir -p $POOL_DIR
 
 bash pdf_downloader.sh | while read pdf ; do
+    echo -n $pdf";";
     bash parse_pdf.sh $pdf
-done | awk -F ';' '{print $7}' | while read xml ; do
+    echo
+done | awk -F ';' '{print $8}' | grep 'xml$' | while read xml ; do
     if test -f $xml ; then
         cp $xml $POOL_DIR
     fi
