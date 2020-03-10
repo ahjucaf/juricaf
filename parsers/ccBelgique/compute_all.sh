a7bcbfd64bf70cd269614221ddc82b0159d0d967
Tangui Morlier <tangui@tangui.eu.org>
2020-03-10 16:20:18 +0100

    Récupération de tous les arrêts de 2010

--- parsers/ccBelgique/compute_all.sh
+++ parsers/ccBelgique/compute_all.sh
@@ -3,7 +3,7 @@
 mydir=$(echo $0 | sed 's/[^\/]*$//')
 if test $mydir ; then cd $mydir ; fi
 
-. config/config.inc
+. config/config.sh
 
-bash juridat_list.sh
+bash juridat_list.sh 2010-01-01
 grep -l 'dec_juridiction>1<' documents/*.xml  | while read xml ; do bash ccBelgique_parser.sh $xml || echo "ERROR $xml ^ "; done
