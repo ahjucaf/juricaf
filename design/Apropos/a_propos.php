f92b3af7f81ece7c47acdc3c55395fd11292919b
Honorine Marie Christy <honorine@24eme.fr>
2020-05-26 17:00:43 +0200

    A propos test avec php

---
+++ design/Apropos/a_propos.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Juricaf" />
+    <meta name="description" content="La base de données de jurisprudence francophone" />
+    <meta name="keywords" content="jurisprudence, cassation, cour, suprême, francophone, francophonie, ahjucaf, arrêt, décision" />
+    <meta name="language" content="fr" />
+    <title>Juricaf - A propos</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+    <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.scrollTo-min.js"></script>
+    <link rel="search" href="/juricaf.xml" title="Rechercher sur Juricaf" type="application/opensearchdescription+xml" />
+  </head>
+  <body>
+    <div class="site">
+      <div class="head">
+
+
+
+        <div class="menu_bar" > <!--26/05-->
+          <a onclick="openMenu()"><i id="icon_menu"></i></a>
+          <h1 id="headbar">JURICAF.ORG</h1>
+            <img id="menu_logo" src="Juricaf%20-%20A%20propos_fichiers/img/juricaf.png"> <!--Ajout d'image-->
+        </div>
+
+
+        <div class="menu" id="menu"> <!--26/05-->
+          <ul>
+            <li><a href="http://www.juricaf.org/documentation/a_propos.php">À PROPOS</a></li><!--26/05-->
+            <li><a href="http://www.juricaf.org/documentation/stats/statuts.php">ÉTENDUE DES COLLECTIONS</a></li><!--26/05-->
+            <li><a href="http://www.juricaf.org/documentation/partenaires.php">PARTENAIRES</a></li><!--26/05-->
+            <li><a href="http://www.juricaf.org/documentation/mentions_legales.php">MENTIONS LEGALES</a></li><!--26/05-->
+	    <li><a href="http://www.juricaf.org/documentation/contact.php">CONTACT</a></li><!--26/05-->
+
+            </ul>
+        </div>
+      </div>
+	  <div class="reseaux_sociaux">
+
+	   <a href="https://www.facebook.com/Juricaf" target="_blank"><img src="Juricaf%20-%20A%20propos_fichiers/img/icon/logo_facebook.png" alt="Facebook" title="Devenez fan sur Facebook"/></a><!--26/05-->
+     <a href="http://twitter.com/juricaf" target="_blank"><img src="Juricaf%20-%20A%20propos_fichiers/img/icon/logo_twitter.png" alt="Twitter" title="Suivez nous sur Twitter"/></a><!--26/05-->
+	   <a href="https://itunes.apple.com/fr/app/id587420315?mt=8&affId=1578782" target="_blank"><img src="Juricaf%20-%20A%20propos_fichiers/img/icon/logo_apple.png" alt="Appstore" title="Accs  la version IPhone"/></a><!--26/05-->
+      </div>
+
+      <div class="main">
+        <div class="content">
+          <div class="form_recherche">
+            <form method="get" action="/recherche">
+            <table summary="Rechercher">
+              <tr>
+                <td>
+                  <a href="http://www.juricaf.org/recherche"><img id="logo" src="Juricaf%20-%20A%20propos_fichiers/img/juricaf.png" alt="Juricaf" /></a><br /><!--26/05-->
+                  <div id="slogan"><h2>La jurisprudence francophone des cours suprêmes</h2></div> <!--26/05-->
+                  <input type="text" name="q" tabindex="10" placeholder="Rechercher parmi  1  050&nbsp; 541  décisions provenant de 45 pays et institutions francophones"><br><!--26/05-->
+                  <input type="submit" value="Rechercher" tabindex="20" /> <a href="#">recherche avancée</a>
+                </td>
+              </tr>
+            </table>
+            </form>
+          </div>
+          <div class="arret">
+            <h1>Genèse du projet</h1>
+
+            <p>L’<a href="http://www.ahjucaf.org/">AHJUCAF</a>, l’association des cours suprêmes judiciaires francophones, forte de cinquante institutions membres, a initié en 2004, la création d’une base de données de jurisprudence en français. La version développée entre 2004 et le 15 juin 2011 est accessible à cette adresse : <a href="http://v1.juricaf.org/">http://v1.juricaf.org/</a></p>
+
+            <p>L’objectif est de permettre aux magistrats et aux professionnels du droit (avocats, notaires, huissiers, juristes) d’avoir accès gratuitement aux décisions des cours suprêmes. Sous la direction d’un magistrat à la Cour de cassation française, le projet a été lancé officiellement début 2005 au moyen d’une subvention de l’Organisation internationale de la Francophonie et du Ministère de la justice français. Accessible à l’adresse <a href="http://www.juricaf.org/">http://www.juricaf.org</a>, la base de données comprend actuellement près de 800 000 arrêts issus de quarante pays.</p>
+
+            <h1>Objectifs généraux du projet</h1>
+
+            <p>L’AHJUCAF a voulu répondre à la demande des professionnels du droit et des citoyens de mettre à disposition un outil permettant un accès gratuit et une meilleure compréhension des décisions de justice. Il s’agit de la contribution des plus hauts magistrats francophones pour renforcer l’État de droit et la démocratie.</p>
+            <p>Notre association a constaté le retard important pris par les États francophones à se doter de bases de données juridiques. Si en Europe, la plupart des hautes juridictions disposent d’un tel outil, dans les pays ACP aucune institution ne diffuse gratuitement sa jurisprudence, contrairement aux pays de <i>Common law</i> où les décisions de justice des <i>final court of appeal</i> sont accessibles sur Internet.</p>
+
+            <p>Ainsi, nombre de citoyens, de défenseurs des droits de l’homme, d’avocats n’ont pas ainsi accès aux décisions de justice et ne peuvent connaître le verdict des affaires rendues dans le passé.. Dans le domaine économique, les arrêts de la Cour de justice de l’Ohada sont accessibles librement , mais avec une mise à jour incertaine  et leur publication ne répond pas aux normes documentaires retardant ainsi leur exploitation par les avocats.</p>
+            <p>Juricaf est destinée en priorité au monde francophone, mais est aussi aux ressortissants des pays de common law afin qu’ils puissent avoir accès à la jurisprudence rendue en français.</p>
+
+            <h1>Objectifs spécifiques à court et moyen terme</h1>
+
+            <h2>Réalisations du projet à court terme</h2>
+
+            <p>Après six ans de présence sur Internet, l’Assemblée générale de l’AHJUCAF en juin 2010 a décidé la mise à niveau sur le plan documentaire  et technique de la base de données. Juricaf respectera ainsi les standards mondiaux, européens et francophones. Elle sera réalisée au moyen de logiciels libres et des outils de veille seront également proposés.<br />
+            Désormais sous la direction d’un juge du Tribunal fédéral suisse, Juricaf comportera à compter d'octobre 2011 l’intégralité des décisions françaises, suisses, canadiennes et belges, les données déjà publiées dans Juricaf, ainsi que la jurisprudence accessible et réutilisable librement sur Internet.</p>
+
+            <p>Pour les autres membres de notre association et particulièrement les cours suprêmes africaines , l’AHJUCAF soutiendra techniquement et financièrement leurs services de documentation à se doter des technologies informatiques et documentaires afin de permettre la diffusion de la jurisprudence à l’échelon mondial.</p>
+
+            <h2>Objectifs du projet à moyen terme</h2>
+            <p>Outil documentaire unique en droit comparé et gratuit sur Internet, Juricaf proposera pour ses cours suprêmes la commercialisation de la jurisprudence à l’international par la publication de recueils, de CD-ROM ou de licences de réutilisation. Dans un objectif de permettre le développement de l’informatique juridique dans les pays du Sud, les recettes générées seront affectées aux projets des cours suprêmes membres de l’AHJUCAF.</p>
+
+            <p>Des liens avec la législation et la doctrine seront effectués, ainsi que l’intégration de thésaurus multilingues  permettant une meilleure connaissance du droit francophone, notamment à destination des professionnels du droit issus des pays de <i>Common law</i>.
+            Pour les autres membres de notre association et particulièrement les cours suprêmes africaines, l’AHJUCAF soutiendra techniquement et financièrement leurs services de documentation à se doter des technologies informatiques et documentaires afin de permettre la diffusion de la jurisprudence à l’échelon mondial.</p>
+          </div>
+        </div>
+      </div>
+	 <a href="http://www.ahjucaf.org/"><img style="margin-left: 10px;" src="Juricaf%20-%20A%20propos_fichiers/img/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a><!--26/05-->
+	 <img style="float: right; margin-right: 10px;" src="Juricaf%20-%20A%20propos_fichiers/img/francophonie.png" alt="Organisation internationale de la francophonie" /><!--26/05-->
+<div class="top_page"><a id="top" href="#">Haut de page<img src="Juricaf%20-%20A%20propos_fichiers/img/fleche_haut.png" alt="Haut de page"></a></div><!--26/05-->
+      <div class="bottom">
+        <p>Juricaf est un projet de l'AHJUCAF, l'association des cours judiciaires suprêmes francophones,<br />
+        réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université&nbsp;Paris&nbsp;I).<br />
+        Il est soutenu par l'Organisation internationale de la Francophonie et le Fonds francophone des inforoutes.</p>
+      </div>
+    </div>
+    <script type="text/javascript">
+    <!--
+    function juricafSearch() {
+      if (window.external && ("AddSearchProvider" in window.external)) {
+        window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
+      }
+      else {
+         alert("Votre navigateur ne supporte pas cette fonctionnalité");
+      }
+    }
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-8802834-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    // -->
+    </script>
+  </body>
+</html>
