d8b9b9706d8f1e8407ad19476f45252a4318c007
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-21 15:27:10 +0200

    créer le dossier tmp puis le supprime

--- parsers/Monaco/TribunalSupreme/script_automatisation.sh
+++ parsers/Monaco/TribunalSupreme/script_automatisation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+mkdir -p tmp/pages
+mkdir -p tmp/home_pages
+cd tmp/
+touch urls.txt
+cd ..
 php homes_downloader.php
 php url_listing.php
 php pages_downloader.php
 php parser_htmltoxml.php
+cd tmp/
+rm -r home_pages
+cd ..
+rm -r tmp
