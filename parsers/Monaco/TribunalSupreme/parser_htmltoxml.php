59544aedf2043456d331efd3f383bd3d892ac323
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-21 14:55:22 +0200

    Problème des espaces à régler

--- parsers/Monaco/TribunalSupreme/parser_htmltoxml.php
+++ parsers/Monaco/TribunalSupreme/parser_htmltoxml.php
@@ -3,7 +3,7 @@ $xmlfile='tmp/xmls/';
 $mois=['01'=>'janvier','02'=>'février','03'=>'mars','04'=>'avril','05'=>'mai','06'=>'juin','07'=>'juillet','08'=>'août','09'=>'septembre','10'=>'octobre','11'=>'novembre','12'=>'décembre'];
 
 $fichiers=scandir('tmp/pages');
-$i=0;
+
 
 foreach ($fichiers as $k => $v) {
 
@@ -20,9 +20,15 @@ foreach ($fichiers as $k => $v) {
 
   preg_match('/<p class="date">.+<\/p><h1>([^<]+)/',$content,$titre);
   $titre=$titre[1];
-  echo($titre."\n");
-  // preg_match('\d\d\d\d-\d\d',$titre,$numero);
+  // echo($titre."\n");
+  preg_match('/[0-9]+[-]+.+[0-9]/',$titre,$numero);
 
+  if ($numero==null){
+    $numero=='';
+  }
+  else{
+    $numero=$numero[0];
+  }
 
   $juridiction1='Tribunal suprême';
   $juridiction2='Tribunal Suprême';
@@ -30,50 +36,35 @@ foreach ($fichiers as $k => $v) {
 
   preg_match("#<div class=\"content\">.+<div class=\"print-button\">#",$content,$content);
   $content=$content[0];
-  // $content=strip_tags($content);
-
-  // $content=preg_replace('#</h[1-4]*>#',"\n",$content);
+  $content=preg_replace('#</h[1-4]*>#',"\n",$content);
   $content=preg_replace('#</p>#',"\n",$content);
-
-  // $content=preg_replace('#<p>#',"\n",$content);
-  // $content=preg_replace('#^>#',"",$content);
-  // $content=preg_replace('#</p>#',"\n",$content);
-
-
-  // $content=preg_replace('#^[<p].+$[>]>#',"\n",$content);
-  // $content=preg_replace('#<p style="text-align: right;"><strong>#',"\n",$content);
-  // $content=preg_replace('#<strong>#',"\n",$content);
-  // $content=preg_replace('#<br>#',"\n",$content);
-
   $content=preg_replace('#<[^>]+>#', "", $content);
-  // $content= str_replace("\n\n"," ",$content);
-
   $content=strip_tags($content);
   $content=html_entity_decode($content);
   $content=str_replace("+[\n]","",$content);
+
 
   $d=new DateTime($date);
   $date_min=new DateTime('2019-05-01');
 
   if($d>$date_min){
 
-  $output = fopen($xmlfile.$i, 'w');
+  $output = fopen($xmlfile.'TS_'.$numero, 'w');
   fwrite($output, '<?xml version="1.0" encoding="utf8"?>');
   fwrite($output,"\n");
   fwrite($output, "<DOCUMENT>\n");
   fwrite($output, "<DATE_ARRET>$date</DATE_ARRET>\n");
   fwrite($output, "<JURIDICTION>$juridiction1</JURIDICTION>\n");
-  // fwrite($output, "<NUM_ARRET>$numero</NUM_ARRET>\n");
+  fwrite($output, "<NUM_ARRET>TS/$numero</NUM_ARRET>\n");
   fwrite($output, "<PAYS>Monaco</PAYS>\n");
   fwrite($output, "<TEXTE_ARRET>$content</TEXTE_ARRET>\n");
   $datefr=$j.' '.$mois[$m].' '.$a;
-  // fwrite($output,"<TITRE>Monaco, $juridiction, $datefr, $numero<TITRE>\n");
+  fwrite($output,"<TITRE>Monaco, $juridiction2, $datefr, $numero<TITRE>\n");
   fwrite($output, "<TYPE>arret</TYPE>\n");
   fwrite($output, "</DOCUMENT>\n");
   fclose($output);
 
 }
-  $i=$i+1;
 
 
 
