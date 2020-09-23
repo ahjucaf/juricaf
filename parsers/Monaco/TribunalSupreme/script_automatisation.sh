595fb835f7d1b4798300b89ce7548cba90e6f6e5
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-23 13:37:45 +0200

    suppression du répertoire au début pour faciliter debug + parcours de tous les .html qui ont été téléchargés

--- parsers/Monaco/TribunalSupreme/script_automatisation.sh
+++ parsers/Monaco/TribunalSupreme/script_automatisation.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+rm -r tmp
 mkdir -p tmp/pages
 mkdir -p tmp/home_pages
 cd tmp/
@@ -7,8 +9,7 @@ cd ..
 php homes_downloader.php
 php url_listing.php
 php pages_downloader.php
-php parser_htmltoxml.php
-cd tmp/
-rm -r home_pages
-cd ..
-rm -r tmp
+
+for page in tmp/pages/* ; do
+  php parser_htmltoxml.php $page
+done
