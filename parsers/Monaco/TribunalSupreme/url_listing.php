01bcc5b596f46ca1c14c3b7b3c8f5c4c3404ae61
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-23 21:45:39 +0200

    si l'url est dans la liste des urls déjà scraper on ne le prend pas

--- parsers/Monaco/TribunalSupreme/url_listing.php
+++ parsers/Monaco/TribunalSupreme/url_listing.php
@@ -18,7 +18,8 @@ foreach ($fichiers as $k => $v) {
     foreach($lien as $k=>$v){
           if ($v!='https://www.tribunal-supreme.mc/' && $v!="https://www.tribunal-supreme.mc/mentions-legales/" && $v!='#tarteaucitron' && stristr($v,'communique')!=True){
             if($k%2 == 0){
-              if(in_array($v,$lignes_all_urls)!=True){
+              if(in_array($v,$lignes_all_urls)==true){
+                echo('Je ne suis pas dedans:');
                 fwrite($output,$v."\n");
               }
             }
