a16ce9dd00b5689316f3b3db5dd1d11d6cd32b4a
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-24 17:28:31 +0200

    tmp dans le gitignore suppression des commentaires dans le pages_downloader et script vierge pour le parser

--- parsers/Monaco/cour_de_revision/pages_downloader.php
+++ parsers/Monaco/cour_de_revision/pages_downloader.php
@@ -6,23 +6,14 @@ $i=1;
 $taburl=[];
 
 foreach($urls as $k=>$v){
-  // $v = rtrim($v);
   if ( preg_match('#^(http|https)://www#',$v))
    {
      preg_match('/(.+?)!/',$v,$v);
      $v=$v[1];
-     // print_r($v);
      $cmd='curl "'.$v.'"'." > tmp/pages/arret$i.html";
-  //     $taburl["arret$i.html"]=$v;
-    // echo($cmd."\n");
      shell_exec($cmd);
      $i=$i+1;
  }
 }
-// if ($taburl!=[]){
-//
-// file_put_contents('tmp/urls.json', json_encode($taburl));
-// }
-
 
 ?>
