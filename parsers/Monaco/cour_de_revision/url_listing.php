7821f7d568a1b9e37e951c344f0a03303ef77902
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-26 09:48:11 +0200

    ajout pour l'automatisation pas encore intégré

--- parsers/Monaco/cour_de_revision/url_listing.php
+++ parsers/Monaco/cour_de_revision/url_listing.php
@@ -2,6 +2,8 @@
 $fichiers=scandir('tmp/home_pages');
 $head="https://www.legimonaco.mc/";
 $output = fopen('tmp/urls.txt', 'w');
+$lignes_all_urls=file('all_urls.txt');
+
 foreach ($fichiers as $k => $v) {
     if ($k!=0 && $k!=1){
       $html=file_get_contents('tmp/home_pages/'.$v);
@@ -12,5 +14,16 @@ foreach ($fichiers as $k => $v) {
       }
     }
 }
+
+
+$all_urls=fopen('all_urls.txt','a+');
+$lignes=file('tmp/urls.txt');
+
+foreach($lignes as $ligne){
+      if (in_array($ligne,$lignes_all_urls)!=True){
+        fwrite($all_urls,$ligne);
+      }
+}
+fclose($all_urls);
 
 ?>
