ae34665259b62a33c7aa7427cf120fc2ad407465
Honorine-MC <honorine.mariechristy@gmail.com>
2020-09-26 09:47:34 +0200

    ajout des vérifications pour savoir s'il y a un demandeur, un defendeur et un sommaire

--- parsers/Monaco/cour_de_revision/homes_downloader.php
+++ parsers/Monaco/cour_de_revision/homes_downloader.php
@@ -2,8 +2,9 @@
 $head="https://www.legimonaco.mc/305/legismc.nsf/ViewJurisCR!OpenView&Start=1&Count=100&Expand=";
 $dossier='tmp/home_pages';
 
+$nb_pages=$argv[1];
 $i=1;
-while($i<=1){  //plus tard mettre 48
+while($i<=$nb_pages){  //plus tard mettre 48
   $url=$head.$i."/";
   $cmd='curl -s "'. $url.'" >'.$dossier.'/page'.$i.'.html';
   shell_exec($cmd);
--- parsers/Monaco/cour_de_revision/parser_htmltoxml.php
+++ parsers/Monaco/cour_de_revision/parser_htmltoxml.php
@@ -2,28 +2,32 @@
 
 $xmlfile='xmls/';
 $mois=['janvier'=>'01','février'=>'02','mars'=>'03','avril'=>'04','mai'=>'05','juin'=>'06','juillet'=>'07','août'=>'08','septembre'=>'09','octobre'=>'10','novembre'=>'11','décembre'=>'12'];
-$inputfile='tmp/pages/arret1.html'; //$arg[1]
+$inputfile= $argv[1];
 $content=file_get_contents($inputfile);
 
 preg_match('/var url="\/305\/legismc\.nsf\/(.+)!/',$content,$numero);
 $numero=$numero[1];
 
-
 $content=html_entity_decode($content);
 $content=preg_replace("/\n/"," ",$content);
-// preg_match('/colspan=6 rowspan=1>(.+?)<\/td>/',$content,$titre_principale);
+
 preg_match('/<td colspan=6 rowspan=1>(.+?)<\/table>/',$content,$titre_principale);
+if(count($titre_principale)>1){
+  $titre_principale=$titre_principale[1];
+  $titre_principale=strip_tags($titre_principale);
+}
+else{
+  $titre_principale="";
+}
 
-$titre_principale=$titre_principale[1];
-$titre_principale=strip_tags($titre_principale);
-// echo($titre_principale);
-
-// preg_match('/<p class=resume>.+<\/p>[^<]+(.+[^<]+<p.+)[^<]+<div class=argument>/',$content,$sommaire);
 preg_match('/<p class=resume>.+?<\/p>(.+<p class=par-resume>.+?)<div class=argument>/',$content,$sommaire);
-
-$sommaire=$sommaire[1];
-$sommaire=strip_tags($sommaire);
-
+if(count($sommaire)>1){
+  $sommaire=$sommaire[1];
+  $sommaire=strip_tags($sommaire);
+}
+else{
+  $sommaire="";
+}
 preg_match('/<p class=date-decision>(.+?)<\/p>/',$content,$datefr);
 $datefr=$datefr[1];
 $j=$datefr[0].$datefr[1];
@@ -33,31 +37,39 @@ $m=$mois[$m];
 preg_match('/.+? (\d+)/',$datefr,$a);
 $a=$a[1];
 $date=$a.'-'.$m.'-'.$j;
+$datefr=trim($datefr);
 
-$juridiction="Cour de Révision";
+$juridiction="Cour de révision";
 $pays="Monaco";
 $type="arret";
+
 preg_match('/<p class=demandeur>(.+?)<\/p>  <p class=defendeur>c\/(.+?)<\/p>/',$content,$parties);
-
-$demandeur=$parties[1];
-$defendeur=$parties[2];
-
+if(count($parties)<3){
+  $defendeur="";
+  $demandeur="";
+}
+else{
+  $demandeur=$parties[1];
+  $defendeur=$parties[2];
+}
 
 preg_match('/<div class=argument>(.+?)<\/html>/',$content,$content);
 $content=$content[1];
 $content=preg_replace('#</p>#',"\n",$content);
 $content=strip_tags($content);
-// $source=regarder le .json avec l url qui est associé au .html;
+$content=preg_replace("/\n/","<br/>",$content);
+
 $sources = json_decode (file_get_contents('tmp/urls.json'), true);
 $source=$sources[basename($inputfile)];
-$name=preg_replace('/\//','_',$source);
-$output = fopen($xmlfile.'CS_'.$name.'.xml', 'w');
 
+$name=preg_replace('/\//','_',$source);
+
+$output = fopen($xmlfile.'CS_'.$name.'.xml', 'w');
 
 fwrite($output, '<?xml version="1.0" encoding="utf8"?>');
 fwrite($output,"\n");
 fwrite($output, "<DOCUMENT>\n");
-fwrite($output, "<ANALYSE>\n<TITRE_PRINCIPAL>$titre_principale</TITRE_PRINCIPAL><SOMMAIRE>$sommaire</SOMMAIRE></ANALYSE>");
+fwrite($output, "<ANALYSES><ANALYSE>\n<TITRE_PRINCIPAL>$titre_principale</TITRE_PRINCIPAL><SOMMAIRE>$sommaire</SOMMAIRE></ANALYSE></ANALYSES>");
 fwrite($output, "<DATE_ARRET>$date</DATE_ARRET>\n");
 fwrite($output, "<JURIDICTION>$juridiction</JURIDICTION>\n");
 fwrite($output,"<FONDS_DOCUMENTAIRE>www.legimonaco.mc</FONDS_DOCUMENTAIRE>\n");
@@ -70,10 +82,6 @@ fwrite($output,"<SOURCE>$source</SOURCE>");
 fwrite($output, "<TYPE>arret</TYPE>\n");
 fwrite($output, "</DOCUMENT>\n");
 fclose($output);
-
-
-
-
 
 
 ?>
