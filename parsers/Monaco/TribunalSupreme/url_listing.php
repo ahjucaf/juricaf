e8c49d0d6d5ac89597120202179b153c6a0cf1ed
Honorine-MC <honorine.mariechristy@gmail.com>
2020-10-12 12:49:50 +0200

    problème avec file qui ne concatène plus le contenu du html j'ai donc fait un file_get_contents et un changement dans la lecture du contenu

--- parsers/Monaco/TribunalSupreme/url_listing.php
+++ parsers/Monaco/TribunalSupreme/url_listing.php
@@ -3,28 +3,39 @@
 $fichiers=scandir('tmp/home_pages');
 
 $lignes_all_urls=file('all_urls.txt');
-
+// print_r($lignes_all_urls);
 $output = fopen('tmp/urls.txt', 'w');
 foreach ($fichiers as $k => $v) {
   if($k!=0 && $k!=1){
-    $html=file('./tmp/home_pages/'.$fichiers[$k]);
-    $html=$html[0];
-    preg_match('#<div class=\"content\">(.+)#',$html,$res);
+    $html=file_get_contents('./tmp/home_pages/'.$fichiers[$k]);
+    // print_r($html);
+    $html=preg_replace('/\n/',' ',$html);
+    // $html=$html[0];
+    // echo($html);
+    preg_match('/<div class="content">(.+)/',$html,$res);
+    // print_r($res);
     $content=$res[1];
 
     preg_match_all('/<a href="([^"]+)"/',$content,$lien);
+    // print_r($lien);
     $lien=$lien[1];
+
     foreach($lien as $k=>$v){
           // echo($v);
           if ($v!='https://www.tribunal-supreme.mc/' && $v!="https://www.tribunal-supreme.mc/mentions-legales/" && $v!='#tarteaucitron' && stristr($v,'communique')!=True){
+            // echo("1\n");
             if($k%2 == 0){
+              // echo("2\n");
               if (count($lignes_all_urls)>0){
+                // echo("3\n");
                 if( in_array($v."\n",$lignes_all_urls)==false){
                   fwrite($output,$v."\n");
+                  // echo("4\n");
                 }
               }
               else{
                 fwrite($output,$v."\n");
+                // echo("5\n");
               }
 
             }
