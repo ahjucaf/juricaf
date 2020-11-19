052cf01718e0ecd53719062c04b28bcd009185a6
Tangui Morlier <tangui@tangui.eu.org>
2020-11-19 15:06:05 +0100

    Présentation des pays par colonnes

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -12,26 +12,25 @@ $cpt = 0;
 <div class="pays">
 	<h3>Rechercher parmi <?php echo number_format($nb, 0, '', ' '); ?> décisions provenant de <?php echo count($pays); ?> pays et institutions francophones</h3>
 	<div class="payscols">
-	  <table><tr>
+	  <table><tr><td>
 		<?php
 		foreach ($pays as $p)
 		{
 		  $pays = preg_replace('/ /', '_', $p['key'][0]);
-		  if ($cpt % 3 == 1) { echo '</tr><tr>'; }
+		  if ($cpt % 15 == 0 && $cpt) { echo '</td><td>'; }
 		  $cpt++;
-		  
 		  // Traitement du nom du pays : si trop long, le nom est coupé
 		  $pays_nom = $p['key'][0].' ('.number_format($p['value'], 0, '', ' ').')';
 		  $link = link_to($pays_nom,'recherche/search?query=+&facets=facet_pays:'.$pays);
 		  if (strlen($pays_nom) > 45) {
 			  $pays_nom_min = substr($pays_nom, 0, 16) . '...';
 			  $link = link_to($pays_nom_min,'recherche/search?query=+&facets=facet_pays:'.$pays);
-			  //$link = str_replace("<a ", "<a title=\"$pays_nom\" ", $link ); // Ajout du titre pour que popup du nom entier apparaisse
 		  }
-if (strlen($pays) > 0) {
-  		  echo '<td><img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.$link .'</td>';
-		}}
+          if (strlen($pays) > 0) {
+  		      echo '<img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.$link .'<br/>';
+          }
+        }
 		?>
-		</tr><tr><td colspan="4" class="plus"><img src="images/+.png" alt="+" /> <a href="/documentation/stats/statuts.php">Plus de statistiques</a></td></tr></table>
+    </td></tr><tr><td colspan="5" class="plus"><img src="images/+.png" alt="+" /> <a href="/documentation/stats/statuts.php">Plus de statistiques</a></td></tr></table>
 	</div>
 </div>
