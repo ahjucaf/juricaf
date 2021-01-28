fe256ab3d0a173c0a2ba9240f6ec3bc85fb410cc
Tangui Morlier <tangui@tangui.eu.org>
2021-01-28 12:17:52 +0100

    Message d'erreur en cas de problème avec le pdf téléchargé

--- parsers/luxembourg/parse_pdf.sh
+++ parsers/luxembourg/parse_pdf.sh
@@ -8,4 +8,8 @@ mkdir -p .tmp
 
 rm -f .tmp/arrets.html
 pdftohtml $pdf .tmp/arret > /dev/null 2>&1
+if test -s .tmp/arrets.html ; then
 cat .tmp/arrets.html | php parser_pdftohtml.php $xml $url
+else
+echo "problème avec le pdf de $url";
+fi
