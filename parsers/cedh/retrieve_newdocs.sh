1cd184a454f5bf8f74b198a13a729a86f71698fd
Tangui Morlier <tangui@tangui.eu.org>
2021-02-02 18:06:24 +0100

    Récupération des métadonnée (json), de l'arrêt html et sa transformation en txt

--- parsers/cedh/retrieve_newdocs.sh
+++ parsers/cedh/retrieve_newdocs.sh
@@ -3,6 +3,7 @@
 date_from=$( cat last_kpdate.txt | awk '{if ($3 > 2000-01-01) print $3 ; else print "2000-01-01" ;}' )
 ARRET_LIST=/tmp/$$"_arrets.list"
 rm -f last_kpdate.txt.new
+mkdir -p arrets
 
 for start in {0..10000..100} ; do
 
@@ -23,5 +24,18 @@ for start in {0..10000..100} ; do
 
 done
 
+cat $ARRET_LIST | while read i arretnum arretdate ; do
+    curl -s -m 1 --ciphers 'DEFAULT:!DH' "https://hudoc.echr.coe.int/app/conversion/docx/html/body?library=ECHR&id="$arretnum > arrets/$arretnum".html"
+    if test -s arrets/$arretnum".html"; then
+        html2markdown arrets/$arretnum".html" > arrets/$arretnum".txt"
+    fi
+    curl -s -m 1 --ciphers 'DEFAULT:!DH' "https://hudoc.echr.coe.int/app/query/results?query=(contentsitename=ECHR)%20AND%20"$arretnum"&select=advopidentifier,applicationnumber,advopstatus,applicability,appno,article,casecitation,conclusion,contentcategory,contentsitename,decisiondate,docname,doctype,documentcollectionid,documentcollectionid2,extractedappno,hudocdate,ecli,externalsources,importance,introductiondate,isplaceholder,issue,itemid,judgementdate,kpdate,kpdateAsText,kpthesaurus,meetingnumber,languageisocode,originatingbody,publishedby,referencedate,reportdate,representedby,resolutiondate,resolutionnumber,respondent,rulesofcourt,scl,sclappnos,separateopinion,typedescription&sort=&start=0&length=1" > arrets/$arretnum".json"
+    echo $arretnum
+done
+
 mv last_kpdate.txt.new last_kpdate.txt
-rm /tmp/$$"_arrets.list"
+rm -f $ARRET_LIST".one" $ARRET_LIST
+
+#https://hudoc.echr.coe.int/app/query/results?query=(contentsitename=ECHR)%20AND%20001-112422&select=itemid,applicability,appno,article,conclusion,decisiondate,docname,documentcollectionid,documentcollectionid2,doctype,externalsources,importance,introductiondate,issue,judgementdate,kpthesaurus,meetingnumber,originatingbody,publishedby,referencedate,kpdate,advopidentifier,advopstatus,reportdate,representedby,resolutiondate,resolutionnumber,respondent,rulesofcourt,separateopinion,scl,typedescription,ecli,casecitation,contentsitename&sort=&start=0&length=1
+
+#https://hudoc.echr.coe.int/app/conversion/docx/html/body?library=ECHR&id=001-112422
