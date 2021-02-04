01ff784a4c5130d247dc218ef6f3c9f8350842a5
Tangui Morlier <tangui@tangui.eu.org>
2021-02-04 14:38:05 +0100

    Si fichier vide on s'en fiche

--- parsers/cedh/update.sh
+++ parsers/cedh/update.sh
@@ -6,4 +6,7 @@ mkdir -p $POOL_DIR
 
 bash retrieve_newdocs.sh | while read id ; do
     php parse_arret.php $id > $POOL_DIR"/"$id".xml"
+    if ! test -s $POOL_DIR"/"$id".xml" ; then
+        rm $POOL_DIR"/"$id".xml"
+    fi
 done
