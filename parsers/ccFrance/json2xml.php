8b956b13e595f313ce8a012201934858196c6ca7
Gabriel Poma <gpoma@24eme.fr>
2021-11-10 19:39:33 +0100

    cdata pour texte_arret + autres noeuds

--- parsers/ccFrance/json2xml.php
+++ parsers/ccFrance/json2xml.php
@@ -7,12 +7,34 @@ if (! $doc) {
     exit(1);
 }
 
-$xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf8"?><DOCUMENT></DOCUMENT>');
+// https://stackoverflow.com/a/20511976
+class SimpleXMLExtended extends SimpleXMLElement {
+  public function addChildWithCDATA($name, $value = NULL) {
+    $new_child = $this->addChild($name);
+
+    if ($new_child !== NULL) {
+      $node = dom_import_simplexml($new_child);
+      $no   = $node->ownerDocument;
+      $node->appendChild($no->createCDATASection($value));
+    }
+
+    return $new_child;
+  }
+}
+
+$xml = new SimpleXMLExtended('<?xml version="1.0" encoding="utf8"?><DOCUMENT></DOCUMENT>');
 $xml->addChild('DATE_ARRET', $doc->decision_date);
 $xml->addChild('PAYS', 'France');
 $xml->addChild('JURIDICTION', $doc->jurisdiction);
 $xml->addChild('FORMATION', $doc->chamber);
-$xml->addChild('TEXTE_ARRET', '<![CDATA['.$doc->text.']]');
+
+$xml->addChildWithCDATA('TEXTE_ARRET', $doc->text);
+
+$xml->addChild('TYPE', $doc->type);
+$xml->addChild('NUM_ARRET', $doc->number);
+
+$xml->addChild('TITRE', "France, $doc->jurisdiction, $doc->chamber, $doc->decision_date, $doc->number");
+
 
 
 echo $xml->asXML();
