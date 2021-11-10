14f22e00c01ec7e081d4a5579cda8e25debc2df7
Gabriel Poma <gpoma@24eme.fr>
2021-11-10 13:13:11 +0100

    plus de noeuds

--- parsers/ccFrance/json2xml.php
+++ parsers/ccFrance/json2xml.php
@@ -4,10 +4,16 @@ $doc = $argv[1];
 $doc = json_decode(@file_get_contents($doc));
 if (! $doc) {
     fwrite(STDERR, "ERREUR: json non trouvé\n");
+    exit(1);
 }
 
 $xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf8"?><DOCUMENT></DOCUMENT>');
-$date_arret = $xml->addChild('DATE_ARRET');
+$xml->addChild('DATE_ARRET', $doc->decision_date);
+$xml->addChild('PAYS', 'France');
+$xml->addChild('JURIDICTION', $doc->jurisdiction);
+$xml->addChild('FORMATION', $doc->chamber);
+$xml->addChild('TEXTE_ARRET', '<![CDATA['.$doc->text.']]');
+
 
 echo $xml->asXML();
 ?>
