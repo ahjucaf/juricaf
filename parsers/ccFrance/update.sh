581620dc27e8147d59efecab7ae3c359fe9d502f
Honorine-MC <honorine.mariechristy@gmail.com>
2021-11-19 11:14:07 +0100

    transfert des xmls vers le dossier pool pour l'import

--- parsers/ccFrance/update.sh
+++ parsers/ccFrance/update.sh
@@ -2,12 +2,25 @@
 
 . config/config.sh
 
-curl "$API_URL/export?batch=0&resolve_references=true" -H "KeyId: $API_TOKEN" | jq -rc '.results[]' > /tmp/batch.json
+if [ -d "/tmp/ccFrance" ];then
+  rm -r /tmp/ccFrance
+fi
 
-cat /tmp/batch.json | while read -r json; do
+ANNEE=$(date +%Y)
+
+mkdir -p /tmp/ccFrance/xmls
+mkdir -p /tmp/ccFrance/jsons
+
+
+curl "$API_URL/export?batch=0&resolve_references=true" -H "KeyId: $API_TOKEN" | jq -rc '.results[]' > /tmp/ccFrance/jsons/batch.json
+
+cat /tmp/ccFrance/jsons/batch.json | while read -r json; do
   _id=$(echo "$json" | cut -d'"' -f4)
-  touch /tmp/$_id.json
-  echo "$json" > /tmp/$_id.json
+  touch /tmp/ccFrance/jsons/$_id.json
+  echo "$json" > /tmp/ccFrance/jsons/$_id.json
 
-  php json2xml.php /tmp/$_id.json > /tmp/$_id.xml
+  php json2xml.php /tmp/ccFrance/jsons/$_id.json > /tmp/ccFrance/xmls/$_id.xml
+
+  cp /tmp/ccFrance/xmls/* $POOL_DIR/"$ANNEE"/courdecassation/
+
 done
