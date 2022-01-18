cc30c4a09e907669ecc43fda058a27fb9ce8a323
Tangui Morlier <tangui@tangui.eu.org>
2022-01-18 17:14:40 +0100

    Données persos

---
+++ project/web/documentation/donnees_personnelles.php
@@ -0,0 +1,220 @@
+<?php
+// Token
+@session_start();
+$token = sha1(mt_rand());
+$_SESSION['token'] = $token;
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="title" content="Juricaf - Données personnelles" />
+<meta name="description" content="La base de données de jurisprudence francophone" />
+<meta name="keywords" content="jurisprudence, cassation, cour, suprême, francophone, francophonie, ahjucaf, arrêt, décision" />
+<meta name="language" content="fr" />
+    <title>Juricaf - Mentions légales</title>
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+
+    <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="/js/jquery.scrollTo-min.js"></script>
+    <link rel="search" href="/juricaf.xml" title="Rechercher sur Juricaf" type="application/opensearchdescription+xml" />
+  </head>
+  <body>
+    <div class="site">
+      <div class="head">
+         <div class="reseaux_sociaux">
+          <a href="http://www.parcesmotifs.net/spip.php?page=groupe&amp;id_groupe=12"><img src="/images/help.png" alt="Aide" title="Aide" /></a>
+          <a href="https://www.facebook.com/Juricaf"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook" /></a>
+          <a href="https://twitter.com/#!/juricaf"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter" /></a>
+          <a href="http://www.parcesmotifs.net/spip.php?page=backend"><img src="/images/rss.png" alt="RSS" title="Flux RSS" /></a>
+          <a href="javascript:juricafSearch();"><img src="/images/juricaf_search.png" alt="J" title="Ajouter Juricaf aux moteurs de recherches de votre navigateur" /></a>
+        </div>
+        <div class="menu">
+          <ul>
+            <li><a href="/documentation/a_propos.php">A propos</a></li>
+
+            <li><a href="/documentation/stats/statuts.php">Étendue des collections</a></li>
+<!---            <li><a href="#">Outils</a></li> -->
+            <li><a href="/documentation/partenaires.php">Partenaires</a></li>
+            <li><a href="/documentation/contact.php">Contact</a></li>
+            <li><a href="/documentation/mentions_legales.php">Mentions légales</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="main">
+        <div class="content">
+          <div class="form_recherche">
+            <form method="get" action="/recherche">
+            <table summary="Rechercher">
+              <tr>
+                <td>
+                  <a href="http://www.juricaf.org/recherche"><img id="logo" src="/images/juricaf.png" alt="Juricaf" /></a><br />
+                  <span style="font-family: Georgia; font-style: italic; color: #4E4C4D;">La jurisprudence francophone des cours suprêmes</span><br />
+                  <input type="text" style="width: 300px; margin-top: 5px;" name="q" value="" tabindex="10" /><br />
+                  <input type="submit" value="Rechercher" tabindex="20" /> <a href="#">recherche avancée</a>
+                </td>
+              </tr>
+            </table>
+            </form>
+          </div>
+          <div class="arret">
+              <h2>1 - Identité du responsable de traitement</h2>
+
+              <p>L’Association des Hautes Juridictions de Cassation des pays ayant en partage l’usage du Français, ci-après AHJUCAF est responsable de traitement pour le site <a href="https://www.juricaf.org/">www.juricaf.org</a> (ci-après JURICAF)</p>
+
+              <p>L’AHJUCAF est une association loi 1901 inscrite au registre des déclarations d’association de la Préfecture de police de Paris, publié au Journal officiel du 9 juin 2001.</p>
+              <p>L’AHJUCAF est inscrite au répertoire national des entreprises et de leurs établissements sous le numéro SIREN 440 233 880 (APE 913 E)</p>
+
+              <p>AHJUCAF, 5 quai de l’Horloge, 75001 PARIS<br/>
+              Tel. : +33 1 46 34 67 40</p>
+
+              <h2>2 - Données collectées par le site et finalités poursuivies</h2>
+
+              <p>Les seules données personnelles collectées par le site sont celles collectées via le formulaire de <a href="https://juricaf.org/documentation/contact.php">contact</a> pour la gestion des commentaires, avis et questions déposés par les usagers par le biais de ce formulaire.</p>
+
+              <p>Les informations recueillies dans le formulaire sont insérées dans un mail adressé au secrétariat général de l’AHJUCAF. La base légale du traitement est le consentement recueilli au moment où vous remplissez le formulaire.</p>
+
+              <p>Les données recueillies obligatoirement sont : l’adresse e-mail.</p>
+              <p>Les données collectées seront communiquées aux seuls destinataires suivants : Secrétariat général de l’AHJUCAF.</p>
+
+              <p>Elles sont conservées pendant une année.</p>
+              
+              <p>Vous pouvez accéder aux données vous concernant, les rectifier, demander leur effacement ou exercer votre droit à la limitation du traitement de vos données.</p>
+
+              <p>Consultez le site <a href="https://www.cnil.fr/">cnil.fr</a> pour plus d’informations sur vos droits.</p>
+
+              <p>Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez contacter le service chargé de l’exercice de ces droits : secrétariat général de l’AHJUCAF, 5 quai de l’Horloge, 75001 Paris, 0033 (1) 46 34 67 40</p>
+
+              <p>Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés » ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL.</p>
+
+              <h2>3 - Données personnelles diffusées sur le site www.juricaf.org</h2>
+
+              <p>Les données de jurisprudence diffusées sur le site www.juricaf.org ont été pseudonymisées conformément aux articles L 111-13 et suivants du code de l’organisation judiciaire sauf à respecter la législation d’origine des décisions et les principes fondamentaux auxquels obéissent les juridictions étrangères. C’est pour cette raison que les décisions de la Cour suprême du Canada et de la Cour Européenne des droits de l’Homme ne font pas l’objet d’une pseudonymisation.</p>
+
+              <p>Destinataire des données :</p>
+              <p>L'accès aux données personnelles pseudonymisées ou non est strictement limité à l’AHJUCAF et, le cas échéant à ses sous-traitants.</p>
+
+              <p>Les sous-traitants en question sont soumis à une obligation de confidentialité et ne peuvent utiliser vos données qu'en conformité avec nos dispositions contractuelles et la législation applicable.</p>
+
+              <p>Le droit d’accès et de rectification ainsi que le droit d’opposition s’exerce auprès du secrétariat général de l’AHJUCAF, 5 quai de l’Horloge, 75001 Paris, 0033 (1) 46 34 67 40. Pour toute demande d’exercice de droits, l’envoi de la pièce d’identité de la personne concernée est requis.</p>
+
+              <p>Vous pouvez écrire à l’adresse mail suivante : « sgahjucaf@ahjucaf.org »</p>
+
+              <h2>4 - Reproduction et réutilisation des données diffusées par le site www.juricaf.org</h2>
+
+              <p>Contexte :</p>
+
+              <p>L’Association des Hautes Juridictions de Cassation des pays ayant en partage l’usage du Français (AHJUCAF) a été créée en 2001 à l’initiative de 34 Juridictions Suprêmes francophones et l’Organisation internationale de la Francophonie. Par ses statuts (article 4) l’AHJUCAF a pour mission de :<br/>
+
+                  • favoriser l’entraide, la solidarité, la coopération, les échanges d’idées et d’expériences entre les institutions judiciaires membres sur les questions relevant de leur compétence ou intéressant leur organisation et leur fonctionnement ;</br>
+
+                  • promouvoir le rôle des Hautes Juridictions dans la consolidation de l’Etat de droit, le renforcement de la sécurité juridique, la régulation des décisions judiciaires et l’harmonisation du droit au sein des États membres.
+              </p>
+
+              <p>Dans ce cadre, l’AHJUCAF a créé la base de données JURICAF accessible sur internet à l’adresse www.juricaf.org. Il s’agit d’une base de données de décisions de justice en langue française de 43 pays et institutions.</p>
+
+              <o>Mise à disposition et réutilisation de données :</p>
+
+              <p>Sauf pour les décisions des juridictions françaises, les données de JURICAF sont diffusées sous la licence ODbL 1.0. Elle est consultable à l'adresse : <a href="http://www.juricaf.org/documentation/licence_odbl.php">http://www.juricaf.org/documentation/licence_odbl.php</a>.</p>
+              
+              <p>La réutilisation est autorisée dans les conditions de la licence ODbL.</p>
+              
+              <p>Pour une compréhension facilitée de la licence ODbL, voici une traduction en français de la licence, toutefois, seule la version anglaise fait foi. Voici également un document de vulgarisation de la licence.</p>
+
+              <p>Si vous ne voulez pas ou ne souhaitez pas réutiliser les décisions dans les conditions de la licence ODbL ou si vous souhaitez disposer de fichiers XML structurés selon vos besoins ou tout autre prestation, les données seront disponibles prochainement sous licence. Merci de contacter le secrétariat pour toute information utile via le formulaire de contact en bas de cette page.</p>
+
+              <p>Les données des juridictions françaises sont issues des bases de données Judilibre et Ariane. La n° 2019-222 du 23 mars 2019 de programmation 2018-2022 et de réforme pour la justice et ses textes d’applications organisent la réutilisation de ces données.</p>
+
+              <p>Pour plus d’informations sur la réutilisation des décisions des juridictions françaises : <a href="http://www.justice.gouv.fr/publications-10047/open-data-des-decisions-de-justice-13008/">http://www.justice.gouv.fr/publications-10047/open-data-des-decisions-de-justice-13008/</a></p>
+
+
+              <p>Interdiction des traitements visant à la réidentification des justiciables :</p>
+
+              <p>Les données présentes sur le site www.juricaf.org sont pseudonymisées conformément aux articles L 111-13 et suivants du code de l’organisation judiciaire français sauf à respecter la législation d’origine des décisions et les principes fondamentaux auxquels obéissent les juridictions étrangères. C’est pour cette raison que les décisions de la Cour suprême de Canada et de la Cour Européenne des droits de l’Homme ne font pas l’objet d’une pseudonymisation.</p>
+
+              <p>La réutilisation des données de la base www.juricaf.org ne peut avoir pour objet ou pour effet de réidentifier les personnes concernées, ce que rappelle la CNIL et Etalab en utilisant la formulation suivante s’agissant de la réutilisation des données de jurisprudence :</p>
+
+              <p>« Dans le cadre de leurs missions de service public, les administrations produisent ou reçoivent des informations publiques qui peuvent être réutilisées par toute personne physique ou morale à d’autres fins que celles de la mission de service public. Lorsque ces informations contiennent des données à caractère personnel, c’est-à-dire des éléments qui permettent d’identifier, directement ou indirectement, une personne physique, leur réutilisation est étroitement encadrée par l’article L322-2 du code des relations entre le public et l’administration. Cet article prévoit que la réutilisation d’une information publique contenant des données à caractère personnel est subordonnée au respect de la loi n° 78-17 du 6 janvier 1978, dite « Informatique et libertés ». Il en résulte notamment que lorsque les données personnelles que cette information publique contient ont, préalablement à leur diffusion, fait l’objet d’une anonymisation totale ou partielle, conformément à des dispositions légales ou aux recommandations de la Commission nationale de l’informatique et des libertés (CNIL), la réutilisation ne peut avoir pour objet ou pour effet de réidentifier les personnes concernées. »</p>
+
+              <p>Interdiction des traitements visant à la réidentification des magistrats et greffiers :</p>
+
+              <p>L’article L 111-13 du COJ dispose que « Les données d'identité des magistrats et des membres du greffe ne peuvent faire l'objet d'une réutilisation ayant pour objet ou pour effet d'évaluer, d'analyser, de comparer ou de prédire leurs pratiques professionnelles réelles ou supposées. La violation de cette interdiction est punie des peines prévues aux articles 226-18,226-24 et 226-31 du code pénal, sans préjudice des mesures et sanctions prévues par la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés. »</p>
+
+
+              <h2>5 - Base juridique du traitement</h2>
+
+              <p>La base légale du traitement des données personnelles insérées dans les décisions de jurisprudence diffusées sur le site www.juricaf.org est l’article 6-1-e du RGPD : « le traitement est nécessaire à l’exécution d’une mission d’intérêt public […] dont est investi le responsable de traitement »</p>
+
+              <p>L’AHJUCAF est une association regroupant les Hautes Cours de Justice de 48 Etats, ayant en commun le français. Le secrétariat général est actuellement assuré par la Cour de cassation française.</p>
+
+              <p>L’article 4 des statuts de l’AHJUCAF fixe ses missions : « favoriser l’entraide, la solidarité, la coopération, les échanges d’idées et d’expériences entre les institutions judiciaires membres sur les questions relevant de leur compétence ou intéressant leur organisation et leur fonctionnement ; promouvoir le rôle des Hautes Juridictions dans la consolidation de l’Etat de droit, le renforcement de la sécurité juridique, la régulation des décisions judiciaires et l’harmonisation du droit au sein des Etats membres. »
+
+              <p>Dans le cadre de ces missions, l’AHJUCAF a développé la base de données Juricaf, une base de décisions de jurisprudence des Etats membres de l’association ainsi que d’organisation internationale. Toutes les décisions sont en français. Cette base de données participe directement à la consolidation de l’Etat de droit, le renforcement de la sécurité juridique, la régulation des décisions judiciaires et l’harmonisation du droit au sein des Etats membres.</p>
+
+              <h2>6 - Vos droits sur les données personnelles diffusées sur le site <a href="https://www.juricaf.org">www.juricaf.org</a></h2>
+
+              <p>Vous disposez d’un droit d’accès, de rectification, d’effacement de vos données ou encore de limitation de traitement. Vous pouvez également, pour des motifs légitimes, vous opposer au traitement des données vous concernant.</p>
+
+              <p>Vous pouvez exercer vos droits en contactant le secrétariat général de l’AHJUCAF.</p>
+
+              <p>Par courrier postal :</p>
+              
+              <p>AHJUCAF, 5 quai de l’Horloge, 75001 Paris</p>
+              
+              <p>Ou par courriel : sgahjucaf@ahjucaf.org</p>
+              
+              <p>Il est également possible d’introduire une réclamation (ou plainte) auprès de la CNIL. Cette page du site internet de la CNIL explique les modalités : https://www.cnil.fr/fr/cnil-direct/question/adresser-unereclamation-plainte-la-cnil-quelles-conditions-et-comment</p>
+              <p>Vous pouvez l’adresser par courrier postal en écrivant à : CNIL ; 3 Place de Fontenoy ; TSA 80715 ; 75334 PARIS CEDEX 07.</p>
+
+              <p>Vous pouvez également adresser votre plainte via le téléservice de plainte en ligne :
+              <a href="https://www.cnil.fr/fr/plaintes">https://www.cnil.fr/fr/plaintes</a></p>
+
+              <h2>7 - Transfert de données personnelles à destination d’un Etat non membre de l’Union Européenne</h2>
+
+              <p>Aucun transfert de données à caractère personnel n’est effectué vers un Etat non membre de l’Union Européenne.</p>
+
+              <h2>8 - Gestion des cookies</h2>
+
+              <p>Aucun cookies n’est déposé par le site <a href="https://www.juricaf.org">www.juricaf.org</a>.</p>
+
+        <br/><br/><br/>
+        <small style="color:gray;"><i>Les  mentions légales et la politique de confidentialité de ce site ont été rédigées avec la collaboration de Maître Blandine Cornevin et Maître Irène Kris, réseau ELOKIA AVOCATS</i></small>
+
+          </div>
+        </div>
+      </div>
+      <div style="margin-top: 50px;">
+      <hr/>
+ 	 <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
+ 	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+       <div class="bottom">
+      <p>Juricaf est un projet de l'AHJUCAF, l'association des cours judiciaires suprêmes francophones,<br />
+         réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I).<br />
+         Il est soutenu par l'Organisation internationale de la Francophonie et le Fonds francophone des inforoutes.</p>
+       </div>
+     </div>
+    <script type="text/javascript">
+    <!--
+    function juricafSearch() {
+      if (window.external && ("AddSearchProvider" in window.external)) {
+        window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
+      }
+      else {
+         alert("Votre navigateur ne supporte pas cette fonctionnalité");
+      }
+    }
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-8802834-4']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    // -->
+    </script>
+  </body>
+</html>
