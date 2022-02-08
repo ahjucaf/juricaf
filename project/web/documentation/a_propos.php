44b029e8435ed75143d482d5cb95d86991948e8c
Honorine-MC <honorine.mariechristy@gmail.com>
2022-02-08 18:00:00 +0100

    footer seul + page à propos

--- project/web/documentation/a_propos.php
+++ project/web/documentation/a_propos.php
@@ -1,126 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="title" content="Juricaf" />
-    <meta name="description" content="La base de données de jurisprudence francophone" />
-    <meta name="keywords" content="jurisprudence, cassation, cour, suprême, francophone, francophonie, ahjucaf, arrêt, décision" />
-    <meta name="language" content="fr" />
-    <title>Juricaf - A propos</title>
-    <link rel="shortcut icon" href="/images/favicon.ico" />
-    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
-    <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
-    <script type="text/javascript" src="/js/jquery.scrollTo-min.js"></script>
-    <link rel="search" href="/juricaf.xml" title="Rechercher sur Juricaf" type="application/opensearchdescription+xml" />
-  </head>
-  <body>
-    <div class="site">
-      <div class="head">
-        <div class="menu">
-          <ul>
-            <li><a href="http://www.juricaf.org/documentation/a_propos.php">A propos</a></li>
-            <li><a href="http://www.juricaf.org/documentation/stats/statuts.php">Etendue des collections</a></li>
-            <li><a href="http://www.juricaf.org/documentation/partenaires.php">Partenaires</a></li>
-            <li><a href="http://www.juricaf.org/documentation/mentions_legales.php">Mentions légales</a></li>
-	    <li><a href="http://www.juricaf.org/documentation/contact.php">Contact</a></li>
-			
-            </ul>
-        </div>
-      </div>
-	  <div class="reseaux_sociaux">
+<?php include("header.php");?>
+    <div class="arret container text-justify mt-5">
 
-	   <a href="https://www.facebook.com/AHJUCAFCoursSupremesJudiciairesFrancophones" target="_blank"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook"/></a>
-          <a href="https://twitter.com/ahjucaf" target="_blank"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter"/></a>
-	 </div>
-      <div class="main">
-        <div class="content">
-          <div class="form_recherche">
-            <form method="get" action="/recherche">
-            <table summary="Rechercher">
-              <tr>
-                <td>
-                  <a href="http://www.juricaf.org/recherche"><img id="logo" src="/images/juricaf.png" alt="Juricaf" /></a><br />
-                  <span style="font-family: Georgia; font-style: italic; color: #4E4C4D;">La jurisprudence francophone des cours suprêmes</span><br />
-                  <input type="text" style="width: 300px; margin-top: 5px;" name="q" value="" tabindex="10" /><br />
-                  <input type="submit" value="Rechercher" tabindex="20" /> <a href="/recherche_avancee">recherche avancée</a>
-                </td>
-              </tr>
-            </table>
-            </form>
-          </div>
-          <div class="arret">
-              
-              <h1>À Propos</h1>
-              
-              <p>La base JURICAF est destinée en priorité au monde francophone, mais aussi aux ressortissants des pays de Common Law afin qu’ils puissent avoir accès à la jurisprudence rendue en français.</p>
-              
-              <p>Cette base de jurisprudence appartient à l’<a href="https://www.ahjucaf.org/">association AHJUCAF</a> et résulte de la contribution régulière des Cours suprêmes adhérentes de l’AHJUCAF et pratiquant l’usage du français.</p>
-              
-              <p>Sous la responsabilité du Secrétaire général de l’AHJUCAF, le fonctionnement et l’alimentation du sitont assurés par une équipe animée par un greffier de la Cour de cassation française qui veille à son alimentation régulière. Un webmestre assure l’hébergement sécurisé des sites JURICAF et AHJUCAF.</p>
+        <h5 class="p-3 mb-2 bg-secondary bg-gradient">À Propos</h4>
 
-              <p>Depuis septembre 2016, M. Jean-Paul Jean Président de chambre honoraire à la Cour de cassation de France assure les fonctions de Secrétaire général de l’AHJUCAF. Une réorganisation des processus de récupération et de traitement des décisions a été conduite avec le concours de partenaires attachés depuis l’origine au projet JURICAF, les animateurs des sociétés 24ème et La Loi des Ours, ainsi que l’éditeur Lexbase. La reprise de l’alimentation régulière et la stratégie de développement ont permis à JURICAF de dépasser au cours de l’année 2020 le million d’arrêts intégrés. </p>
+        <p>La base JURICAF est destinée en priorité au monde francophone, mais aussi aux ressortissants des pays de Common Law afin qu’ils puissent avoir accès à la jurisprudence rendue en français.</p>
 
-              <p>Dans le même temps JURICAF a été mis aux normes RGPD. La quasi-totalité de ses fonds sont désormais pseudonymisés, sauf pour quelques situations très particulières de Cours suprêmes non soumises à cette exigence (voir la page <a href="mentions_legales.php">Mentions légales</a>)</p>
+        <p>Cette base de jurisprudence appartient à l’<a href="https://www.ahjucaf.org/">association AHJUCAF</a> et résulte de la contribution régulière des Cours suprêmes adhérentes de l’AHJUCAF et pratiquant l’usage du français.</p>
 
-              <p>Les arrêts des Cours suprêmes de pays francophones de l’hémisphère nord (France, Belgique, Luxembourg, Canada) sont intégrés automatiquement tous les mois. Début 2021 le fonds des arrêts CEDH a été mis à jour avec les décisions de 2013 à 2021.</p>
+        <p>Sous la responsabilité du Secrétaire général de l’AHJUCAF, le fonctionnement et l’alimentation du sitont assurés par une équipe animée par un greffier de la Cour de cassation française qui veille à son alimentation régulière. Un webmestre assure l’hébergement sécurisé des sites JURICAF et AHJUCAF.</p>
 
-              <p>Plus de la moitié des Cours suprêmes d’Afrique francophone font parvenir leurs décisions au secrétariat de l’AHJUCAF, de manière plus régulière et sous un format normalisé. D’autres pays membres de l’AHJUCAF dont les arrêts ne sont pas rendus en français (par exemple l’Albanie, la Bulgarie, la Roumanie, les pays de langue arabe) transmettent régulièrement des arrêts traduits. Des séminaires de formation sont organisés avec les correspondants de l’AHJUCAF, facilitant ainsi la transmission régulière de jurisprudences sous une forme compatible.</p>
+        <p>Depuis septembre 2016, M. Jean-Paul Jean Président de chambre honoraire à la Cour de cassation de France assure les fonctions de Secrétaire général de l’AHJUCAF. Une réorganisation des processus de récupération et de traitement des décisions a été conduite avec le concours de partenaires attachés depuis l’origine au projet JURICAF, les animateurs des sociétés 24ème et La Loi des Ours, ainsi que l’éditeur Lexbase. La reprise de l’alimentation régulière et la stratégie de développement ont permis à JURICAF de dépasser au cours de l’année 2020 le million d’arrêts intégrés. </p>
+
+        <p>Dans le même temps JURICAF a été mis aux normes RGPD. La quasi-totalité de ses fonds sont désormais pseudonymisés, sauf pour quelques situations très particulières de Cours suprêmes non soumises à cette exigence (voir la page <a href="mentions_legales.php">Mentions légales</a>)</p>
+
+        <p>Les arrêts des Cours suprêmes de pays francophones de l’hémisphère nord (France, Belgique, Luxembourg, Canada) sont intégrés automatiquement tous les mois. Début 2021 le fonds des arrêts CEDH a été mis à jour avec les décisions de 2013 à 2021.</p>
+
+        <p>Plus de la moitié des Cours suprêmes d’Afrique francophone font parvenir leurs décisions au secrétariat de l’AHJUCAF, de manière plus régulière et sous un format normalisé. D’autres pays membres de l’AHJUCAF dont les arrêts ne sont pas rendus en français (par exemple l’Albanie, la Bulgarie, la Roumanie, les pays de langue arabe) transmettent régulièrement des arrêts traduits. Des séminaires de formation sont organisés avec les correspondants de l’AHJUCAF, facilitant ainsi la transmission régulière de jurisprudences sous une forme compatible.</p>
 
 
-              <h1>Histoire de JURICAF</h1>
-              
-              <p>JURICAF est un projet de l'AHJUCAF, l'association des Cours judiciaires suprêmes francophones, réalisé initialement en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I), avec le soutien de l'Organisation internationale de la Francophonie.</p>
-              <p>L’AHJUCAF a décidé de mettre à disposition des professionnels du droit et des citoyens de l’espace judiciaire francophone une base de jurisprudence permettant un accès gratuit aux décisions de justice rédigées ou traduites en français. La diffusion du droit francophone vient ainsi renforcer l’État de droit et la démocratie.</p>
-              <p>A la fin des années 2000, les États francophones avaient pris un retard important dans le développement de bases de données juridiques disponibles sur internet. Si en Europe, la plupart des hautes juridictions disposaient d’un tel outil, dans les pays d'Afrique, Caraïbes et Pacifique, aucune institution ne diffusait gratuitement sa jurisprudence, contrairement aux pays de Common Law où les décisions de justice des Final Court of Appeal étaient déjà accessibles sur Internet.</p>
-              <p>Nombre d’avocats, de citoyens, de défenseurs des droits de l’homme, n’avaient pas accès aux décisions de justice et ne pouvaient connaître le verdict d’affaires jugées. Dans le domaine économique, nombre d’arrêts de la Cour Commune de Justice et d’Arbitrage de l’OHADA étaient accessibles, mais avec une mise à jour incertaine et une publication ne répondant pas aux normes documentaires, retardant ainsi leur exploitation par les avocats.</p>
-              <p>En juin 2010, après six ans d’existence sur Internet, l’Assemblée générale de l’AHJUCAF a décidé une remise à niveau ambitieuse sur le plan documentaire et technique de sa base de données JURICAF qui respecte les standards mondiaux, européens et francophones.</p>
-              <p>L’AHJUCAF soutient et accompagne désormais les services de documentation des Cours suprêmes africaines pour qu’elles se dotent progressivement des technologies informatiques et documentaires et permettre ainsi la diffusion de leur jurisprudence à l’échelle mondiale.</p>
-              <p>Le VIème Congrès de l’AHJUCAF tenu à Beyrouth les 13-14 juin 2019 sur le thème « <a href="https://www.ahjucaf.org/node/356">La diffusion de la jurisprudence des Cours suprêmes judiciaires au temps d’internet</a> », a décidé  une stratégie de large diffusion de la jurisprudence francophone, traduite par une déclaration commune.</p>
-              <p>Les <a href="https://www.ahjucaf.org/sites/default/files/beyrouth%20recommendation.pdf">Recommandations adoptées à Beyrouth par les Cours suprêmes judiciaires de la Francophonie</a>, au nom de leurs valeurs communes et au moment de l’ouverture croissante des données publiques, ont fixé comme action prioritaire la diffusion par internet de la jurisprudence francophone numérisée, afin de faciliter les échanges de la communauté internationale des juristes et le développement de l’Etat de droit, au service des citoyens pour faciliter la connaissance et l’exercice effectifs de leurs droits dans une société démocratique.</p>
-              
-              <p>Télécharger l’ouvrage «&nbsp;<a href="https://www.ahjucaf.org/sites/default/files/AHJUCAF%20Congr%C3%A8s%20Beyrouth%20Actes%20mars%202020.pdf">La diffusion de la jurisprudence des Cours suprêmes judiciaires dela francophonie au temps d’internet</a>&nbsp;»</p>
+        <h5 class="p-3 mb-2 bg-secondary bg-gradient">Histoire de JURICAF</h5>
 
-              <h2>L’AHJUCAF présente ses remerciements aux acteurs qui ont contribué à création et à la refonte du site de la base de données JURICAF :</h2>
-                  <p> - Cour de cassation : M. le Président Jean-Louis Gillet, M. Edouard Rottier, M. Guillaume Adréani, M. Mehdi Ben Mimoun</p>
-                  <p> - Partenaire public : Université de Paris 1 CAVEJ, Bruno Dondéro, M. Jean Gasnault, M. Stéphane Cottin</p>
-                  <p> - Partenaires privés : M. Tangui Morlier, M. Brice Person</p>
-          </div>
-        </div>
-      </div>
-      <div style="margin-top: 50px;">
-      <hr/>
- 	 <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
- 	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
-       <div class="bottom">
-       <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
-      initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
-      Il est soutenu par l'Organisation Internationale de la Francophonie.</p>
+        <p>JURICAF est un projet de l'AHJUCAF, l'association des Cours judiciaires suprêmes francophones, réalisé initialement en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I), avec le soutien de l'Organisation internationale de la Francophonie.</p>
+        <p>L’AHJUCAF a décidé de mettre à disposition des professionnels du droit et des citoyens de l’espace judiciaire francophone une base de jurisprudence permettant un accès gratuit aux décisions de justice rédigées ou traduites en français. La diffusion du droit francophone vient ainsi renforcer l’État de droit et la démocratie.</p>
+        <p>A la fin des années 2000, les États francophones avaient pris un retard important dans le développement de bases de données juridiques disponibles sur internet. Si en Europe, la plupart des hautes juridictions disposaient d’un tel outil, dans les pays d'Afrique, Caraïbes et Pacifique, aucune institution ne diffusait gratuitement sa jurisprudence, contrairement aux pays de Common Law où les décisions de justice des Final Court of Appeal étaient déjà accessibles sur Internet.</p>
+        <p>Nombre d’avocats, de citoyens, de défenseurs des droits de l’homme, n’avaient pas accès aux décisions de justice et ne pouvaient connaître le verdict d’affaires jugées. Dans le domaine économique, nombre d’arrêts de la Cour Commune de Justice et d’Arbitrage de l’OHADA étaient accessibles, mais avec une mise à jour incertaine et une publication ne répondant pas aux normes documentaires, retardant ainsi leur exploitation par les avocats.</p>
+        <p>En juin 2010, après six ans d’existence sur Internet, l’Assemblée générale de l’AHJUCAF a décidé une remise à niveau ambitieuse sur le plan documentaire et technique de sa base de données JURICAF qui respecte les standards mondiaux, européens et francophones.</p>
+        <p>L’AHJUCAF soutient et accompagne désormais les services de documentation des Cours suprêmes africaines pour qu’elles se dotent progressivement des technologies informatiques et documentaires et permettre ainsi la diffusion de leur jurisprudence à l’échelle mondiale.</p>
+        <p>Le VIème Congrès de l’AHJUCAF tenu à Beyrouth les 13-14 juin 2019 sur le thème « <a href="https://www.ahjucaf.org/node/356">La diffusion de la jurisprudence des Cours suprêmes judiciaires au temps d’internet</a> », a décidé  une stratégie de large diffusion de la jurisprudence francophone, traduite par une déclaration commune.</p>
+        <p>Les <a href="https://www.ahjucaf.org/sites/default/files/beyrouth%20recommendation.pdf">Recommandations adoptées à Beyrouth par les Cours suprêmes judiciaires de la Francophonie</a>, au nom de leurs valeurs communes et au moment de l’ouverture croissante des données publiques, ont fixé comme action prioritaire la diffusion par internet de la jurisprudence francophone numérisée, afin de faciliter les échanges de la communauté internationale des juristes et le développement de l’Etat de droit, au service des citoyens pour faciliter la connaissance et l’exercice effectifs de leurs droits dans une société démocratique.</p>
+
+        <p>Télécharger l’ouvrage «&nbsp;<a href="https://www.ahjucaf.org/sites/default/files/AHJUCAF%20Congr%C3%A8s%20Beyrouth%20Actes%20mars%202020.pdf">La diffusion de la jurisprudence des Cours suprêmes judiciaires dela francophonie au temps d’internet</a>&nbsp;»</p>
+
+        <h5>L’AHJUCAF présente ses remerciements aux acteurs qui ont contribué à création et à la refonte du site de la base de données JURICAF :</h5>
+            <p> - Cour de cassation : M. le Président Jean-Louis Gillet, M. Edouard Rottier, M. Guillaume Adréani, M. Mehdi Ben Mimoun</p>
+            <p> - Partenaire public : Université de Paris 1 CAVEJ, Bruno Dondéro, M. Jean Gasnault, M. Stéphane Cottin</p>
+            <p> - Partenaires privés : M. Tangui Morlier, M. Brice Person</p>
     </div>
-     </div>
-    </div>
-    <script type="text/javascript">
-    <!--
-    function juricafSearch() {
-      if (window.external && ("AddSearchProvider" in window.external)) {
-        window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
-      }
-      else {
-         alert("Votre navigateur ne supporte pas cette fonctionnalité");
-      }
-    }
+  </div>
+</div>
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-8802834-4']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-    // -->
-    </script>
-  </body>
-</html>
-
-
+    <?php include("footer.php") ?>
---
+++ project/web/documentation/footer.php
@@ -0,0 +1,35 @@
+<div class="container"style="margin-top: 50px;">
+<hr/>
+<img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
+<img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+ <div class="bottom">
+ <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
+initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
+Il est soutenu par l'Organisation Internationale de la Francophonie.</p>
+</div>
+</div>
+</div>
+<script type="text/javascript">
+<!--
+function juricafSearch() {
+if (window.external && ("AddSearchProvider" in window.external)) {
+  window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
+}
+else {
+   alert("Votre navigateur ne supporte pas cette fonctionnalité");
+}
+}
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-8802834-4']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+// -->
+</script>
+</body>
+</html>
