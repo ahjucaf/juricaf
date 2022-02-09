a590a1793efd0a709f19e2c8d9eb5f6b69935259
Honorine-MC <honorine.mariechristy@gmail.com>
2022-02-09 12:04:38 +0100

    page contact responsive

--- project/web/documentation/contact.php
+++ project/web/documentation/contact.php
@@ -10,32 +10,31 @@ $_SESSION['cap2'] = intval(rand(0, 10) + 1);
 <?php include("header.php") ?>
     <div class="arret container text-justify mt-5">
 		   <h5 class="p-3 mb-2 bg-secondary bg-gradient">Formulaire de contact</h5>
-
             <form action="form2mail.php" method="post">
-              <table>
-                <tr class="mt-">
-                  <td>Entrez votre adresse mail :</td>
-                  <td><input type="text" name="email" /></td>
-                </tr>
-                <tr>
-                  <td style="vertical-align: top;">Message :</td>
-                  <td>
-                    <textarea name="message" rows="8" cols="50"></textarea>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Captcha :</td>
-                  <td><?php echo $_SESSION['cap1']; ?> + <?php echo $_SESSION['cap2']; ?> = <input type="text" name="captcha" size=4 /></td>
-                </tr>
-                <tr>
-                  <td colspan="2">
-                    <input name="token" type="hidden" value="<?php echo $token; ?>" />
-                    <input type="checkbox" required=required/>  L’AHJUCAF traite les données recueillies pour la gestion des commentaires, avis et questions déposés par les usagers par le biais de ce formulaire<br/>
-                    <br/>
-                    <input type="submit" value="Envoi" />
-                  </td>
-                </tr>
-              </table>
+               <div class="form-group row">
+                  <label class="col-sm-2 col-form-label"> Email:</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="email" class="form-control" id="inputPassword" placeholder="Entrez votre adresse mail">
+                  </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Message:</label>
+                <div class="col-sm-10">
+                  <textarea name="message" class="form-control" rows="8" cols="50"></textarea>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-2 col-form-label">Captcha:</label>
+                <div class="col-sm-10">
+                  <?php echo $_SESSION['cap1']; ?> + <?php echo $_SESSION['cap2']; ?> = <input type="text" name="captcha" size=4 />
+                </div>
+              </div>
+              <div class="form-check">
+                <input name="token" type="hidden" value="<?php echo $token; ?>" />
+                <input type="checkbox" class="form-check-input" required=required/>
+                <label class="form-check-label"> L’AHJUCAF traite les données recueillies pour la gestion des commentaires, avis et questions déposés par les usagers par le biais de ce formulaire</label>
+              </div>
+              <button type="submit" class="btn btn-primary mt-3" value="Envoi">Envoyer</button>
             </form>
           </div>
         </div>
