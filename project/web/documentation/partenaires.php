2100903d466235fddc4dbacf56e1ee8a4109d846
Honorine-MC <honorine.mariechristy@gmail.com>
2022-02-09 15:24:50 +0100

    responsive page partenaires

--- project/web/documentation/partenaires.php
+++ project/web/documentation/partenaires.php
@@ -1,81 +1,72 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="title" content="Juricaf" />
-<meta name="description" content="La base de données de jurisprudence francophone" />
-<meta name="keywords" content="jurisprudence, cassation, cour, suprême, francophone, francophonie, ahjucaf, arrêt, décision" />
-<meta name="language" content="fr" />
-    <title>Juricaf - Partenaires</title>
-    <link rel="shortcut icon" href="/images/favicon.ico" />
-    <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
+<?php include("header.php")?>
+          <div class="container text-justify mt-5">
+            <h5 class="p-3 mb-2 bg-secondary bg-gradient">Partenaires institutionnels</h5>
 
-    <script type="text/javascript" src="/js/jquery-1.6.1.min.js"></script>
-<script type="text/javascript" src="/js/jquery.scrollTo-min.js"></script>
-    <link rel="search" href="/juricaf.xml" title="Rechercher sur Juricaf" type="application/opensearchdescription+xml" />
-  </head>
-  <body>
-    <div class="site">
-      <div class="head">
-        <div class="menu">
-          <ul>
-            <li><a href="http://www.juricaf.org/documentation/a_propos.php">A propos</a></li>
-            <li><a href="http://www.juricaf.org/documentation/stats/statuts.php">Etendue des collections</a></li>
-            <li><a href="http://www.juricaf.org/documentation/partenaires.php">Partenaires</a></li>
-            <li><a href="http://www.juricaf.org/documentation/mentions_legales.php">Mentions légales</a></li>
-	    <li><a href="http://www.juricaf.org/documentation/contact.php">Contact</a></li>
-			
-            </ul>
-        </div>
-      </div>
-	  <div class="reseaux_sociaux">
-    <a href="https://www.facebook.com/AHJUCAFCoursSupremesJudiciairesFrancophones" target="_blank"><img src="/images/facebook.png" alt="Facebook" title="Devenez fan sur Facebook"/></a>
-          <a href="https://twitter.com/ahjucaf" target="_blank"><img src="/images/twitter.png" alt="Twitter" title="Suivez nous sur Twitter"/></a>
-	 
-	   </div>
-      <div class="main">
-        <div class="content">
-          <div class="form_recherche">
-            <form method="get" action="/recherche">
-            <table summary="Rechercher">
-              <tr>
-                <td>
-                  <a href="http://www.juricaf.org/recherche"><img id="logo" src="/images/juricaf.png" alt="Juricaf" /></a><br />
-                  <span style="font-family: Georgia; font-style: italic; color: #4E4C4D;">La jurisprudence francophone des cours suprêmes</span><br />
-                  <input type="text" style="width: 300px; margin-top: 5px;" name="q" value="" tabindex="10" /><br />
-                  <input type="submit" value="Rechercher" tabindex="20" /> <a href="/recherche_avancee">recherche avancée</a>
-                </td>
-              </tr>
-            </table>
-            </form>
-          </div>
-          <div class="arret">
-            <h1>Partenaires institutionnels</h1>
-			
             <p>Juricaf est soutenu par <a href="#francophonie"><a href="http://www.francophonie.org/">l'Organisation internationale de la Francophonie</a>.</p>
 			<p style="text-align: center;"><a href="http://www.francophonie.org/"><img src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a></p>
-						
-			
-            <h1>Les cours suprêmes membres de l’AHJUCAF</h1>
-
-            <table summary="Les cours suprêmes membres de l’AHJUCAF">
-            <tr><td><img src="/images/drapeaux/Belgique.png" alt="Belgique" />&nbsp;Cour de cassation de Belgique</td></tr><tr><td><img src="/images/drapeaux/B%C3%A9nin.png" alt="Bénin" />&nbsp;Cour suprême du Bénin</td><td><img src="/images/drapeaux/Bulgarie.png" alt="Bulgarie" />&nbsp;Cour suprême de Bulgarie</td></tr><tr><td><img src="/images/drapeaux/Burkina_Faso.png" alt="Burkina_Faso" />&nbsp;Cour de cassation du Burkina Faso</td><td><img src="/images/drapeaux/Burundi.png" alt="Burundi" />&nbsp;Cour suprême du Burundi</td></tr><tr><td><img src="/images/drapeaux/Cambodge.png" alt="Cambodge" />&nbsp;Cour suprême du Cambodge</td><td><img src="/images/drapeaux/Cameroun.png" alt="Cameroun" />&nbsp;Cour suprême du Cameroun</td></tr><tr><td><img src="/images/drapeaux/Canada.png" alt="Canada" />&nbsp;Cour suprême du Canada</td><td><img src="/images/drapeaux/CEMAC.png" alt="CEMAC" />&nbsp;Cour de justice de la CEMAC</td></tr><tr><td><img src="/images/drapeaux/Congo.png" alt="Congo" />&nbsp;Cour de justice du Congo</td><td><img src="/images/drapeaux/Congo_d%C3%A9mocratique.png" alt="Congo_démocratique" />&nbsp;Cour suprême de justice du Congo démocratique</td></tr><tr><td><img src="/images/drapeaux/C%C3%B4te_d_Ivoire.png" alt="Côte_d'Ivoire" />&nbsp;Cour suprême de Côte d'Ivoire</td></tr><tr><td><img src="/images/drapeaux/France.png" alt="France" />&nbsp;Cour de cassation de France</td><td><img src="/images/drapeaux/Gabon.png" alt="Gabon" />&nbsp;Cour de cassation du Gabon</td></tr><tr><td><img src="/images/drapeaux/Guin%C3%A9e.png" alt="Guinée" />&nbsp;Cour suprême de Guinée</td><td><img src="/images/drapeaux/Ha%C3%AFti.png" alt="Haïti" />&nbsp;Cour de cassation d'Haïti</td></tr><tr><td><img src="/images/drapeaux/Hongrie.png" alt="Hongrie" />&nbsp;Cour suprême de Hongrie</td><td><img src="/images/drapeaux/Liban.png" alt="Liban" />&nbsp;Cour de cassation du Liban</td></tr><tr><td><img src="/images/drapeaux/Madagascar.png" alt="Madagascar" />&nbsp;Cour suprême de Madagascar</td></tr><tr><td><img src="/images/drapeaux/Mali.png" alt="Mali" />&nbsp;Cour suprême du Mali</td><td><img src="/images/drapeaux/Maroc.png" alt="Maroc" />&nbsp;Cour suprême du Maroc</td></tr><tr><td><img src="/images/drapeaux/Mauritanie.png" alt="Mauritanie" />&nbsp;Cour suprême de Mauritanie</td><td><img src="/images/drapeaux/Monaco.png" alt="Monaco" />&nbsp;Cour de révision de Monaco</td></tr><tr><td><img src="/images/drapeaux/Niger.png" alt="Niger" />&nbsp;Cour d'Etat du Niger</td></tr><tr><td><img src="/images/drapeaux/OHADA.png" alt="OHADA" />&nbsp;Cour commune de justice et d'arbitrage de l'OHADA</td><td><img src="/images/drapeaux/Pologne.png" alt="Pologne" />&nbsp;Cour suprême de Pologne</td></tr><tr><td><img src="/images/drapeaux/R%C3%A9publique_Tch%C3%A8que.png" alt="République_Tchèque" />&nbsp;Cour suprême de République Tchèque</td><td><img src="/images/drapeaux/Roumanie.png" alt="Roumanie" />&nbsp;Haute Cour de justice et de cassation de Roumanie</td></tr><tr><td><img src="/images/drapeaux/Rwanda.png" alt="Rwanda" />&nbsp;Cour suprême du Rwanda</td><td><img src="/images/drapeaux/Sao_Tom%C3%A9_et_Principe.png" alt="Sao_Tomé_et_Principe" />&nbsp;Cour suprême de Sao Tomé et Principe</td></tr><tr><td><img src="/images/drapeaux/S%C3%A9n%C3%A9gal.png" alt="Sénégal" />&nbsp;Cour suprême du Sénégal</td><td><img src="/images/drapeaux/Suisse.png" alt="Suisse" />&nbsp;Tribunal fédéral suisse</td></tr><tr><td><img src="/images/drapeaux/Tchad.png" alt="Tchad" />&nbsp;Cour suprême du Tchad</td><td><img src="/images/drapeaux/Togo.png" alt="Togo" />&nbsp;Cour suprême du Togo</td></tr><tr><td><img src="/images/drapeaux/Tunisie.png" alt="Tunisie" />&nbsp;Cour de cassation de Tunisie</td><td><img src="/images/drapeaux/UEMOA.png" alt="UEMOA" />&nbsp;Cour de justice de l'UEMOA</td></tr><tr><td><img src="/images/drapeaux/Vanuatu.png" alt="Vanuatu" />&nbsp;Cour suprême du Vanuatu</td><td><img src="/images/drapeaux/Vietnam.png" alt="Vietnam" />&nbsp;Cour suprême du Vietnam</td></tr>
-            </table>
-
-            L’AHJUCAF ouvre actuellement des partenariats avec des universités françaises et francophones.
 
 
-            <h1>Partenaires privés</h1>
+            <h5 class="p-3 mb-2 bg-secondary bg-gradient">Les cours suprêmes membres de l’AHJUCAF</h5>
 
-            <h2>Entreprises / Structures professionnelles</h2>
+            <div class="container">
+                <div class="row">
+                   <div class="col-lg-6"><img src="/images/drapeaux/Belgique.png" alt="Belgique" />&nbsp;Cour de cassation de Belgique</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/B%C3%A9nin.png" alt="Bénin" />&nbsp;Cour suprême du Bénin</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Bulgarie.png" alt="Bulgarie" />&nbsp;Cour suprême de Bulgarie</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Burkina_Faso.png" alt="Burkina_Faso" />&nbsp;Cour de cassation du Burkina Faso</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Burundi.png" alt="Burundi" />&nbsp;Cour suprême du Burundi</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Cambodge.png" alt="Cambodge" />&nbsp;Cour suprême du Cambodge</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Cameroun.png" alt="Cameroun" />&nbsp;Cour suprême du Cameroun</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Canada.png" alt="Canada" />&nbsp;Cour suprême du Canada</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/CEMAC.png" alt="CEMAC" />&nbsp;Cour de justice de la CEMAC</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Congo.png" alt="Congo" />&nbsp;Cour de justice du Congo</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Congo_d%C3%A9mocratique.png" alt="Congo_démocratique" />&nbsp;Cour suprême de justice du Congo démocratique</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/C%C3%B4te_d_Ivoire.png" alt="Côte_d'Ivoire" />&nbsp;Cour suprême de Côte d'Ivoire </div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/France.png" alt="France" />&nbsp;Cour de cassation de France</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Gabon.png" alt="Gabon" />&nbsp;Cour de cassation du Gabon</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Guin%C3%A9e.png" alt="Guinée" />&nbsp;Cour suprême de Guinée</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Ha%C3%AFti.png" alt="Haïti" />&nbsp;Cour de cassation d'Haïti</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Hongrie.png" alt="Hongrie" />&nbsp;Cour suprême de Hongrie</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Liban.png" alt="Liban" />&nbsp;Cour de cassation du Liban</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Madagascar.png" alt="Madagascar" />&nbsp;Cour suprême de Madagascar</div>
+                   <div class="col-lg-6"><img src="/images/drapeaux/Mali.png" alt="Mali" />&nbsp;Cour suprême du Mali</div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-6"><img src="/images/drapeaux/Maroc.png" alt="Maroc" />&nbsp;Cour suprême du Maroc</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Mauritanie.png" alt="Mauritanie" />&nbsp;Cour suprême de Mauritanie</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Monaco.png" alt="Monaco" />&nbsp;Cour de révision de Monaco</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Niger.png" alt="Niger" />&nbsp;Cour d'Etat du Niger</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/OHADA.png" alt="OHADA" />&nbsp;Cour commune de justice et d'arbitrage de l'OHADA</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Pologne.png" alt="Pologne" />&nbsp;Cour suprême de Pologne</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/R%C3%A9publique_Tch%C3%A8que.png" alt="République_Tchèque" />&nbsp;Cour suprême de République Tchèque</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Roumanie.png" alt="Roumanie" />&nbsp;Haute Cour de justice et de cassation de Roumanie</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Rwanda.png" alt="Rwanda" />&nbsp;Cour suprême du Rwanda</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Sao_Tom%C3%A9_et_Principe.png" alt="Sao_Tomé_et_Principe" />&nbsp;Cour suprême de Sao Tomé et Principe</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/S%C3%A9n%C3%A9gal.png" alt="Sénégal" />&nbsp;Cour suprême du Sénégal</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Suisse.png" alt="Suisse" />&nbsp;Tribunal fédéral suisse</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Tchad.png" alt="Tchad" />&nbsp;Cour suprême du Tchad</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Togo.png" alt="Togo" />&nbsp;Cour suprême du Togo</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Tunisie.png" alt="Tunisie" />&nbsp;Cour de cassation de Tunisie</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/UEMOA.png" alt="UEMOA" />&nbsp;Cour de justice de l'UEMOA</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Vanuatu.png" alt="Vanuatu" />&nbsp;Cour suprême du Vanuatu</div>
+                  <div class="col-lg-6"><img src="/images/drapeaux/Vietnam.png" alt="Vietnam" />&nbsp;Cour suprême du Vietnam</div>
+                </div>
+            </div>
+
+            <p class="mt-2">L’AHJUCAF ouvre actuellement des partenariats avec des universités françaises et francophones.</p>
+
+
+            <h5 class="p-3 mb-2 bg-secondary bg-gradient">Partenaires privés</u></h5>
+
+            <h5 class="mt-3 mb-3"><u>Entreprises / Structures professionnelles :</u></h5>
 
             <p> - La Loi des Ours</p>
             <p> - Lexbase</p>
             <p> - 24ème</p>
             <p> - Le Réseau ALioka avocats</p>
 
-            <h2>Associations</h2>
-            
+            <h5 class="mt-3 mb-3"><u>Associations :</u></h5>
+
             <p>(Elles accompagnent les développements de JURICAF et rediffusent les informations venant de l’AHJUCAF)</p>
 
             <p> - Open Law</p>
@@ -84,38 +75,5 @@
           </div>
         </div>
       </div>
-     <div style="margin-top: 50px;">
-     <hr/>
-	 <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
-	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
-      <div class="bottom">
-      <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
-      initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
-      Il est soutenu par l'Organisation Internationale de la Francophonie.</p>
-    </div>
-    </div>
-    </div>
-    <script type="text/javascript">
-    <!--
-    function juricafSearch() {
-      if (window.external && ("AddSearchProvider" in window.external)) {
-        window.external.AddSearchProvider("http://www.juricaf.org/juricaf.xml");
-      }
-      else {
-         alert("Votre navigateur ne supporte pas cette fonctionnalité");
-      }
-    }
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-8802834-4']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-    // -->
-    </script>
-  </body>
-</html>
+<?php include("footer.php")?>
