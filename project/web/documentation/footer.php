2717ef4b64db10411e7e530b392884776f711cbf
Honorine-MC <honorine.mariechristy@gmail.com>
2022-02-09 13:24:26 +0100

    maj footer toute la largeur

--- project/web/documentation/footer.php
+++ project/web/documentation/footer.php
@@ -1,7 +1,10 @@
-<div class="container"style="margin-top: 50px;">
+<div >
+<div class="container">
 <hr/>
+
 <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+</div>
  <div class="bottom text-center mt-3 p-3">
  <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
 initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
