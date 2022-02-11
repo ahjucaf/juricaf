8800a3f8859d53a837844fcbc17db0522e7c50fc
Honorine-MC <honorine.mariechristy@gmail.com>
2022-02-11 10:49:59 +0100

    responsive page d'accueil

--- project/apps/frontend/modules/arret/templates/_statsPays.php
+++ project/apps/frontend/modules/arret/templates/_statsPays.php
@@ -6,32 +6,34 @@ function replaceBlank($str) {
 function pathToFlag($str) {
   return urlencode(str_replace("'", '_', replaceBlank($str)));
 }
-
 $cpt = 0;
 ?>
 <div class="pays">
 	<h3 style="text-align: center;">Rechercher parmi <?php echo number_format($nb, 0, '', ' '); ?> décisions provenant de <?php echo count($pays); ?> pays et institutions francophones</h3>
 	<div class="payscols">
-	  <table style="margin: auto;"><tr><td><ul>
-		<?php
-        $max_per_col = intval(count($pays) / 3) + 1;
-		foreach ($pays as $p)
-		{
-		  $pays = preg_replace('/ /', '_', $p['key'][0]);
-		  if ($cpt % $max_per_col == 0 && $cpt) { echo '</td><td>'; }
-		  $cpt++;
-		  // Traitement du nom du pays : si trop long, le nom est coupé
-		  $pays_nom = $p['key'][0].' ('.number_format($p['value'], 0, '', ' ').')';
-		  $link = link_to($pays_nom,'recherche/search?query=+&facets=facet_pays:'.$pays);
-		  if (strlen($pays_nom) > 45) {
-			  $pays_nom_min = substr($pays_nom, 0, 16) . '...';
-			  $link = link_to($pays_nom_min,'recherche/search?query=+&facets=facet_pays:'.$pays);
-		  }
-          if (strlen($pays) > 0) {
-  		      echo '<li class="lipays"><img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.$link .'</li>';
-          }
+    <div class="container accueil-div text-justify p-5">
+      <div class="row">
+    <?php
+      $max_per_col = intval(count($pays) / 3) + 1;
+      foreach($pays as $p){
+        $pays = preg_replace('/ /', '_', $p['key'][0]);
+        if($cpt % $max_per_col == 0 && $cpt){
+          echo('</div><div class="row">');
         }
-		?>
-    </ul></td></tr><tr><td colspan="5" class="plus"><img src="images/+.png" alt="+" /> <a href="/documentation/stats/statuts.php">Plus de statistiques</a></td></tr></table>
+        $cpt++;
+        $pays_nom = $p['key'][0].' ('.number_format($p['value'], 0, '', ' ').')';
+        $link = link_to($pays_nom,'recherche/search?query=+&facets=facet_pays:'.$pays);
+        if (strlen($pays_nom) > 45) {
+          $pays_nom_min = substr($pays_nom, 0, 16) . '...';
+          $link = link_to($pays_nom_min,'recherche/search?query=+&facets=facet_pays:'.$pays);
+        }
+        if (strlen($pays) > 0) {
+          echo '<div class="col-lg-6"><img src="/images/drapeaux/'.pathToFlag(ucfirst($pays)).'.png" alt="'.$pays.'" />&nbsp;'.$link .'</div>';
+        }
+      }
+      ?>
 	</div>
+  <div class="text-center mt-3 p-3">
+    <img src="images/+.png" alt="+" /> <a href="/documentation/stats/statuts.php">Plus de statistiques</a>
+  </div>
 </div>
