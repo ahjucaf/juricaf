186fcb7257b6e4ecf0aebdc52f782231563b36c2
Honorine-MC <honorine.mariechristy@gmail.com>
2022-03-01 16:55:47 +0100

    bas de la page d'accueil plus petit

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -1,9 +1,9 @@
 <div class="stats">
 <?php include_component('arret', 'statsPays'); ?>
 	<div>
-	<center><h4><img src="images/iexpo.jpg" />
-	<p>Lauréat du prix I-Expo 2012 <br>catégorie "Open Data"
-	<p><a href="http://iall.org/webAward.html" target="_blank"><img src="images/iall.jpg" /></a></h4></center>
+	<center><h5><img src="images/iexpo.jpg" />
+	<p>Lauréat du prix I-Expo 2012 catégorie "Open Data"
+	<p><a href="http://iall.org/webAward.html" target="_blank"><img src="images/iall.jpg" /></a></h5></center>
 	</div>
 	<div style="clear:both;">&nbsp;</div>
 </div>
