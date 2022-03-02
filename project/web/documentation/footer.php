05b0224777e14ad6fb1d2201f9dc5b7adeda2c3b
Honorine-MC <honorine.mariechristy@gmail.com>
2022-03-02 17:36:12 +0100

    avancement footer à revoir

--- project/web/documentation/footer.php
+++ project/web/documentation/footer.php
@@ -1,28 +1,23 @@
 
-<div class="container">
-
-  <hr>
-  <div class="mt-3 text-center">
-    <a href="https://www.lemondedudroit.fr/interviews/66303-jean-paul-jean-ahjucaf.html" target="new">Interview de JP Jean secrétaire général de l’AHJUCAF dans « Le Monde du droit » sur l’accès à la jurisprudence francophone.</a><br><br><br>
-    <a href="http://www.ahjucaf.org/"><img src="/images/ahjucafSite.jpg" alt="Association des cours judiciaires suprêmes francophones" /></a>
-    <a href="http://www.ahjucaf.org/"><br/>Accédez au site de l’AHJUCAF </a>
-  </div>
 
 </div>
-</div>
 
 
- <div class="bottom text-center mt-3 p-3 row">
-   <div class="col-md-4">
-     <img src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones"/>
+<div class="bottom text-center mt-3 p-3">
+  <div class="row">
+    <div class="col-4 col-sm-4">
+      <img  src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones"/>
+    </div>
+    <div class="col-4 col-sm-4">
+      <div>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br/>
+       initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br/>
+       Il est soutenu par l'Organisation Internationale de la Francophonie.
+     </div>
+    </div>
+    <div class="col-4 col-sm-4">
+      <img src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+    </div>
   </div>
-   <div class="col-md-4 ml-auto">Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br/>
-    initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br/>
-    Il est soutenu par l'Organisation Internationale de la Francophonie.
-  </div>
-  <div class="col-md-4 ml-auto">
-  <img src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
-</div>
 </div>
 </div>
 </div>
