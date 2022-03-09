8dec6bc89d0f1aa39ad824a1480f2bcaf0a7edfc
Honorine-MC <honorine.mariechristy@gmail.com>
2022-03-09 16:17:40 +0100

    page de recherche avancée debut responsive

--- project/apps/frontend/modules/recherche/templates/_fullsearch.php
+++ project/apps/frontend/modules/recherche/templates/_fullsearch.php
@@ -41,37 +41,51 @@ jQuery(function($){
 -->
 </script>
 
-<h1>Recherche avancée</h1>
-<h2>Critères</h2>
+<div class="container">
+ <h5 class="p-3 mb-2 mt-5 bg-secondary bg-gradient">Recherche avancée</h5>
+
+ <h5><b>Critères</b></h5>
 <p>
-<select name="cr[1]">
-  <option value="content">Plein texte</option>
-  <option value="num_arret">Numéro d’affaire</option>
-  <option value="sens_arret">Sens</option>
-  <option value="nor">NOR</option>
-  <option value="urnlex">URN:LEX</option>
-  <option value="ecli">ECLI</option>
-  <option value="type_affaire">Type affaire</option>
-  <option value="type_recours">Type recours</option>
-  <option value="president">Président</option>
-  <option value="avocat_gl">Avocat général</option>
-  <option value="rapporteur">Rapporteur</option>
-  <option value="commissaire_gvt">Commissaire du gouvernement</option>
-  <option value="avocats">Avocat</option>
-  <option value="parties">Parties</option>
-  <option value="analyses">Analyses</option>
-  <option value="saisines">Saisine</option>
-  <option value="fonds_documentaire">Fonds documentaire</option>
-</select>
-<input type="text" name="val[1]" class="text_input" />
-<br />
-<select name="cond[1]">
+<div class="row">
+  <div class="col-lg-3">
+    <select class="form-select" name="cr[1]">
+      <option value="content">Plein texte</option>
+      <option value="num_arret">Numéro d’affaire</option>
+      <option value="sens_arret">Sens</option>
+      <option value="nor">NOR</option>
+      <option value="urnlex">URN:LEX</option>
+      <option value="ecli">ECLI</option>
+      <option value="type_affaire">Type affaire</option>
+      <option value="type_recours">Type recours</option>
+      <option value="president">Président</option>
+      <option value="avocat_gl">Avocat général</option>
+      <option value="rapporteur">Rapporteur</option>
+      <option value="commissaire_gvt">Commissaire du gouvernement</option>
+      <option value="avocats">Avocat</option>
+      <option value="parties">Parties</option>
+      <option value="analyses">Analyses</option>
+      <option value="saisines">Saisine</option>
+      <option value="fonds_documentaire">Fonds documentaire</option>
+    </select>
+  </div>
+  <div class="col-lg-9">
+    <input type="text" name="val[1]" class="text_input form-control" />
+  </div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[1]">
   <option value="AND">ET</option>
   <option value="OR">OU</option>
   <option value="NOT">SAUF</option>
 </select>
-<br />
-<select name="cr[2]">
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cr[2]">
   <option value="content">Plein texte</option>
   <option value="num_arret">Numéro d’affaire</option>
   <option value="sens_arret">Sens</option>
@@ -90,15 +104,25 @@ jQuery(function($){
   <option value="saisines">Saisine</option>
   <option value="fonds_documentaire">Fonds documentaire</option>
 </select>
-<input type="text" name="val[2]" class="text_input" />
-<br />
-<select name="cond[2]">
+</div>
+<div class="col-lg-9">
+<input type="text" name="val[2]" class="text_input form-control" />
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[2]">
   <option value="AND">ET</option>
   <option value="OR">OU</option>
   <option value="NOT">SAUF</option>
 </select>
-<br />
-<select name="cr[3]">
+</div>
+</div>
+<p></p>
+<div class="row">
+  <div class="col-lg-3">
+<select class="form-select" name="cr[3]">
   <option value="content">Plein texte</option>
   <option value="num_arret">Numéro d’affaire</option>
   <option value="sens_arret">Sens</option>
@@ -117,15 +141,25 @@ jQuery(function($){
   <option value="saisines">Saisine</option>
   <option value="fonds_documentaire">Fonds documentaire</option>
 </select>
-<input type="text" name="val[3]" class="text_input" />
-<br />
-<select name="cond[3]">
+</div>
+<div class="col-lg-9">
+<input type="text" name="val[3]" class="text_input form-control" />
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[3]">
   <option value="AND">ET</option>
   <option value="OR">OU</option>
   <option value="NOT">SAUF</option>
 </select>
-<br />
-<select name="cr[4]">
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cr[4]">
   <option value="content">Plein texte</option>
   <option value="num_arret">Numéro d’affaire</option>
   <option value="sens_arret">Sens</option>
@@ -144,29 +178,53 @@ jQuery(function($){
   <option value="saisines">Saisine</option>
   <option value="fonds_documentaire">Fonds documentaire</option>
 </select>
-<input type="text" name="val[4]" class="text_input" /><br />
-</p>
+</div>
+<div class="col-9">
+<input type="text" name="val[4]" class="text_input form-control"/>
+</div>
+</div>
+<p></p>
 
-<div style="float: right; padding-top: 0.6em; padding-bottom: 1em;"> <input type="reset" value="Effacer" /> <input type="submit" value="Valider" /></div>
+<div class="float-end mb-3">
+  <input class="btn btn-primary mt-3" type="reset" value="Effacer" />
+  <input class="btn btn-primary mt-3" type="submit" value="Valider" />
+</div>
+<hr style="clear: both;"/>
 
-<hr style="clear: both;" />
-<div class="calendars">
-<h2>Date de la décision</h2>
-<p>
-<input type="text" name="date[arret]" id="date_arret" size="10" /><label for="date_arret"><img src="/images/calendar.png" alt="" /></label>
-</p>
+<div class="row g-3 align-items-center">
+  <div class="col-auto">
+    <label class="col-form-label"><h5><b>Date de la décision : </b></h5></label>
+  </div>
+  <div class="col-auto">
+    <input class="form-control col-auto" type="date" name="date[arret]" id="date_arret" size="10">
+  </div>
 </div>
-<div class="calendars">
-<p>OU</p>
+OU
+<div class="row g-3 align-items-center">
+  <div class="col-auto">
+    <label class="col-form-label"><h5><b>Période du :</b></h5></label>
+  </div>
+  <div class="col-auto">
+    <input class="form-control col-auto" type="date" name="date[debut]" id="date_debut" size="10" />
+  </div>
+  <div class="col-auto">
+    <label class="col-form-label"><h5><b>au :</b></h5></label>
+  </div>
+  <div class="col-auto">
+    <input class="form-control col-auto" type="date" name="date[fin]" id="date_fin" size="10" />
+  </div>
 </div>
-<div class="calendars">
-<h2>Période</h2>
-<p>
-du <input type="text" name="date[debut]" id="date_debut" size="10" /><label for="date_debut"><img src="/images/calendar.png" alt="" /></label>
-au <input type="text" name="date[fin]" id="date_fin" size="10" /><label for="date_fin"><img src="/images/calendar.png" alt="" /></label>
-</p>
-</div>
-<hr style="clear: both;" />
-<h2>Références</h2>
-<p><input type="text" name="references" style="width:90.5%" /></p>
+
 <hr />
+
+<div class="row">
+  <div class="col-lg-3">
+    <label class="col-form-label"><h5><b>Références :</b></h5></label>
+  </div>
+  <div class="col-lg-9">
+    <input class="form-control" type="text" name="references" />
+  </div>
+</div>
+<hr />
+
+</div>
