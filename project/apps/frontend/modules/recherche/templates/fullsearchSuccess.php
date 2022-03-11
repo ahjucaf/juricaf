fd0c057d9cdb77941f82ce5ca630bef1a2f84859
Honorine-MC <honorine.mariechristy@gmail.com>
2022-03-11 17:17:18 +0100

    bouton effacer en light

--- project/apps/frontend/modules/recherche/templates/fullsearchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/fullsearchSuccess.php
@@ -8,7 +8,7 @@ if(!empty($filter)) { echo 'Filter : '.$filter; }
 <?php include_component('arret', 'searchPays'); ?>
 <hr />
 <div class="float-end mb-3">
-  <input class="btn btn-primary mt-3" type="reset" value="Effacer" />
+  <input class="btn btn-light mt-3" type="reset" value="Effacer" />
   <input class="btn btn-primary mt-3" type="submit" value="Valider" />
 </div>
 <hr style="clear: both;" />
