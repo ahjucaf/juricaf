8e2175845765e93a1e254327e66835970ea042b7
Honorine-MC <honorine.mariechristy@gmail.com>
2022-04-01 17:43:25 +0200

    commentaire du js

--- project/web/js/main.js
+++ project/web/js/main.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
+
+
+  // POUR LA PAGINATION
   pages = document.getElementsByClassName('page-item');
   for(let i=0; i< pages.length; i++){
     if(pages[i].children[0] && pages[i].children[0].innerHTML){
@@ -10,48 +13,43 @@ $( document ).ready(function() {
     }
   }
 
+  //SAVOIR SI ON EST EN MODE MOBILE GRACE A   <span id="is_mobile" class="d-lg-none"></span> qui se trouve dans le header
   var is_mobile = function() {
       return !(window.getComputedStyle(document.getElementById('is_mobile')).display === "none");
   };
 
-  $(document).change(function(){
-    if($("#juridiction").val() != ""){
-      pays = $("#juridiction :selected").data("pays");
-      $('#pays_filter').val(pays);
-    }
-  });
-
+  //SI ON EST EN MODE MOBILE
   if(is_mobile()){
-    if(document.getElementById("navbar")){
+    if(document.getElementById("navbar")){   //id dans le header. la barre de menu reste en position fixed.
       $("#navbar").addClass('fixed-top');
     }
-    if(document.getElementById("menu")){
+    if(document.getElementById("menu")){  //id dans le header. si on est page dans la page d'un arret alors on fait un margin-top de 100px (voir main.css pour mt-10)
       $('#menu').addClass("mt-10");
     }
-    if(document.getElementById("textArret")){
+    if(document.getElementById("textArret")){ //id dans la page d'un arret/templates/indexSuccess.php on cache le texte pour n'afficher que le texte début
       $("#textArret").addClass("collapse");
     }
-    if(document.getElementById('hidden-mode-mobile')){
+    if(document.getElementById('hidden-mode-mobile')){ //id dans le header si page d'un arret alors pas de barre de recherche
       $('#hidden-mode-mobile').addClass("d-none");
       $('#hidden-mode-mobile').addClass("d-lg-none");
     }
-    if(document.getElementById("btn-see-more")){
+    if(document.getElementById("btn-see-more")){  //id dans la page d'un arret/templates/indexSuccess.php si click sur le bouton voir plus d'un arrêt.
       $("#btn-see-more").click(function(){
           if($("#btn-see-more").text()=="Voir plus"){
             $("#btn-see-more").html("Voir moins");
-            $("#debutArret").addClass("d-sm-none");
+            $("#debutArret").addClass("d-sm-none"); //cache le texte debut "resume"
           }
           else{
             $("#btn-see-more").html("Voir plus");
-            $("#debutArret").removeClass("d-sm-none");
+            $("#debutArret").removeClass("d-sm-none");//affiche le texte debut "resume"
           }
       });
     }
-    if(document.getElementById('bloc-filtres')){
-      $("#bloc-filtres").addClass("collapse");
+    if(document.getElementById('bloc-filtres')){ //id dans la page des resultats d'une recherche recherche/templates/searchSuccess.php
+      $("#bloc-filtres").addClass("collapse"); //on cache les blocs des filtres.
     }
-    if(document.getElementById("open-filters")){
-      $("#open-filters").click(function(){
+    if(document.getElementById("open-filters")){ //id dans la page des resultats d'une recherche recherche/templates/searchSuccess.php
+      $("#open-filters").click(function(){ //changement de text
           if($("#open-filters").text()=="Ouvrir les filtres"){
             $("#open-filters").html("Fermer les filtres");
           }
@@ -60,6 +58,9 @@ $( document ).ready(function() {
           }
       });
     }
+
+    //class dans la page des resultats d'une recherche recherche/templates/searchSuccess.php
+    //permet de pouvoir cliquer sur une "card" div d'un arret pour aller sur la page de l'arret
     var arrets=document.getElementsByClassName("card-body");
     if(arrets){
       for(i=0;i<arrets.length;i++){
@@ -67,16 +68,26 @@ $( document ).ready(function() {
       }
     }
   }
+
+  //SI PAS en mode mobile => maj des résultats au changement du filtres
+  //en mode mobile obligé de cliquer sur le bouton filtrer.
   else{
     $(document).change(function(){
       $( "#filtrer" ).trigger( "click" );
     });
   }
+
+  //au choix de la juridiction le pays en question est mis dans le input du pays avant l'envoi du formulaire
+  $(document).change(function(){
+    if($("#juridiction").val() != ""){
+      pays = $("#juridiction :selected").data("pays");
+      $('#pays_filter').val(pays);
+    }
+  });
+
 });
 
-
-
-
+//fonction appelé au click du bouton "zoom" de la barre d'outils dans la page d'un arret.
 function fontSizePlus(){
   corps = document.getElementById('arret');
   size = window.getComputedStyle(corps).getPropertyValue("font-size");
@@ -84,6 +95,7 @@ function fontSizePlus(){
   corps.style.fontSize = (value + 1) + 'px';
 }
 
+//fonction appelé au click du bouton "dézoom" de la barre d'outils dans la page d'un arret.
 function fontSizeMoins(){
   corps = document.getElementById('arret');
   size = window.getComputedStyle(corps).getPropertyValue("font-size");
@@ -91,21 +103,7 @@ function fontSizeMoins(){
   corps.style.fontSize = (value - 1) + 'px';
 }
 
-function deletePaysfilter(){
-  var parsedUrl = new URL(window.location.href);
-  parsedUrl.searchParams.delete("pays");
-  parsedUrl.searchParams.delete("juridiction");
-  parsedUrl.searchParams.delete("page");
-  window.location.replace(parsedUrl);
-}
-
-function deleteJuridictionfilter(){
-  var parsedUrl = new URL(window.location.href);
-  parsedUrl.searchParams.delete("juridiction");
-  parsedUrl.searchParams.delete("page");
-  window.location.replace(parsedUrl);
-}
-
+//fonction appelé au click du bouton "copier" de la barre d'outils dans la page d'un arret pour copier le titre et l'url
 function copyArretUrl(titre){
   var btnCpy = document.getElementById("btn-cpy");
   btnCpy.children[0].setAttribute("class","bi bi-check2-square");
@@ -113,7 +111,24 @@ function copyArretUrl(titre){
   navigator.clipboard.writeText(titre+' ('+window.location.href+')');
 }
 
+//fonction qui réaffiche l'icone initial du copier qui a été changé après le click pour avertir que ça avait bien été copié
 function redisplayClipBoard(){
   var btnCpy = document.getElementById("btn-cpy");
   btnCpy.children[0].setAttribute("class","bi bi-clipboard");
+}
+
+////fonction appelé au click sur la croix à coté du input pays dans les filtres dans la page de recherche
+function deletePaysfilter(){
+  var parsedUrl = new URL(window.location.href);
+  parsedUrl.searchParams.delete("pays");
+  parsedUrl.searchParams.delete("juridiction");
+  parsedUrl.searchParams.delete("page");
+  window.location.replace(parsedUrl);
+}
+////fonction appelé au click sur la croix à coté du input de la juridiction dans les filtres dans la page de recherche
+function deleteJuridictionfilter(){
+  var parsedUrl = new URL(window.location.href);
+  parsedUrl.searchParams.delete("juridiction");
+  parsedUrl.searchParams.delete("page");
+  window.location.replace(parsedUrl);
 }
