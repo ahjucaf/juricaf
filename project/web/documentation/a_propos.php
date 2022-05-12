04ba0d725f9ecdfa0af0aef6cf93d9d44b4db722
Tchaboy <103650228+Tchaboy@users.noreply.github.com>
2022-05-12 14:01:53 +0200

    Update a_propos.php

--- project/web/documentation/a_propos.php
+++ project/web/documentation/a_propos.php
@@ -80,9 +80,9 @@
               
               <p>Télécharger l’ouvrage «&nbsp;<a href="https://www.ahjucaf.org/sites/default/files/AHJUCAF%20Congr%C3%A8s%20Beyrouth%20Actes%20mars%202020.pdf">La diffusion de la jurisprudence des Cours suprêmes judiciaires dela francophonie au temps d’internet</a>&nbsp;»</p>
 
-              <h2>L’AHJUCAF présente ses remerciements aux acteurs qui ont contribué à création et à la refonte du site de la base de données JURICAF :</h2>
+              <h2>L’AHJUCAF présente ses remerciements aux acteurs qui ont contribué à la création et à la refonte du site de la base de données JURICAF :</h2>
                   <p> - Cour de cassation : M. le Président Jean-Louis Gillet, M. Edouard Rottier, M. Guillaume Adréani, M. Mehdi Ben Mimoun</p>
-                  <p> - Partenaire public : Université de Paris 1 CAVEJ, Bruno Dondéro, M. Jean Gasnault, M. Stéphane Cottin</p>
+                  <p> - Partenaires publics : Université de Paris 1 CAVEJ, M. Bruno Dondéro, M. Jean Gasnault, M. Stéphane Cottin</p>
                   <p> - Partenaires privés : M. Tangui Morlier, M. Brice Person</p>
           </div>
         </div>
@@ -92,7 +92,7 @@
  	 <img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones" />
  	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
        <div class="bottom">
-       <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
+       <p>Juricaf est un projet de l'AHJUCAF, l'association des Cours suprêmes judiciaires francophones,<br />
       initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
       Il est soutenu par l'Organisation Internationale de la Francophonie.</p>
     </div>
