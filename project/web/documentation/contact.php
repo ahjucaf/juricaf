90dd5a0f998a24f7e3a6f84767c1a938b6bf1ddb
Tchaboy <103650228+Tchaboy@users.noreply.github.com>
2022-05-12 14:09:11 +0200

    Update contact.php

--- project/web/documentation/contact.php
+++ project/web/documentation/contact.php
@@ -83,7 +83,7 @@ $_SESSION['cap2'] = intval(rand(0, 10) + 1);
 	 <a href="http://www.ahjucaf.org/"><img style="margin-left: 10px;" src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprêmes francophones" /></a>
 	 <img style="float: right; margin-right: 10px;" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
       <div class="bottom">
-      <p>Juricaf est un projet de l'AHJUCAF, l'association des cours suprêmes judiciaires francophones,<br />
+      <p>JURICAF est un projet de l'AHJUCAF, l'association des Cours suprêmes judiciaires francophones,<br />
       initialement réalisé en partenariat avec le Laboratoire Normologie Linguistique et Informatique du droit (Université Paris I),<br />
       Il est soutenu par l'Organisation Internationale de la Francophonie.</p></div>
     </div>
