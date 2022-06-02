28c02814a102684f48409617914b02b42a3f242c
Tangui Morlier <tangui@tangui.eu.org>
2022-06-02 15:44:27 +0200

    On prépare l'arriver de la page actualité

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -2,9 +2,4 @@
 <?php include_component('arret', 'statsPays'); ?>
 </div>
 <hr class="d-none d-sm-none d-lg-block">
-<div class="mt-3 text-center d-none d-sm-none d-lg-block">
-	<p>Découvrir les coulisses de juricaf : <a href="https://www.lemondedudroit.fr/interviews/66303-jean-paul-jean-ahjucaf.html" target="new">Interview de JP Jean secrétaire général de l’AHJUCAF dans « Le Monde du droit » sur l’accès à la jurisprudence francophone.</a></p>
-	<p>Une initiative de l'AHJUCAF : 	</p>
-	<a href="http://www.ahjucaf.org/"><img src="/images/ahjucafSite.jpg" alt="Association des cours judiciaires suprêmes francophones" /></a>
-  <a href="http://www.ahjucaf.org/"><br/>Accédez au site de l’AHJUCAF </a>
-</div>
+<?php include_partial('recherche/actualites'); ?>
