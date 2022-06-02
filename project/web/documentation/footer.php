9a5ea63df0c65c8474225801fde62c17102f7118
Gabriel Poma <gpoma@24eme.fr>
2022-06-02 11:42:44 +0200

    lien vers ahjucaf / oif footer

--- project/web/documentation/footer.php
+++ project/web/documentation/footer.php
@@ -26,8 +26,8 @@
        Il est soutenu par l'Organisation Internationale de la Francophonie.
     </div>
     <div class="col-lg pt-3 d-lg-none">
-      <img class="float-start"src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones"/>
-      <img class="float-end" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" />
+      <a href="https://www.ahjucaf.org/"><img class="float-start"src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones"/></a>
+      <a href="https://www.francophonie.org/"><img class="float-end" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a>
     </div>
   </div>
   <br>
