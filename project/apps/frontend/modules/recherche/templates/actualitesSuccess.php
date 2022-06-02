46d79c4988eb789c6d6777296355e4de9134f5e4
Honorine-MC <honorine.mariechristy@gmail.com>
2022-06-02 18:21:31 +0200

    breadcrumb dans la page d'actualité

--- project/apps/frontend/modules/recherche/templates/actualitesSuccess.php
+++ project/apps/frontend/modules/recherche/templates/actualitesSuccess.php
@@ -1,7 +1,14 @@
 <div class="container">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/">Accueil</a></li>
+      <li class="breadcrumb-item"><a href="">Actualités</a></li>
+    </ol>
+</div>
+
+<div class="container">
 
 <h5 class="p-3 mb-2 bg-secondary bg-gradient">Actualités</h4>
 
-<div>
+<div class="container">
     <?php include_partial('recherche/actualites'); ?>
 </div>
