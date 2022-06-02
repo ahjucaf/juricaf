353e051a1d747bbbf2449a7374a5f85e8fb02c3b
Tangui Morlier <tangui@tangui.eu.org>
2022-06-02 14:47:06 +0200

    cas des pays avec '

--- project/apps/frontend/modules/recherche/actions/actions.class.php
+++ project/apps/frontend/modules/recherche/actions/actions.class.php
@@ -286,7 +286,7 @@ class rechercheActions extends sfActions
                 $filter .= '( ';
                 foreach ($pays as $p) {
                     $quote = '';
-                    if (preg_match('/ /', $p))
+                    if (preg_match("/[' ]/", $p))
                     $quote = '"';
                     $filter .= 'pays:'.$quote.$p.$quote;
                     if($i < $nb) { $filter .= ' OR '; }
