244fc3e15e0405ccdf192b90944fddd2815ce6a1
Tangui Morlier <tangui@tangui.eu.org>
2022-06-02 15:22:50 +0200

    Suppression des boutons valider dans la recherche avancée + amélioration des champs

--- project/apps/frontend/modules/recherche/templates/fullsearchSuccess.php
+++ project/apps/frontend/modules/recherche/templates/fullsearchSuccess.php
@@ -4,7 +4,200 @@
 if(!empty($query)) { echo 'Query : '.$query."<br />"; }
 if(!empty($filter)) { echo 'Filter : '.$filter; }
 ?>
-<?php include_component('recherche', 'fullsearch'); ?>
+
+<h5 class="p-3 mb-2 mt-5 bg-secondary bg-gradient">Recherche avancée</h5>
+
+<h5><b>Critères</b></h5>
+<p>
+<div class="row">
+  <div class="col-lg-3">
+    <select class="form-select" name="cr[1]">
+      <option value="content">Plein texte</option>
+      <option value="num_arret">Numéro d’affaire</option>
+      <option value="sens_arret">Sens</option>
+      <option value="nor">NOR</option>
+      <option value="urnlex">URN:LEX</option>
+      <option value="ecli">ECLI</option>
+      <option value="type_affaire">Type affaire</option>
+      <option value="type_recours">Type recours</option>
+      <option value="president">Président</option>
+      <option value="avocat_gl">Avocat général</option>
+      <option value="rapporteur">Rapporteur</option>
+      <option value="commissaire_gvt">Commissaire du gouvernement</option>
+      <option value="avocats">Avocat</option>
+      <option value="parties">Parties</option>
+      <option value="analyses">Analyses</option>
+      <option value="saisines">Saisine</option>
+      <option value="fonds_documentaire">Fonds documentaire</option>
+    </select>
+  </div>
+  <div class="col-lg-9">
+    <input type="text" name="val[1]" class="text_input form-control" />
+  </div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[1]">
+  <option value="AND">ET</option>
+  <option value="OR">OU</option>
+  <option value="NOT">SAUF</option>
+</select>
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cr[2]">
+  <option value="content">Plein texte</option>
+  <option value="num_arret">Numéro d’affaire</option>
+  <option value="sens_arret">Sens</option>
+  <option value="nor">NOR</option>
+  <option value="urnlex">URN:LEX</option>
+  <option value="ecli" selected="selected">ECLI</option>
+  <option value="type_affaire">Type affaire</option>
+  <option value="type_recours">Type recours</option>
+  <option value="president">Président</option>
+  <option value="avocat_gl">Avocat général</option>
+  <option value="rapporteur">Rapporteur</option>
+  <option value="commissaire_gvt">Commissaire du gouvernement</option>
+  <option value="avocats">Avocat</option>
+  <option value="parties">Parties</option>
+  <option value="analyses">Analyses</option>
+  <option value="saisines">Saisine</option>
+  <option value="fonds_documentaire">Fonds documentaire</option>
+</select>
+</div>
+<div class="col-lg-9">
+<input type="text" name="val[2]" class="text_input form-control" />
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[2]">
+  <option value="AND">ET</option>
+  <option value="OR">OU</option>
+  <option value="NOT">SAUF</option>
+</select>
+</div>
+</div>
+<p></p>
+<div class="row">
+  <div class="col-lg-3">
+<select class="form-select" name="cr[3]">
+  <option value="content">Plein texte</option>
+  <option value="num_arret">Numéro d’affaire</option>
+  <option value="sens_arret">Sens</option>
+  <option value="nor">NOR</option>
+  <option value="urnlex">URN:LEX</option>
+  <option value="ecli">ECLI</option>
+  <option value="type_affaire">Type affaire</option>
+  <option value="type_recours">Type recours</option>
+  <option value="president">Président</option>
+  <option value="avocat_gl">Avocat général</option>
+  <option value="rapporteur">Rapporteur</option>
+  <option value="commissaire_gvt">Commissaire du gouvernement</option>
+  <option value="avocats">Avocat</option>
+  <option value="parties" selected="selected">Parties</option>
+  <option value="analyses">Analyses</option>
+  <option value="saisines">Saisine</option>
+  <option value="fonds_documentaire">Fonds documentaire</option>
+</select>
+</div>
+<div class="col-lg-9">
+<input type="text" name="val[3]" class="text_input form-control" />
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cond[3]">
+  <option value="AND">ET</option>
+  <option value="OR">OU</option>
+  <option value="NOT">SAUF</option>
+</select>
+</div>
+</div>
+<p></p>
+<div class="row">
+<div class="col-lg-3">
+<select class="form-select" name="cr[4]">
+  <option value="content">Plein texte</option>
+  <option value="num_arret">Numéro d’affaire</option>
+  <option value="sens_arret">Sens</option>
+  <option value="nor">NOR</option>
+  <option value="urnlex">URN:LEX</option>
+  <option value="ecli">ECLI</option>
+  <option value="type_affaire">Type affaire</option>
+  <option value="type_recours">Type recours</option>
+  <option value="president">Président</option>
+  <option value="avocat_gl">Avocat général</option>
+  <option value="rapporteur">Rapporteur</option>
+  <option value="commissaire_gvt">Commissaire du gouvernement</option>
+  <option value="avocats">Avocat</option>
+  <option value="parties">Parties</option>
+  <option value="analyses" selected="selected">Analyses</option>
+  <option value="saisines">Saisine</option>
+  <option value="fonds_documentaire">Fonds documentaire</option>
+</select>
+</div>
+<div class="col-lg-9">
+<input type="text" name="val[4]" class="text_input form-control"/>
+</div>
+</div>
+<p></p>
+
+<hr style="clear: both;"/>
+
+<div class="row g-3">
+  <div class="col-lg-3">
+    <label class="col-form-label" for="date_arret"><h5><b>Date de la décision : </b></h5></label>
+  </div>
+  <div class="col-auto">
+    <div class="input-group">
+        <input class="form-control col-auto" type="date" name="date[arret]" id="date_arret" max=<?php echo(date('Y-m-d'))?>>
+        <label class="input-group-text" for="date_arret"><i class="bi bi-calendar"></i></label>
+    </div>
+  </div>
+</div>
+<div class="row g-3">
+    <div class="col-lg-3">
+        OU
+    </div>
+</div>
+<div class="row g-3">
+  <div class="col-lg-3">
+    <label class="col-form-label" for="date_debut"><h5><b>Période du :</b></h5></label>
+  </div>
+  <div class="col-auto">
+    <div class="input-group">
+        <input class="form-control col-auto" type="date" name="date[debut]" id="date_debut" />
+        <label class="input-group-text" for="date_debut"><i class="bi bi-calendar"></i></label>
+    </div>
+  </div>
+  <div class="col-auto">
+    <label class="col-form-label" for="date_fin"><h5><b>au :</b></h5></label>
+  </div>
+  <div class="col-auto">
+    <div class="input-group">
+        <input class="form-control col-auto" type="date" name="date[fin]" id="date_fin" />
+        <label class="input-group-text" for="date_fin"><i class="bi bi-calendar"></i></label>
+    </div>
+  </div>
+</div>
+
+<hr />
+
+<div class="row">
+  <div class="col-lg-3">
+    <label class="col-form-label"><h5><b>Références :</b></h5></label>
+  </div>
+  <div class="col-lg-9">
+    <input class="form-control" type="text" name="references" />
+  </div>
+</div>
+<hr />
 <?php include_component('arret', 'searchPays'); ?>
 <hr />
 <div class="float-end mb-3">
