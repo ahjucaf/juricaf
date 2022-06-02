26239448702884dae10411b25b6ae2511908ce09
Tangui Morlier <tangui@tangui.eu.org>
2022-06-02 16:02:41 +0200

    Actualites

--- project/apps/frontend/modules/recherche/templates/indexSuccess.php
+++ project/apps/frontend/modules/recherche/templates/indexSuccess.php
@@ -2,6 +2,6 @@
 <?php include_component('arret', 'statsPays'); ?>
 </div>
 <hr class="d-none d-sm-none d-lg-block">
-<div class="text-center">
+<div class="text-center d-none d-sm-none">
 <?php include_partial('recherche/actualites'); ?>
 </div>
