77330509966df9f9b049df6336f47d45840775ad
Gabriel Poma <gpoma@24eme.fr>
2022-06-02 15:16:18 +0200

    mobile: logo sur fond blanc

--- project/web/documentation/footer.php
+++ project/web/documentation/footer.php
@@ -1,33 +1,32 @@
-<div class="bottom text-center p-3 mt-5">
+<div class="bottom text-center p-3 mt-5 d-none d-lg-block">
   <div class="row">
-    <div class="col-3 d-none d-lg-block">
+    <div class="col-3">
       <a href="https://www.ahjucaf.org/"><img src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprèmes francophones"/></a>
     </div>
-    <div class="col-6 d-none d-lg-block small">
+    <div class="col-6 small">
       <p>Juricaf est un projet de l'AHJUCAF, l'association des Cours suprêmes judiciaires francophones. Il est soutenu par l'Organisation Internationale de la Francophonie.</p>
       <img class="mt-3" src="/images/iall.jpg" />
     </div>
-    <div class="col-3 d-none d-lg-block">
+    <div class="col-3">
       <a href="https://www.francophonie.org/"><img src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a>
     </div>
+  </div>
+</div>
 
     <!-- VERSION MOBILE -->
-
-    <div class="col-sm d-lg-none small">
-       Juricaf est un projet de l'AHJUCAF, l'association des Cours suprêmes judiciaires francophones. Il est soutenu par l'Organisation Internationale de la Francophonie.
-    </div>
-    <div class="col-lg pt-3 d-lg-none">
+<div class="text-center d-lg-none mt-3">
+  <div class="row">
+    <div class="col-lg pt-3 px-5 mb-3">
       <a href="https://www.ahjucaf.org/"><img class="float-start"src="/images/ahjucaf_small.png" alt="Association des cours judiciaires suprmes francophones"/></a>
       <a href="https://www.francophonie.org/"><img class="float-end" src="/images/francophonie.png" alt="Organisation internationale de la francophonie" /></a>
     </div>
-    <div class="col-lg d-lg-none pt-3">
+    <div class="col-sm small bottom p-2">
+       Juricaf est un projet de l'AHJUCAF, l'association des Cours suprêmes judiciaires francophones. Il est soutenu par l'Organisation Internationale de la Francophonie.
+    </div>
+    <div class="col-lg pt-3 bottom p-2">
         <img src="/images/iall.jpg" />
     </div>
   </div>
-
-
-</div>
-</div>
 </div>
 <script type="text/javascript">
 <!--
