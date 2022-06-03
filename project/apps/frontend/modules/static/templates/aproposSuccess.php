38e8c9e94add7d5bb8ce53e71e4de8b0701082d8
Honorine-MC <honorine.mariechristy@gmail.com>
2022-06-03 11:57:34 +0200

    maj

--- project/apps/frontend/modules/static/templates/aproposSuccess.php
+++ project/apps/frontend/modules/static/templates/aproposSuccess.php
@@ -6,7 +6,6 @@
       <li class="breadcrumb-item"><a href="">À-propos</a></li>
     </ol>
     <h5 class="p-3 mb-2 bg-secondary bg-title fw-bold ">À-propos</h5>
-    <?php $title="À-propos";?>
 </div>
     <div class="container text-justify mt-3">
 
