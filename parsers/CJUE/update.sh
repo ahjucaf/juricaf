d5e13f33c7e0dc2ce186d292fe29419f020315ae
Jb Lm <lemetayer.jb@gmail.com>
2022-06-22 13:51:03 +0200

    premier jet d'un script pour générer les xml cjue a partir des donnees telechargees

---
+++ parsers/CJUE/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p xml
+mkdir -p data
+
+ls data|grep ".xml"|sed "s/.xml//"|sort|uniq | while read id ; do
+  if [ -e data/$id.html -a -e data/$id.xml ]
+  then
+    php parse_arret.php data/$id.xml data/$id.html > xml/$id.xml
+  fi
+done
