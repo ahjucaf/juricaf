2d767529c278df91a8bfc4688318bd6e7bb95675
NicoDuclos <nicolas@24eme.fr>
2022-12-15 15:34:39 +0100

    Belg html > html Génère xml même si html déja dwnld

--- parsers/Belgique/update.sh
+++ parsers/Belgique/update.sh
@@ -2,7 +2,7 @@
 
 mkdir -p html xml
 
-php pages_downloader.php $(date "%Y") | while read htmlfile source; do
+php pages_downloader.php $(date '+%Y') | while read htmlfile source; do
     xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
     php parser_htmltoxml.php $htmlfile $source > $xmlfile;
 done
