f7e1886b4c0fd0e054c86cdfedfcef969900c0ab
NicoDuclos <nicolas@24eme.fr>
2022-12-19 13:25:10 +0100

    Belg html>xml suppr xml si vide ds update.sh

--- parsers/Belgique/update.sh
+++ parsers/Belgique/update.sh
@@ -13,5 +13,10 @@ php pages_downloader.php $annee | while read htmlfile source; do
     if [[ $htmlfile =~ .*ECLI:BE:CASS:$annee:ARR.* ]]; then
         xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
         [ -f $xmlfile ] && echo "$xmlfile existe." || php parser_htmltoxml.php $htmlfile $source > $xmlfile;
+        xmlsize=$(wc -c <"$xmlfile")
+        if [ $xmlsize -le 0 ]; then
+            # si vide il y a eu une erreur, on supprime le xml
+            rm $xmlfile
+        fi
     fi
 done
