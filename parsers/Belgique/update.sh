61c4aa39a79bfecb8c806a8cb3c2f6aaa5b99974
NicoDuclos <nicolas@24eme.fr>
2022-12-19 12:15:15 +0100

    Belg html>xml  corrige regexp update.sh

--- parsers/Belgique/update.sh
+++ parsers/Belgique/update.sh
@@ -10,7 +10,7 @@ fi
 
 php pages_downloader.php $annee | while read htmlfile source; do
     # On ne prend que les arrets de la cour de cass
-    if [[ "$htmlfile" =~ ^ECLI:BE:CASS:\d{4}:ARR.* ]]; then
+    if [[ $htmlfile =~ .*ECLI:BE:CASS:$annee:ARR.* ]]; then
         xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
         [ -f $xmlfile ] && echo "$xmlfile existe." || php parser_htmltoxml.php $htmlfile $source > $xmlfile;
     fi
