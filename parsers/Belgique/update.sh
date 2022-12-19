70dfb7ce19c93a1714d77cb2a974fbb5dd05eea0
NicoDuclos <nicolas@24eme.fr>
2022-12-19 12:07:57 +0100

    Belg html>xml  ne prend que arret cour de cass pour le moment

--- parsers/Belgique/update.sh
+++ parsers/Belgique/update.sh
@@ -2,7 +2,16 @@
 
 mkdir -p html xml
 
-php pages_downloader.php $(date '+%Y') | while read htmlfile source; do
-    xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
-    [ -f $xmlfile ] && echo "$xmlfile existe." || php parser_htmltoxml.php $htmlfile $source > $xmlfile;
+if [ $# -eq 0 ]; then
+    annee=$(date '+%Y')
+else
+    annee=$1
+fi
+
+php pages_downloader.php $annee | while read htmlfile source; do
+    # On ne prend que les arrets de la cour de cass
+    if [[ "$htmlfile" =~ ^ECLI:BE:CASS:\d{4}:ARR.* ]]; then
+        xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
+        [ -f $xmlfile ] && echo "$xmlfile existe." || php parser_htmltoxml.php $htmlfile $source > $xmlfile;
+    fi
 done
