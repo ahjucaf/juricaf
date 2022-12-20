29bcecf37127dec7e9690959cb3afa3546e0afe6
Honorine-MC <honorine.mariechristy@gmail.com>
2022-12-20 16:35:47 +0100

    création des dossiers xml et html après s'etre placé dans le dossier

--- parsers/Canada/CanLII/update.sh
+++ parsers/Canada/CanLII/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir -p json xml
 cd $(dirname $0)
 . config/config.inc
+
+mkdir -p json xml
+
 php pages_downloader.php | while read json_meta json_content ; do
     xmlfile=$(echo $json_meta | sed 's/json/xml/g'| sed 's/-meta//g')
     php parser_jsonstoxml.php $json_meta $json_content > $xmlfile
