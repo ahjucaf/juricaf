aef8653eda295b7daae8dc5c6554ea79e9af04cd
Honorine-MC <honorine.mariechristy@gmail.com>
2022-12-20 16:24:52 +0100

    oubli

--- parsers/Canada/CanLII/update.sh
+++ parsers/Canada/CanLII/update.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 mkdir -p json xml
+cd $(dirname $0)
 . config/config.inc
 php pages_downloader.php | while read json_meta json_content ; do
     xmlfile=$(echo $json_meta | sed 's/json/xml/g'| sed 's/-meta//g')
-    if [ ! -f "$xmlfile" ]; then
-      if ! php parser_jsonstoxml.php $json_meta $json_content > $xmlfile
-      then
-        rm $xmlfile;
-      else
-        cp $xmlfile $POOL_DIR/
-      fi
+    php parser_jsonstoxml.php $json_meta $json_content > $xmlfile
+    if ! test -s $xmlfile;then
+      rm $xmlfile;
+    else
+      cp $xmlfile $POOL_DIR/
     fi
 done
