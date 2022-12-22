086eb50a2b21429d5320a43391632b3183432362
NicoDuclos <nicolas@24eme.fr>
2022-12-22 15:32:11 +0100

    Belg html>xml préparation du script pour la cron

--- parsers/Belgique/update.sh
+++ parsers/Belgique/update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+cd $(dirname $0)
+. config/config.inc
+
 mkdir -p html xml
+mkdir -p $POOL_DIR
 
 if [ $# -eq 0 ]; then
     annee=$(date '+%Y')
@@ -9,14 +13,23 @@ else
 fi
 
 php pages_downloader.php $annee | while read htmlfile source; do
-    # On ne prend que les arrets de la cour de cass
-    if [[ $htmlfile =~ .*ECLI:BE:CASS:$annee:ARR.* ]]; then
+    # On ne prend que les arrets de la cour de cass et constitutionnelle 
+    if [[ $htmlfile =~ .*ECLI:BE:(?:CASS|GHCC):$annee:ARR.* ]]; then
         xmlfile=$(echo $htmlfile | sed 's/html/xml/g')
-        [ -f $xmlfile ] && echo "$xmlfile existe." || php parser_htmltoxml.php $htmlfile $source > $xmlfile;
-        xmlsize=$(wc -c <"$xmlfile")
-        if [ $xmlsize -le 0 ]; then
-            # si vide il y a eu une erreur, on supprime le xml
-            rm $xmlfile
+
+        # Si le xml a déja été généré on passe
+        if [ -f $xmlfile ]; then
+            echo "$xmlfile existe."
+        else
+            echo "Création du xml"
+            php parser_htmltoxml.php $htmlfile $source > $xmlfile
+            xmlsize=$(wc -c <"$xmlfile")
+
+            # si le xml n'est pas vide (juportal.be peut renvoyer des html avec pas de données dedans => xml généré plus ahut est vide)
+            if [ $xmlsize -g 0 ]; then
+                echo "xml -> pool"
+                cp $xmlfile $POOL_DIR/
+            fi
         fi
     fi
 done
