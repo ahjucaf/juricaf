96d38109c0e188440d447fccde1e4bf28718dcb0
Tangui Morlier <tangui@tangui.eu.org>
2023-04-14 15:05:52 +0200

    Gestion d'erreur

--- juricaf2solr/juricaf2couchdb/updatejson4couchdb.php
+++ juricaf2solr/juricaf2couchdb/updatejson4couchdb.php
@@ -4,6 +4,10 @@ $file = $argv[1];
 $id  = $argv[2];
 $rev = $argv[3];
 $json = json_decode(file_get_contents($file));
+if (!$json || !isset($json->docs)) {
+    error_log('ERROR: '.$file.' has no "docs" entity');
+    exit(1);
+}
 foreach ($json->docs as $j) {
 	if ($j->_id == $id) {
 		$j->_rev = $rev;
