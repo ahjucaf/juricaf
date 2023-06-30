340a53d9942ec4ec5847e98ae5fd5f9e83cec18d
Tangui Morlier <tangui@tangui.eu.org>
2023-06-30 12:26:28 +0200

    Update régulier et identification des arrêts par la page globale de CURIA

--- parsers/CJUE/update.sh
+++ parsers/CJUE/update.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-mkdir -p xml
-mkdir -p data
+cd $(dirname $0)
 
-ls data|grep ".xml"|sed "s/.xml//"|sort|uniq | while read id ; do
+. ../config/config.inc
+
+mkdir -p xml data
+mkdir -p $POOL"/CJUE"
+
+bash download_lasts.sh | while read id ; do
   if [ -e data/$id.xml -a -e data/$id.txt ]
   then
     php parse_arret.php data/$id.xml data/$id.txt > xml/$id.xml
     if ! test -s xml/$id.xml ; then
         rm xml/$id.xml
+    else
+        cp xml/$id.xml $POOL"/CJUE/"
     fi
   fi
 done
