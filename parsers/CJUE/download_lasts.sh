9f91c1988dbfd43487c494fe5f12eca678a8a7fb
Tangui Morlier <tangui@tangui.eu.org>
2023-06-30 16:25:27 +0200

    cas des CELEX embarqués dans du js

--- parsers/CJUE/download_lasts.sh
+++ parsers/CJUE/download_lasts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://curia.europa.eu/jcms/jcms/P_106311/fr/ | grep 'FR/TXT' | grep "CELEX:" | sed 's/[&_]/\n/g' | grep CELEX | sed 's/.*CELEX://' | grep -v img | sort -u | while read celex ; do
+curl -s https://curia.europa.eu/jcms/jcms/P_106311/fr/ | grep 'FR/TXT' | grep "CELEX:" | sed "s/[&_']/\n/g" | grep CELEX | sed 's/.*CELEX://' | grep -v img | sort -u | while read celex ; do
     ISNEW=""
     if ! test -f data/$celex.xml ; then
         curl -s -L -H "Accept: application/xml;notice=branch" -H "Accept-language: fr" http://publications.europa.eu/resource/celex/$celex > data/$celex.xml
