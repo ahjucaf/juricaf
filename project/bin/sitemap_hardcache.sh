4b26a46f9f6cff39a2228b92f5c83bccb859eff4
Vincent LAURENT <vince.laurent@gmail.com>
2023-08-03 00:07:47 +0200

    curl plus discret

--- project/bin/sitemap_hardcache.sh
+++ project/bin/sitemap_hardcache.sh
@@ -1,4 +1,4 @@
 curl -s https://juricaf.org/sitemap > web/sitemap.xml.tmp
-cat web/sitemap.xml.tmp | grep loc | sed -r 's|<[^<>]*>||g' |sed 's/ //g' | while read url; do XMLFILENAME=$(echo -n $url | sed -r 's|.*/||'); curl $url > web/sitemap_$XMLFILENAME.xml.tmp; mv web/sitemap_$XMLFILENAME.xml{.tmp,}; done;
+cat web/sitemap.xml.tmp | grep loc | sed -r 's|<[^<>]*>||g' |sed 's/ //g' | while read url; do XMLFILENAME=$(echo -n $url | sed -r 's|.*/||'); curl -s $url > web/sitemap_$XMLFILENAME.xml.tmp; mv web/sitemap_$XMLFILENAME.xml{.tmp,}; done;
 sed -ri 's|sitemap/([^\/]+)</loc>|sitemap_\1.xml</loc>|' web/sitemap.xml.tmp
 mv web/sitemap.xml{.tmp,}
