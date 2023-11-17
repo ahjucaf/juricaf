a6ad4bd97c386e7375b984d7eec82a167523d8f5
Tangui Morlier <tangui@tangui.eu.org>
2023-11-17 12:04:06 +0100

    Petites améliorations de l'import lexbase

--- parsers/lexbase/update.sh
+++ parsers/lexbase/update.sh
@@ -3,13 +3,13 @@
 cd $(dirname $0)
 . ../config/config.inc
 
-mirror_subdir=$1
-
-if ! test "$mirror_subdir"; then
-	echo "subdir of lexbase source needed"
+mirror_subdir=$(echo $1 | sed 's/\/*$//')
+if ! test "$mirror_subdir" || echo $mirror_subdir | grep '://' > /dev/null ; then
+	echo "subdir of lexbase source needed from $LEXBASE_SOURCEURL";
 	exit 1;
 fi
 
 cd $LEXBASE_MIRRORDIR
 wget -m $LEXBASE_WGET_AUTH $LEXBASE_SOURCEURL/$mirror_subdir
-rsync -a $(echo $LEXBASE_SOURCEURL | sed 's/^[a-z]*:\/\///' )/$mirror_subdir $POOL_DIR/$mirror_subdir
+mkdir -p $POOL_DIR/$mirror_subdir
+rsync -a $(echo $LEXBASE_SOURCEURL | sed 's/^[a-z]*:\/\///' )/$mirror_subdir/ $POOL_DIR/$mirror_subdir
