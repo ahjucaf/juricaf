ce3b925a0b30a5eca5e3bac8182cc2177dca3620
Tangui Morlier <tangui@tangui.eu.org>
2024-01-19 19:26:52 +0100

    Suite du travail sur le parser judilibre

---
+++ parsers/judilibre/parse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file="$1"
+id=$(echo $file | sed 's/raw.//')
+
+source ../config/config.inc
+
+mkdir -p $POOL_DIR/cc $POOL_DIR/ca
+
+juridiction=$(jq '.jurisdiction' < $file)
+if test "$juridiction" == "Cour de cassation" ; then
+	num=$( jq .number < $file | sed 's/["\.]//g');
+	if ! curl 'https://juricaf.org/recherche/'$num'/facet_pays%3AFrance%2Cfacet_pays_juridiction%3AFrance_|_Cour_de_cassation?format=json' | grep 'https://juricaf.org/arret/' ; then
+		php parse_cass.php $file > $POOL_DIR/cc/$id".xml"
+	fi
+elif test "Cour d'appel" = $file ; then
+	php parse_courdappel.php $file > $POOL_DIR/ca/$id".xml"
+else
+	echo ERROR: $file : juridiction $juridiction non gérée 1>&2
+fi
---
+++ parsers/judilibre/parse_jurilibre.php
@@ -0,0 +1,135 @@
+<?php
+
+$rawfile = $argv[1];
+$json = json_decode(file_get_contents($rawfile));
+
+if (isset($json->chamber) && strlen($json->chamber) < 4) {
+	switch ($json->chamber) {
+		case 'cr':
+			$json->chamber = "chambre criminelle";
+			break;
+		case 'civ3':
+			$json->chamber = 'troisième chambre civile';
+			break;
+		case 'civ2':
+			$json->chamber = 'deuxieme chambre civile';
+			break;
+		case 'civ1':
+			$json->chamber = 'première chambre civile';
+			break;
+	}
+
+}
+if (isset($json->jurisdiction) && strlen($json->jurisdiction) < 4) {
+	switch ($json->jurisdiction) {
+		case 'cc':
+			$json->jurisdiction = "Cour de cassation";
+			break;
+	}
+
+}
+if (isset($json->formation) && strlen($json->formation) < 5) {
+	switch ($json->formation) {
+		case 'frh':
+			$json->formation = 'formation restreinte';
+			break;
+		case 'fs':
+			$json->formation = 'formation de section';
+			break;
+	}
+}
+
+
+print_r($json);
+
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo "\n<DOCUMENT>\n";
+echo "<PAYS>FRANCE</PAYS>\n";
+echo "<SOURCE>".$json->source."</SOURCE>\n";
+echo "<ID_SOURCE>".$json->id."</ID_SOURCE>\n";
+echo "<JURIDICTION>".$json->jurisdiction."</JURIDICTION>\n";
+if (isset($json->location)) {
+	echo "<TRIBUNAL>".$json->location."</TRIBUNAL>\n";
+}
+if (isset($json->formation) || isset($json->chamber)) {
+	echo "<FORMATION>";
+	if (isset($json->chamber)) {
+		echo $json->chamber;
+	}
+	if (isset($json->formation) && isset($json->chamber)) {
+		echo " - ";
+	}
+	if (isset($json->formation)) {
+		echo $json->formation;
+	}
+	"</FORMATION>\n";
+}
+echo "<TYPE>".strtolower(transliterator_transliterate('NFKC; [:Nonspacing Mark:] Remove; NFKC; Any-Latin; Latin-ASCII', $json->type))."</TYPE>\n";
+echo "<DATE_ARRET>".$json->decision_date."</DATE_ARRET>\n";
+echo "<NUM_ARRET>".$json->number."</NUM_ARRET>\n";
+if (count($json->numbers) > 1) {
+	echo "<NUMEROS_AFFAIRES>\n";
+	foreach($json->numbers as $num)  {
+		echo "<NUMERO_AFFAIRE>".$num."</NUMERO_AFFAIRE>\n";
+	}
+	echo "</NUMEROS_AFFAIRES>\n";
+}
+if (isset($json->publication) || isset($json->rapprochements) || isset($json->visa)) {
+echo "<REFERENCES>\n";
+	foreach($json->publication as $pub) {
+		switch (strtolower($pub)) {
+			case 'b':
+				$pub = "bulletin";
+				break;
+			case 'a':
+				$pub = 'recueil Lebon';
+				break;
+			case 'c':
+			case 'c+':
+			 	$pub = 'Inédit';
+				break;
+		}
+		echo "<REFERENCE>\n";
+		echo "<TYPE>PUBLICATION</TYPE>\n";
+		echo "<TITRE>".$pub."</TITRE>\n";
+		echo "</REFERENCE>\n";
+	}
+	foreach ($json->rapprochements as $rappro) {
+		echo "<REFERENCE>\n";
+		echo "<TYPE>SIMILAIRE</TYPE>\n";
+		echo "<TITRE>".$rappro."</TITRE>\n";
+		echo "</REFERENCE>\n";
+	}
+	foreach ($json->visa as $v) {
+		echo "<REFERENCE>\n";
+		echo "<TYPE>VISA</TYPE>\n";
+		echo "<TITRE>".$v->titile."</TITRE>\n";
+		echo "</REFERENCE>\n";
+	}
+echo "</REFERENCES>\n";
+}
+echo "<TEXTE_ARRET>\n".$json->text."\n</TEXTE_ARRET>\n";
+if (isset($json->ecli)) {
+	echo "<ECLI>".$json->ecli."</ECLI>\n";
+}
+if (isset($json->contested)) {
+	echo "<DECISIONS_ATTAQUEES>\n";
+	echo "<DECISION_ATTAQUEE>\n";
+	echo "<TYPE>DECISION</TYPE>\n";
+	echo "<DATE>".$json->contested->date."</DATE>\n";
+	echo "<TITRE>".str_replace("\n", " ", $json->contested->titre)."</TITRE>\n";
+	echo "<FORMATION>".str_replace("\n", " ", $json->contested->jurisdiction)."</FORMATION>\n";
+	echo "</DECISION_ATTAQUEE>\n";
+	echo "</DECISIONS_ATTAQUEES>\n";
+}
+if (isset($json->summary)) {
+	echo "<ANALYSES>\n";
+	if (isset($json->summary)) {
+		echo "<ANALYSE>";
+		
+		echo "</ANALYSE>";
+	}
+	echo "</ANALYSES>\n";
+}
+echo "<SENS_ARRET>".$json->solution."</SENS_ARRET>\n";
+echo "</DOCUMENT>\n";
