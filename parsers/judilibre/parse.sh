3485cae2ac5816274d30677be742134e3378902e
Tangui Morlier <tangui@tangui.eu.org>
2024-03-25 16:57:55 +0100

    Divers petits bug + clarification

--- parsers/judilibre/parse.sh
+++ parsers/judilibre/parse.sh
@@ -7,13 +7,13 @@ source ../config/config.inc
 
 mkdir -p $POOL_DIR/cc $POOL_DIR/ca
 
-juridiction=$(jq '.jurisdiction' < $file)
-if test "$juridiction" == "Cour de cassation" ; then
+juridiction=$(jq '.jurisdiction' < $file | sed "s/[^a-z]//ig")
+if test "$juridiction" == "Courdecassation" ; then
 	num=$( jq .number < $file | sed 's/["\.]//g');
 	if ! curl 'https://juricaf.org/recherche/'$num'/facet_pays%3AFrance%2Cfacet_pays_juridiction%3AFrance_|_Cour_de_cassation?format=json' | grep 'https://juricaf.org/arret/' ; then
-		php parse_cass.php $file > $POOL_DIR/cc/$id".xml"
+		php parse_jurilibre.php $file > $POOL_DIR/cc/$id".xml"
 	fi
-elif test "Cour d'appel" = $file ; then
+elif test "$juridiction" == "Courdappel" ; then
 	php parse_courdappel.php $file > $POOL_DIR/ca/$id".xml"
 else
 	echo ERROR: $file : juridiction $juridiction non gérée 1>&2
