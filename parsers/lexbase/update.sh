d4080c990b7bae3874f3f0a24face5e96430cf0c
Tangui Morlier <tangui@tangui.eu.org>
2024-03-25 14:03:15 +0100

    Nouvelle manière de synchroniser pour que ca fonctionne avec sftp

--- parsers/lexbase/update.sh
+++ parsers/lexbase/update.sh
@@ -4,12 +4,27 @@ cd $(dirname $0)
 . ../config/config.inc
 
 mirror_subdir=$(echo $1 | sed 's/\/*$//')
+
+if echo $LEXBASE_SOURCEURL | grep sftp > /dev/null && ! ssh-keygen -F $(echo $LEXBASE_SOURCEURL | sed 's/sftp:/[/' | sed 's|/||g' | sed 's/:/]:/') > /dev/null ; then
+	echo "$LEXBASE_SOURCEURL not found in ssh's known_hosts file";
+	exit 3;
+fi
+
 if ! test "$mirror_subdir" || echo $mirror_subdir | grep '://' > /dev/null ; then
 	echo "subdir of lexbase source needed from $LEXBASE_SOURCEURL";
+	echo "possible choices :"
+	lftp $LEXBASE_WGET_AUTH  $LEXBASE_SOURCEURL"/" -e "ls; exit;"
 	exit 1;
 fi
 
+if test "$2" ; then
+	lftp $LEXBASE_WGET_AUTH $LEXBASE_SOURCEURL -e "ls $mirror_subdir ; exit;"
+	exit 2;
+fi
+
 cd $LEXBASE_MIRRORDIR
-wget -m $LEXBASE_WGET_AUTH $LEXBASE_SOURCEURL/$mirror_subdir
+wget -q -m $LEXBASE_WGET_AUTH $LEXBASE_SOURCEURL/$mirror_subdir
 mkdir -p $POOL_DIR/$mirror_subdir
-rsync -a --exclude=.listing $(echo $LEXBASE_SOURCEURL | sed 's/^[a-z]*:\/\///' )/$mirror_subdir/ $POOL_DIR/$mirror_subdir
+cd $POOL_DIR/$mirror_subdir
+lftp $LEXBASE_WGET_AUTH $LEXBASE_SOURCEURL -e "cd $mirror_subdir ; mirror -r . . ; exit;"
+cd - > /dev/null
