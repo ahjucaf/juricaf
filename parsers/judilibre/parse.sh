67f03300d63ac8baec3149ad69dcfdd99d03d585
Tangui Morlier <tangui@tangui.eu.org>
2024-03-26 18:09:46 +0100

    Numéro qui ne pose pas de problème au curl

--- parsers/judilibre/parse.sh
+++ parsers/judilibre/parse.sh
@@ -8,7 +8,7 @@ source ../config/config.inc
 mkdir -p $POOL_DIR/cc $POOL_DIR/ca
 
 juridiction=$(jq '.jurisdiction' < $file | sed "s/[^a-z]//ig")
-num=$( jq .number < $file | sed 's/["\.]//g' | sed 's|/|+|g');
+num=$( jq .number < $file | sed 's/["\.\+]//g' | sed 's|[/'"'"' ]|+|g');
 if test "$juridiction" == "Courdecassation" ; then
 	numres=$(curl -s 'https://juricaf.org/recherche/num_arret:%22'$num'%22/facet_pays%3AFrance%2Cfacet_pays_juridiction%3AFrance_%7C_Cour_de_cassation?format=json' | jq .nb_resultat)
 	if test "$numres" -eq 0; then
