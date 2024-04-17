4b9335fbefd71f5ff792dc9b51efce6ce18b024e
Tangui Morlier <tangui@tangui.eu.org>
2024-04-17 18:55:06 +0200

    Retour des stats par champs

--- stats/createStats.sh
+++ stats/createStats.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Répertoire de travail
 cd $(dirname $0)
+
+. ../juricaf2solr/conf/juricaf.conf
 
 mkdir -p static
 
 php statsBase.php
 
-#echo -e "\n\n* Statistiques avancées des champs :";
-#php statsChamps.php
+find static -name luke.xml -mtime +7 -delete
+if ! test -s static/luke.xml ; then
+	curl -s http://$SOLRHOST:8080/solr/admin/luke > static/luke.xml
+	php statsChamps.php > static/champs.csv.new
+	mv -f static/champs.csv.new static/champs.csv
+fi
--- stats/statsChamps.php
+++ stats/statsChamps.php
@@ -1,52 +1,41 @@
 <?php
 require("config.php");
 
-$criteres = array(
-  'analyses',
-  'sens_arret',
-  'decisions_attaquees',
-  'parties',
-  'references',
-  'nor',
-  'ecli',
-  'type_affaire',
-  'type_recours',
-  'president',
-  'avocat_gl',
-  'rapporteur',
-  'commissaire_gvt',
-  'avocats',
-  'saisines'
-);
-
 function getSolrResults($pays, $juridiction, $critere = '') {
   global $SOLRHOST;
   if(!empty($critere)) { $critere = '+'.$critere.':*'; }
-  $stream = fopen('http://'.$SOLRHOST.':8080/solr/select/?q=facet_pays:%22'.urlencode($pays).'%22+facet_juridiction:%22'.urlencode($juridiction).'%22'.$critere.'&fq=type:arret&indent=on', 'r');
+  $stream = fopen('http://'.$SOLRHOST.':8080/solr/select/?q=facet_pays:%22'.urlencode($pays).'%22+facet_juridiction:%22'.urlencode($juridiction).'%22'.$critere.'&rows=0&indent=on', 'r');
   $xml = trim(stream_get_contents($stream));
   $response = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_COMPACT);
   fclose($stream);
   return $response->result["numFound"];
 }
 
-$csv = '"Pays";"Institution";"Nombre"';
-$tableau = "<table class=\"statsbase\">\n";
-$tableau .= '<tr><th>Pays</th><th>Institution</th><th>Nombre</th>';
+$criteres = [];
+$stream = fopen('static/luke.xml', 'r');
+$xml = trim(stream_get_contents($stream));
+$response = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_COMPACT);
+foreach($response->lst[2]->lst as $lst) {
+    $criteres[] = strval($lst['name']);
+}
 
+$tableau = "<div class='table-responsive'><table class=\"table statsbase table-striped table-bordered\" id=\"statsbase\">\n";
+$tableau .= '<tr><th>Pays</th><th>Institution</th><th>Nombre</th>';
 foreach ($criteres as $value) {
-  $csv .= ';'.$value;
   $tableau .= '<th>'.$value.'</th>';
 }
 
-$csv .= "\n";
 $tableau .= "</tr>\n";
 $classe = 'color2';
 
-$line = -1;
 $stream = fopen('http://'.$SOLRHOST.':8080/solr/select?indent=on&version=2.2&q=type:arret&rows=0&facet=true&facet.field=facet_pays_juridiction&facet.limit=-1', 'r');
 $xml = trim(stream_get_contents($stream));
 $response = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_COMPACT);
 fclose($stream);
+
+echo '"Pays";"Institution";"Critère";"Nombre"';
+echo "\n";
+
 foreach($response->lst[1]->lst[1]->lst->int as $int) {
   $name = (string) $int['name'];
   $nb = $int[0];
@@ -59,35 +48,31 @@ foreach($response->lst[1]->lst[1]->lst->int as $int) {
   $nb = getSolrResults($pays, $juridiction);
   foreach ($criteres as $critere) {
     $collection[$critere] = getSolrResults($pays, $juridiction, $critere);
+    if ($collection[$critere] > 0) {
+        echo '"'.$pays.'";"'.$juridiction.'";'.$critere.';'.$collection[$critere]."\n";
+    }
   }
-
-  $csv .= '"'.$pays.'";"'.$juridiction.'";'.$nb;
-
   $fpjlink = str_replace(' ', '_', 'http://www.juricaf.org/recherche/+/facet_pays:'.$pays.',facet_juridiction:'.$juridiction);
   if($classe == "color1") { $classe = "color2"; } else { $classe = "color1"; }
   $tableau .= '<tr class="'.$classe.'"><td><a href="http://www.juricaf.org/recherche/recherche/+/facet_pays:'.$pays.'">'.$pays.'</a></td><td><a href="'.$fpjlink.'">'.$juridiction.'</a></td><td class="num">'.$nb.'</td>';
 
   foreach ($criteres as $critere) {
-    $csv .= ';'.$collection[$critere];
     $tableau .= '<td class="num">'.$collection[$critere].'</td>';
   }
-
-  $csv .= "\n";
   $tableau .= "</tr>\n";
 }
-
 $tableau .= "\n</table>";
+$tableau .= '<p class="text-muted">généré le '.date("d/m/Y à H:i:s").'</p>';
+$tableau .= '<script type="text/javascript" src="/js/jquery-3.6.0.slim.min.js?5ff8755abb8669f8185a89437b34389870241c92"></script>';
+$tableau .= '<script type="text/javascript" src="/js/dataTables.js"></script>';
+$tableau .= '<script>$("#statsbase").DataTable( {paging: false, searching: false, info: false} );</script>';
 
 try {
-  $handler = fopen("static/champs.csv","w");
-  fputs($handler,$csv);
   $handler = fopen("static/champs.html","w");
   fputs($handler,$tableau);
-  echo "Tableur généré : http://www.juricaf.org/documentation/stats/champs.csv";
 }
 catch (Exception $e) {
   echo "Erreur d'enregistrement\n";
   echo $e->getMessage()."\n";
   exit;
 }
-?>
