639217f191b258bcf8c7b568e02ce3f4518e7593
Tangui Morlier <tangui@tangui.eu.org>
2024-04-17 19:42:42 +0200

    On conserve le luke.xml le plus longtemps possible et on s'assure qu'il est généré si il n'existe pas

--- stats/createStats.sh
+++ stats/createStats.sh
@@ -8,9 +8,10 @@ mkdir -p static
 
 php statsBase.php
 
-find static -name luke.xml -mtime +7 -delete
-if ! test -s static/luke.xml ; then
-	curl -s http://$SOLRHOST:8080/solr/admin/luke > static/luke.xml
+if find static -name luke.xml -mtime +7 | grep luke > /dev/null || ! test -s static/luke.xml; then
+	curl -s http://$SOLRHOST:8080/solr/admin/luke > static/luke.xml.new
+	#Pivot sur luke.xml car il est exploité dans la recherche avancée du site
+	mv -f static/luke.xml.new static/luke.xml
 	php statsChamps.php > static/champs.csv.new
 	mv -f static/champs.csv.new static/champs.csv
 fi
