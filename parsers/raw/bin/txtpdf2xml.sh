935668736cca18601a3f83fb388d576ceef77e6a
Tangui Morlier <tangui@tangui.eu.org>
2024-07-16 13:04:21 +0200

    Documentation

--- parsers/raw/bin/txtpdf2xml.sh
+++ parsers/raw/bin/txtpdf2xml.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Format des TXT :
+#     Pays_Juridiction_Formation_DateISO_NumArret.txt
 
 ls *txt | sed 's/.txt//' | while read title ; do 
 	echo $title | awk -F '_' '{print "<?xml version=\"1.0\" encoding=\"UTF-8\"?><DOCUMENT><PAYS>"$1"</PAYS><JURIDICTION>"$2"</JURIDICTION><FORMATION>"$3"</FORMATION><DATE_ARRET>"$4"</DATE_ARRET><NUM_ARRET>"$5"</NUM_ARRET><TEXTE_ARRET>"}' | tr -d '\n' > "$title"".xml"
