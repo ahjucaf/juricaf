84d037db1612ce7b3662cdec3f729af2cc8be80f
Tangui Morlier <tangui@tangui.eu.org>
2024-08-29 17:51:24 +0200

    Permet de faire une mise à jour jusqu'à une date donnée

--- parsers/judilibre/download_update.sh
+++ parsers/judilibre/download_update.sh
@@ -4,10 +4,13 @@ cd $(dirname $0)
 source ../config/config.inc
 
 mkdir -p raw
+if ! test "$1"; then
+date=$(date '+%Y-%m-%d')
+fi
 
-for (( i = 0 ; i < 100 ; i++ )) ; do
+for (( i = 0 ; i < 20 ; i++ )) ; do
 rm -f .continue
-curl -s 'https://www.courdecassation.fr/recherche-judilibre?sort=date-desc&items_per_page=500&search_api_fulltext=&expression_exacte=&date_du=&date_au=&judilibre_chambre=&judilibre_type=&judilibre_publication=&judilibre_solution=&judilibre_juridiction=all&judilibre_formation=&judilibre_zonage=&judilibre_doctype=&judilibre_siege_ca=&judilibre_nature_du_contentieux=&judilibre_type_ca=&op=Trier&page='$i | grep /decision | awk -F '"' '{print $2}' | sed 's/.decision.//'  | while read decision ; do
+curl -s 'https://www.courdecassation.fr/recherche-judilibre?sort=update_date-desc&items_per_page=500&search_api_fulltext=&expression_exacte=&date_du=&date_au='$date'&judilibre_chambre=&judilibre_type=&judilibre_publication=&judilibre_solution=&judilibre_juridiction=all&judilibre_formation=&judilibre_zonage=&judilibre_doctype=&judilibre_siege_ca=&judilibre_nature_du_contentieux=&judilibre_type_ca=&op=Trier&page='$i | grep /decision | awk -F '"' '{print $2}' | sed 's/.decision.//'  | while read decision ; do
 	if ! test -s raw/$decision".json" ; then
 	touch .continue
 	curl -s -H "accept: application/json" -H "KeyId: "$JUDILIBRE_KEYID -X GET 'https://api.piste.gouv.fr/cassation/judilibre/v1.0/decision?resolve_references=true&id='$decision > raw/$decision".json";
--- parsers/judilibre/update.sh
+++ parsers/judilibre/update.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 
-bash download_update.sh | while read file ; do
+bash download_update.sh $1 | while read file ; do
 	bash parse.sh "$file"
 done
