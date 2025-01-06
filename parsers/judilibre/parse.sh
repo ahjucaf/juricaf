dfe00974be61d35a0768b83ae7f6baa48b0d111d
Tangui Morlier <tangui@tangui.eu.org>
2025-01-06 12:24:35 +0100

    Permet une meilleure détection dans la détection des doublons

--- parsers/judilibre/parse.sh
+++ parsers/judilibre/parse.sh
@@ -13,8 +13,8 @@ if test "$juridiction" == "Courdecassation" ; then
 	numres=$(curl -s 'https://juricaf.org/recherche/num_arret:%22'$num'%22/facet_pays%3AFrance%2Cfacet_pays_juridiction%3AFrance_%7C_Cour_de_cassation?format=json' | jq .nb_resultat)
 	if test "$numres" -eq 0; then
 		php parse_jurilibre.php $file > $POOL_DIR/cc/$id".xml"
-	elif test "$numres" -ge 2; then
-		echo "WARNING: duplicates cour de cass $num " 1>&2
+	else
+		echo "WARNING: duplicates/error $num (cour de cass)" 1>&2
 	fi
 elif test "$juridiction" == "Courdappel" || test "$juridiction" == "Tribunaljudiciaire"; then
 	tribunal=$(jq '.location' < $file | sed 's/ /+/g')
@@ -25,8 +25,8 @@ elif test "$juridiction" == "Courdappel" || test "$juridiction" == "Tribunaljudi
 	fi
 	if test "$numres" -eq 0; then
 		php parse_jurilibre.php $file > $POOL_DIR/ca/$id".xml"
-	elif test "$numres" -ge 2; then
-		echo "WARNING: duplicates $tribunal $num" 1>&2
+	else
+		echo "WARNING: duplicates/error $tribunal $num" 1>&2
 	fi
 else
 	echo ERROR: $file : juridiction $juridiction non gérée : 1>&2
