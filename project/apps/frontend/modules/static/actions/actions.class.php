f193ceb047619f7e82fa1b53473c9f06d0deab1e
Tangui Morlier <tangui@tangui.eu.org>
2025-05-05 15:23:48 +0200

    Formulaire de contact ++

--- project/apps/frontend/modules/static/actions/actions.class.php
+++ project/apps/frontend/modules/static/actions/actions.class.php
@@ -10,39 +10,37 @@
 */
 class staticActions extends sfActions
 {
+    private function setCaptchaData() {
+        $this->token = sha1(mt_rand());
+        $_SESSION['token'] = $this->token;
+        $_SESSION['cap1'] = intval(rand(0, 10) + 1);
+        $_SESSION['cap2'] = intval(rand(0, 10) + 1);
+        $_SESSION['captime'] = time();
+    }
+
     public function executeContact(sfWebRequest $request) {
 
         @session_start();
         
         if (!isset($_POST['email'])) {
-            $this->token = sha1(mt_rand());
-            $_SESSION['token'] = $this->token;
-            $_SESSION['cap1'] = intval(rand(0, 10) + 1);
-            $_SESSION['cap2'] = intval(rand(0, 10) + 1);
+            $this->setCaptchaData();
             return sfView::SUCCESS;
         }
         /* Initialisation des variables */
         $from = "juricaf@ahjucaf.org";
         $to = "sgahjucaf@ahjucaf.org";
-        
+        $cc = 'logs@24eme.fr';
+
         /* Préparation */
         $subject = "[Juricaf] Demande de contact"; // le sujet du mail
         $this->email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
         $this->email = filter_var($this->email, FILTER_VALIDATE_EMAIL);
         $this->message = NULL;
 
-        if(!isset($_POST['token']) || ($_SESSION['token'] !== $_POST['token']) || $_SESSION['cap1'] + $_SESSION['cap2'] != $_POST['captcha']) {
-            $this->resultat = "Mauvais Captcha";
-            $this->token = sha1(mt_rand());
-            $_SESSION['token'] = $this->token;
-            return sfView::SUCCESS;
-        }
-        
         /* Récupération du champs email */
         if (empty($this->email) || empty(htmlspecialchars($_POST['message']))) {
             $this->resultat = "Erreur: vous devez spécifier une adresse email valide et un texte\n";
-            $this->token = sha1(mt_rand());
-            $_SESSION['token'] = $this->token;
+            $this->setCaptchaData();
             return sfView::SUCCESS;
         }
 
@@ -54,11 +52,28 @@ class staticActions extends sfActions
         $message .= "-----------------------\n\n";
         $message .= $_POST['message'];
         $message .= "\n\n-----------------------\nAdresse IP de l'expediteur : ".$_SERVER["REMOTE_ADDR"];
-        
+
         if (!$this->email){
             $this->resultat = "Erreur: vous devez spécifier une adresse email valide et un texte\n";
-            $this->token = sha1(mt_rand());
-            $_SESSION['token'] = $this->token;
+            $this->setCaptchaData();
+            return sfView::SUCCESS;
+        }
+
+        if (!$_SESSION['captime'] || ((time() - $_SESSION['captime']) < 5) || !$_SESSION['cap1']) {
+            
+            //Visiblement c'est un robot, donc on ne raconte pas la vérité
+            $this->resultat = "Envoi OK";
+
+            unset($_SESSION['cap1']);
+            unset($_SESSION['cap2']);
+            unset($_SESSION['token']);
+            unset($_SESSION['captime']);
+            return sfView::SUCCESS;
+        }
+
+        if(!isset($_POST['token']) || ($_SESSION['token'] !== $_POST['token']) || ($_SESSION['cap1'] + $_SESSION['cap2'] != $_POST['captcha'])) {
+            $this->resultat = "Mauvais Captcha";
+            $this->setCaptchaData();
             return sfView::SUCCESS;
         }
 
@@ -71,15 +86,20 @@ class staticActions extends sfActions
             //$headers .= "To: Contact <".$to.">\n"; (la variable $to est utilisée à la place)
             $headers .= "MIME-Version: 1.0\n";
             $headers .= "Content-type: text/plain; charset=UTF-8\n";
-            
+
             /* Appel a la fonction mail */
             if (!mail($to, $subject, $message, $headers)){
                 $this->resultat = "Erreur: Impossible d'envoyer le mail";
             } else {
+                mail($cc, $subject, $message, $headers);
                 $this->resultat = "Envoi réussi";
+                unset($_SESSION['cap1']);
+                unset($_SESSION['cap2']);
+                unset($_SESSION['token']);
+                unset($_SESSION['captime']);
             }
         }
-        
+
     }
 
     const SITEMAP_LIMIT = 50000;
