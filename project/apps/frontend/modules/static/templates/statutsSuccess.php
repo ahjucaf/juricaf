9a5056b674cc2e6656335a7894c8dcd669ac7f51
chachathecool <char.var@posteo.com>
2026-01-19 22:03:40 +0100

    Feat: ajouter bouton vers imports dans les statuts

--- project/apps/frontend/modules/static/templates/statutsSuccess.php
+++ project/apps/frontend/modules/static/templates/statutsSuccess.php
@@ -7,6 +7,10 @@
 </div>
 <div class="container mt-5">
     <h5 class="p-3 mb-2 bg-secondary bg-gradient">Statuts et licences des collections</h5>
+    <div style="width:100%; display:flex; justify-content:flex-end;">
+      <?php $today = new DateTime()->format('Y-m-d'); ?>
+      <span class="btn btn-primary" style="margin-left:auto;"><?php echo link_to('<span style="color:white;">Imports sur 30 jours</span>', '@imports?selectedDate=' . $today) ?></span>
+    </div>
     <?php include(__DIR__.'/../../../../../../stats/static/base.html'); ?>
     <div>
         <p>Statistiques brutes de <a href="/documentation/stats/base.csv">mis à jour</a> et de <a href="/documentation/stats/champs.csv">champs disponibles</a>.</p>
