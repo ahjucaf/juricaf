6f7482268210e571475d1e51c1186d79b8d53889
chachathecool <char.var@posteo.com>
2026-01-21 12:01:04 +0100

    Refacto: déplacer url vers page import en bas de la page statuts

--- project/apps/frontend/modules/static/templates/statutsSuccess.php
+++ project/apps/frontend/modules/static/templates/statutsSuccess.php
@@ -7,13 +7,9 @@
 </div>
 <div class="container mt-5">
     <h5 class="p-3 mb-2 bg-secondary bg-gradient">Statuts et licences des collections</h5>
-    <div style="width:100%; display:flex; justify-content:flex-end;">
-      <?php $today = new DateTime(); ?>
-      <span class="btn btn-primary" style="margin-left:auto;"><?php echo link_to('<span style="color:white;">Imports sur 30 jours</span>', '@imports?selectedDate=' . $today->format('Y-m-d')) ?></span>
-    </div>
     <?php include(__DIR__.'/../../../../../../stats/static/base.html'); ?>
     <div>
-        <p>Statistiques brutes de <a href="/documentation/stats/base.csv">mis à jour</a> et de <a href="/documentation/stats/champs.csv">champs disponibles</a>.</p>
+        <p>Statistiques brutes de <a href="/documentation/stats/base.csv">mis à jour</a>, de <a href="/documentation/stats/champs.csv">champs disponibles</a> et <a href="/stats/imports/now" > d'imports</a>.</p>
     </div>
     <div class="text-end"><a id="top" href="#">Haut de page</a></div>
 </div>
