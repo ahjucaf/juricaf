3a104f63f362c93ed618c576a20a9520936721ad
Vincent LAURENT <vince.laurent@gmail.com>
2026-02-26 23:40:45 +0100

    Changement de l'url de plan du site pour éviter les robots

--- project/bin/sitemap_hardcache.sh
+++ project/bin/sitemap_hardcache.sh
@@ -1,4 +1,4 @@
-curl -s https://juricaf.org/sitemap > web/sitemap.xml.tmp
-cat web/sitemap.xml.tmp | grep loc | sed -r 's|<[^<>]*>||g' |sed 's/ //g' | while read url; do XMLFILENAME=$(echo -n $url | sed -r 's|.*/||'); curl -s $url > web/sitemap_$XMLFILENAME.xml.tmp; mv web/sitemap_$XMLFILENAME.xml{.tmp,}; done;
-sed -ri 's|sitemap/([^\/]+)</loc>|sitemap_\1.xml</loc>|' web/sitemap.xml.tmp
-mv web/sitemap.xml{.tmp,}
+curl -s https://juricaf.org/plandusite > web/sitemapsecret/sitemap.xml.tmp
+cat web/sitemapsecret/sitemap.xml.tmp | grep loc | sed -r 's|<[^<>]*>||g' |sed 's/ //g' | while read url; do XMLFILENAME=$(echo -n $url | sed -r 's|.*/||'); curl -s $url > web/sitemapsecret/sitemap_$XMLFILENAME.xml.tmp; mv web/sitemapsecret/sitemap_$XMLFILENAME.xml{.tmp,}; done;
+sed -ri 's|plandusite/([^\/]+)</loc>|sitemapsecret/sitemap_\1.xml</loc>|' web/sitemapsecret/sitemap.xml.tmp
+mv web/sitemapsecret/sitemap.xml{.tmp,}
